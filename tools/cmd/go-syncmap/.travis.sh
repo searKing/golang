93ae654c2be025bf3788b7e93d85f2b5454868c5
陈海欣 <471030698@qq.com>
2019-07-24 19:38:31 +0800

    [CHG] safe exit for bash

--- tools/cmd/go-syncmap/.travis.sh
+++ tools/cmd/go-syncmap/.travis.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+rm -Rf .travis.workspace/ || exit -1
 git clone https://github.com/searKing/golang.git .travis.workspace/golang || exit -1
 pushd .travis.workspace/golang/ || exit -1
 git filter-branch --prune-empty --subdirectory-filter tools/cmd/go-syncmap/ master || exit -1
