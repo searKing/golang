73aa0ecc03a56f71e54dccffe9a0cc8ab5b2f575
陈海欣 <471030698@qq.com>
2019-07-24 19:36:29 +0800

    [CHG] safe exit for bash

--- tools/cmd/go-atomicvalue/.travis.sh
+++ tools/cmd/go-atomicvalue/.travis.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/searKing/golang.git .travis.workspace/golang
-pushd golang/
-git filter-branch --prune-empty --subdirectory-filter tools/cmd/go-atomicvalue/ master
-git remote set-url origin https://github.com/searKing/travis-ci.git
-git push -f origin master:go-atomicvalue
-popd
-rm -Rf .travis.workspace/
+git clone https://github.com/searKing/golang.git .travis.workspace/golang || exit -1
+pushd golang/ || exit -1
+git filter-branch --prune-empty --subdirectory-filter tools/cmd/go-atomicvalue/ master || exit -1
+git remote set-url origin https://github.com/searKing/travis-ci.git || exit -1
+git push -f origin master:go-atomicvalue || exit -1
+popd || exit -1
+rm -Rf .travis.workspace/ || exit -1
