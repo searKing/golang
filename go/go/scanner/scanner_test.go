6da96db06d88c2485a52cd7748e6ec5397e59135
陈海欣 <471030698@qq.com>
2019-07-27 14:27:14 +0800

    [ADD] bufio.SplitFunc

--- go/go/scanner/scanner_test.go
+++ go/go/scanner/scanner_test.go
@@ -17,12 +17,12 @@ type ScanTests struct {
 var (
 	scanTests = []ScanTests{
 		{
-			split:  bufio.ScanWords,
+			split:  scanner.ScanWords,
 			input:  []byte("cos(x) + 1i*sin(x) // Euler"),
 			output: [][]byte{[]byte("cos(x)"), []byte("+"), []byte("1i*sin(x)"), []byte("//"), []byte("Euler")},
 		},
 		{
-			split: bufio.ScanBytes,
+			split: scanner.ScanBytes,
 			input: []byte("cos(x) + 1i*sin(x) // Euler"),
 			output: [][]byte{[]byte("c"), []byte("o"), []byte("s"), []byte("("), []byte("x"), []byte(")"),
 				[]byte(" "), []byte("+"), []byte(" "),
@@ -33,12 +33,12 @@ var (
 				[]byte("E"), []byte("u"), []byte("l"), []byte("e"), []byte("r")},
 		},
 		{
-			split:  bufio.ScanLines,
+			split:  scanner.ScanLines,
 			input:  []byte("cos(x) + 1i*sin(x) \n// Euler"),
 			output: [][]byte{[]byte("cos(x) + 1i*sin(x) "), []byte("// Euler")},
 		},
 		{
-			split: bufio.ScanRunes,
+			split: scanner.ScanRunes,
 			input: []byte("cos(x) + 1i*sin(x) // Euler"),
 			output: [][]byte{[]byte("c"), []byte("o"), []byte("s"), []byte("("), []byte("x"), []byte(")"),
 				[]byte(" "), []byte("+"), []byte(" "),
@@ -47,6 +47,81 @@ var (
 				[]byte("/"), []byte("/"),
 				[]byte(" "),
 				[]byte("E"), []byte("u"), []byte("l"), []byte("e"), []byte("r")},
+		},
+		{
+			split:  scanner.ScanEscapes('"'),
+			input:  []byte(`\\\r\n\"cos(x) + 1i*sin(x) // Euler`),
+			output: [][]byte{[]byte(`\\`), []byte(`\r`), []byte(`\n`), []byte(`\"`)},
+		},
+		{
+			split:  scanner.ScanEscapes('"'),
+			input:  []byte(`\y`),
+			output: [][]byte{},
+		},
+		{
+			split:  scanner.ScanEscapes('"'),
+			input:  []byte(`\xGGGG`),
+			output: [][]byte{},
+		},
+		{
+			split:  scanner.ScanInterpretedStrings,
+			input:  []byte(`\xGGGG`),
+			output: [][]byte{},
+		},
+		{
+			split:  scanner.ScanInterpretedStrings,
+			input:  []byte(`"""Hello World\""\xGGGG`),
+			output: [][]byte{[]byte(`""`), []byte(`"Hello World\""`)},
+		},
+		{
+			split:  scanner.ScanRawStrings,
+			input:  []byte("```Hello World\r`"),
+			output: [][]byte{[]byte("``"), []byte("`Hello World\r`")},
+		},
+		{
+			split:  scanner.ScanMantissas(10),
+			input:  []byte("123"),
+			output: [][]byte{[]byte("123")},
+		},
+		{
+			split:  scanner.ScanMantissas(8),
+			input:  []byte("1238"),
+			output: [][]byte{[]byte("123")},
+		},
+		{
+			split:  scanner.ScanNumbers,
+			input:  []byte("123"),
+			output: [][]byte{[]byte("123")},
+		},
+		{
+			split:  scanner.ScanNumbers,
+			input:  []byte("078"),
+			output: [][]byte{},
+		},
+		{
+			split:  scanner.ScanNumbers,
+			input:  []byte("123.56"),
+			output: [][]byte{[]byte("123.56")},
+		},
+		{
+			split:  scanner.ScanNumbers,
+			input:  []byte("123.56i78.96i"),
+			output: [][]byte{[]byte("123.56i"), []byte("78.96i")},
+		},
+		{
+			split:  scanner.ScanNumbers,
+			input:  []byte("0x55.FF"),
+			output: [][]byte{[]byte("0x55")},
+		},
+		{
+			split:  scanner.ScanNumbers,
+			input:  []byte("077x"),
+			output: [][]byte{[]byte("077")},
+		},
+		{
+			split:  scanner.ScanNumbers,
+			input:  []byte("0xFG"),
+			output: [][]byte{[]byte("0xF")},
 		},
 	}
 )
@@ -59,12 +134,15 @@ Outer:
 		fset := token.NewFileSet()                             // positions are relative to fset
 		file := fset.AddFile("", fset.Base(), len(test.input)) // register input "file"
 		s.Init(file, test.input, nil /* no error handler */, scanner.ModeCaseSensitive)
-		s.Split(test.split)
 		var i int
 		for ; ; i++ {
-			token, has := s.Scan()
+			token, has := s.ScanSplits(test.split)
 			if !has {
 				break
+			}
+			if i >= len(test.output) {
+				t.Errorf("#%d: scan[%d] got %v; nothing expected", n, i, string(token))
+				continue Outer
 			}
 			if bytes.Compare(token, test.output[i]) != 0 {
 				t.Errorf("#%d: scan[%d] got %v; expected %v", n, i, string(token), string(test.output[i]))
