7bddbf90e7d2dcd88697620b57780a863c7814d6
陈海欣 <471030698@qq.com>
2019-08-20 22:50:25 +0800

    [ADD] io.DynamicReaderFunc returns a Reader that's from the provided input reader getter function.

---
+++ go/io/dynamic.go
@@ -0,0 +1,18 @@
+package io
+
+import "io"
+
+// DynamicReaderFunc returns a Reader that's from
+// the provided input reader getter function.
+type DynamicReaderFunc func() io.Reader
+
+func (f DynamicReaderFunc) Read(p []byte) (n int, err error) {
+	var r io.Reader
+	if f != nil {
+		r = f()
+	}
+	if r == nil {
+		r = EOFReader()
+	}
+	return r.Read(p)
+}
