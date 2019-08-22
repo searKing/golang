df23590f0c9a52d64bb4d5125d042af6751f32a4
陈海欣 <471030698@qq.com>
2019-08-22 20:24:25 +0800

    [ADD] net.TcpKeepAliveListener sets TCP keep-alive timeouts on accepted connections. It's used by ListenAndServe and ListenAndServeTLS so dead TCP connections (e.g. closing laptop mid-download) eventually go away.

---
+++ go/net/tcp_keep_alive_listener.go
@@ -0,0 +1,33 @@
+package net
+
+import (
+	"net"
+	"time"
+)
+
+type tcpKeepAliveListener struct {
+	*net.TCPListener
+	d time.Duration
+}
+
+func (ln tcpKeepAliveListener) Accept() (net.Conn, error) {
+	tc, err := ln.AcceptTCP()
+	if err != nil {
+		return nil, err
+	}
+	if ln.d == 0 {
+		_ = tc.SetKeepAlive(false)
+		return tc, nil
+	}
+	_ = tc.SetKeepAlive(true)
+	_ = tc.SetKeepAlivePeriod(ln.d)
+	return tc, nil
+}
+
+// TcpKeepAliveListener sets TCP keep-alive timeouts on accepted
+// connections. It's used by ListenAndServe and ListenAndServeTLS so
+// dead TCP connections (e.g. closing laptop mid-download) eventually
+// go away.
+func TcpKeepAliveListener(l *net.TCPListener, d time.Duration) net.Listener {
+	return &tcpKeepAliveListener{l, d}
+}
