372df1cf1064b62aa07baab51c4a920c99b0c729
陈海欣 <471030698@qq.com>
2019-08-23 13:59:30 +0800

    [REFACTOR] rename

--- go/container/traversal/bfs.go
+++ go/container/traversal/bfs.go
@@ -1,9 +1,31 @@
+// Copyright 2019 The searKing Author. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// https://en.wikipedia.org/wiki/Breadth-first_search
 package traversal
 
+// Breadth-first search (BFS)
+// 1. Check if the current depth level is empty or null.
+// 2. Display the data part of all of the neighbor nodes at the present depth.
+// 2. Traverse the next depth level by recursively calling the bfs-order function.
+// Implement:
+//	procedure BreadthFirstSearch(G,start_v):
+//		let Q be a queue
+//		label start_v as discovered
+//		Q.enqueue(start_v)
+//		while Q is not empty
+//			v = Q.dequeue()
+//			if v is the goal:
+//				return v
+//			for all edges from v to w in G.adjacentEdges(v) do
+//				if w is not labeled as discovered:
+//					label w as discovered
+//					w.parent = v
+//					Q.enqueue(w)
 // TODO template in Go2.0 is expected
-// Breadth First Search
-func BFS(ele interface{}, filterFn func(ele interface{}, depth int) (gotoNextLayer bool), processFn func(ele interface{}, depth int) (gotoNextLayer bool)) {
-	traversalBFS([]Node{{
+func BreadthFirstSearchOrder(ele interface{}, filterFn func(ele interface{}, depth int) (gotoNextLayer bool), processFn func(ele interface{}, depth int) (gotoNextLayer bool)) {
+	bfs([]Node{{
 		ele: ele,
 	}}, func(node Node) (gotoNextLayer bool) {
 		if filterFn == nil {
@@ -21,12 +43,12 @@ func BFS(ele interface{}, filterFn func(ele interface{}, depth int) (gotoNextLay
 }
 
 // isRoot root needs to be filtered first time
-func traversalBFS(current []Node, filterFn func(node Node) (gotoNextLayer bool), processFn func(node Node) (gotoNextLayer bool), isRoot bool) (gotoNextLayer bool) {
+func bfs(current []Node, filterFn func(node Node) (gotoNextLayer bool), processFn func(node Node) (gotoNextLayer bool), isRoot bool) (gotoNextLayer bool) {
 	if len(current) == 0 {
 		return false
 	}
 	// Step 1: brothers layer
-	nextBrothers := []Node{}
+	var nextBrothers []Node
 	for _, node := range current {
 		// filter root
 		if isRoot {
@@ -42,7 +64,7 @@ func traversalBFS(current []Node, filterFn func(node Node) (gotoNextLayer bool),
 	}
 
 	// Step 2: children layer
-	nextChildren := []Node{}
+	var nextChildren []Node
 	// filter children
 	for _, node := range nextBrothers {
 		// Scan node for nodes to include.
@@ -50,6 +72,6 @@ func traversalBFS(current []Node, filterFn func(node Node) (gotoNextLayer bool),
 		nextChildren = append(nextChildren, filterChildren(node, node.MiddleNodes(), filterFn)...)
 		nextChildren = append(nextChildren, filterChildren(node, node.RightNodes(), filterFn)...)
 	}
-	traversalBFS(nextChildren, filterFn, processFn, false)
+	bfs(nextChildren, filterFn, processFn, false)
 	return true
 }
--- go/container/traversal/dfs.go
+++ go/container/traversal/dfs.go
@@ -1,9 +1,25 @@
+// Copyright 2019 The searKing Author. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// https://en.wikipedia.org/wiki/Depth-first_search
 package traversal
 
+// Depth-first search (DFS)
+// 1. Check if the current node is empty or null.
+// 2. Display the data part of the root (or current node).
+// 3. Traverse the middle subtree by recursively calling the dfs-order function.
+// 4. Traverse the left subtree by recursively calling the dfs-order function.
+// 5. Traverse the right subtree by recursively calling the dfs-order function.
+// Implement:
+//	procedure DFS(G,v):
+//		label v as discovered
+//		for all directed edges from v to w that are in G.adjacentEdges(v) do
+//			if vertex w is not labeled as discovered then
+//				recursively call DFS(G,w)
 // TODO template in Go2.0 is expected
-// Depth First Search
-func DFS(ele interface{}, filterFn func(ele interface{}, depth int) (gotoNextLayer bool), processFn func(ele interface{}, depth int) (gotoNextLayer bool)) {
-	traversalDFS([]Node{{
+func DepthFirstSearchOrder(ele interface{}, filterFn func(ele interface{}, depth int) (gotoNextLayer bool), processFn func(ele interface{}, depth int) (gotoNextLayer bool)) {
+	dfs([]Node{{
 		ele: ele,
 	}}, func(node Node) (gotoNextLayer bool) {
 		if filterFn == nil {
@@ -21,7 +37,7 @@ func DFS(ele interface{}, filterFn func(ele interface{}, depth int) (gotoNextLay
 }
 
 // isRoot root needs to be filtered first time
-func traversalDFS(current []Node, filterFn func(node Node) (gotoNextLayer bool), processFn func(node Node) (gotoNextLayer bool), isRoot bool) (gotoNextLayer bool) {
+func dfs(current []Node, filterFn func(node Node) (gotoNextLayer bool), processFn func(node Node) (gotoNextLayer bool), isRoot bool) (gotoNextLayer bool) {
 	if len(current) == 0 {
 		return false
 	}
@@ -37,9 +53,9 @@ func traversalDFS(current []Node, filterFn func(node Node) (gotoNextLayer bool),
 			return false
 		}
 		// filter children
-		traversalDFS(filterChildren(node, node.MiddleNodes(), filterFn), filterFn, processFn, false)
-		traversalDFS(filterChildren(node, node.LeftNodes(), filterFn), filterFn, processFn, false)
-		traversalDFS(filterChildren(node, node.RightNodes(), filterFn), filterFn, processFn, false)
+		dfs(filterChildren(node, node.MiddleNodes(), filterFn), filterFn, processFn, false)
+		dfs(filterChildren(node, node.LeftNodes(), filterFn), filterFn, processFn, false)
+		dfs(filterChildren(node, node.RightNodes(), filterFn), filterFn, processFn, false)
 	}
 	return true
 }
