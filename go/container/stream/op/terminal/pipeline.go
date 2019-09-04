52048d4b079f37f2ab88d74393055baa09a9c0e7
陈海欣 <471030698@qq.com>
2019-09-04 17:13:27 +0800

    [ADD] java.util.stream.TerminalOps

---
+++ go/container/stream/op/terminal/pipeline.go
@@ -0,0 +1,33 @@
+package terminal
+
+import (
+	"context"
+	"github.com/searKing/golang/go/util/object"
+	"github.com/searKing/golang/go/util/spliterator"
+)
+
+/**
+ * Applies the pipeline stages described by this {@code PipelineHelper} to
+ * the provided {@code Spliterator} and send the results to the provided
+ * {@code Sink}.
+ *
+ * @implSpec
+ * The implementation behaves as if:
+ * <pre>{@code
+ *     copyInto(wrapSink(sink), spliterator);
+ * }</pre>
+ *
+ * @param sink the {@code Sink} to receive the results
+ * @param spliterator the spliterator describing the source input to process
+ */
+func WrapAndCopyInto(ctx context.Context, sink Sink, spliterator spliterator.Spliterator) Sink {
+	CopyInto(ctx, object.RequireNonNull(sink).(Sink), spliterator)
+	return sink
+}
+
+func CopyInto(ctx context.Context, wrappedSink Sink, spliterator spliterator.Spliterator) {
+	object.RequireNonNil(wrappedSink)
+	wrappedSink.Begin(spliterator.GetExactSizeIfKnown())
+	spliterator.ForEachRemaining(ctx, wrappedSink)
+	wrappedSink.End()
+}
