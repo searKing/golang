bac184f1bf56924359c8a7a72409b3eda69e705c
陈海欣 <471030698@qq.com>
2019-09-04 19:42:48 +0800

    [TODO] java.util.stream

---
+++ go/container/stream/doc.go
@@ -0,0 +1,4 @@
+// A sequence of elements supporting sequential and parallel aggregate
+// operations.  The following example illustrates an aggregate operation using
+// SEE java/util/stream/Stream.java
+package stream
