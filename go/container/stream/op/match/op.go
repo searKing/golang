bd0668598b32d66d4ad3dbca5f54dc37f8735c90
陈海欣 <471030698@qq.com>
2019-09-04 19:40:34 +0800

    [ADD] java.util.stream.MatchOps

---
+++ go/container/stream/op/match/op.go
@@ -0,0 +1,50 @@
+package match
+
+import (
+	"context"
+	"github.com/searKing/golang/go/container/stream/op/terminal"
+	"github.com/searKing/golang/go/util/function/predicate"
+	"github.com/searKing/golang/go/util/object"
+	"github.com/searKing/golang/go/util/optional"
+	"github.com/searKing/golang/go/util/spliterator"
+)
+
+type MatchOp struct {
+	terminal.TODOOperation
+	matchKind Kind
+	sinkNewer func() *MatchSink
+}
+
+func NewMatchOp(matchKind Kind, sinkNewer func() *MatchSink) *MatchOp {
+	reduceOp := &MatchOp{
+		matchKind: matchKind,
+		sinkNewer: sinkNewer,
+	}
+	reduceOp.SetDerived(reduceOp)
+	return reduceOp
+}
+
+func NewMatchOp2(matchKind Kind, predicate predicate.Predicater) *MatchOp {
+	object.RequireNonNil(predicate)
+	return NewMatchOp(matchKind, func() *MatchSink {
+		return NewMatchSink(matchKind, predicate)
+	})
+}
+
+func (op MatchOp) MakeSink() *MatchSink {
+	object.RequireNonNull(op.sinkNewer)
+	return op.sinkNewer()
+}
+
+func (op MatchOp) EvaluateParallel(ctx context.Context, spliterator spliterator.Spliterator) optional.Optional {
+	return terminal.WrapAndCopyInto(ctx, op.MakeSink(), spliterator).Get()
+}
+
+func (op MatchOp) EvaluateSequential(ctx context.Context, spliterator spliterator.Spliterator) optional.Optional {
+	// Approach for parallel implementation:
+	// - Decompose as per usual
+	// - run match on leaf chunks, call result "b"
+	// - if b == matchKind.shortCircuitOn, complete early and return b
+	// - else if we complete normally, return !shortCircuitOn
+	return NewMatchTask(op, spliterator).Invoke(ctx).Get()
+}
---
+++ go/container/stream/op/match/task.go
@@ -0,0 +1,44 @@
+package match
+
+import (
+	"context"
+	"github.com/searKing/golang/go/container/stream/op/terminal"
+	"github.com/searKing/golang/go/util/spliterator"
+)
+
+type MatchTask struct {
+	terminal.TODOShortCircuitTask
+
+	op MatchOp
+}
+
+func NewMatchTask(op MatchOp, spliterator spliterator.Spliterator) *MatchTask {
+	matcher := &MatchTask{
+		op: op,
+	}
+	matcher.WithSpliterator(spliterator)
+	matcher.SetDerived(matcher)
+	return matcher
+}
+
+func NewTaskFromParent(parent MatchTask, spliterator spliterator.Spliterator) *MatchTask {
+	matcher := &MatchTask{
+		op: parent.op,
+	}
+	matcher.WithSpliterator(spliterator)
+	matcher.SetDerived(matcher)
+	return matcher
+}
+
+func (task MatchTask) MakeChild(spliterator spliterator.Spliterator) terminal.Task {
+	return NewTaskFromParent(task, spliterator)
+}
+
+func (task MatchTask) DoLeaf(ctx context.Context) terminal.Sink {
+	result := terminal.WrapAndCopyInto(ctx, task.op.MakeSink(), task.GetSpliterator())
+	b := result.(*booleanTerminalSink).GetAndClearState()
+	if b == task.op.matchKind.ShortCircuitResult {
+		task.ShortCircuit(result)
+	}
+	return nil
+}
