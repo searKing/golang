02c5d718c067a434e831f373e936ac32ad161026
陈海欣 <471030698@qq.com>
2019-09-04 19:42:48 +0800

    [TODO] java.util.stream

---
+++ go/container/stream/of.go
@@ -0,0 +1,12 @@
+package stream
+
+/**
+ * Returns a sequential ordered stream whose elements are the specified values.
+ *
+ * @param <T> the type of stream elements
+ * @param values the elements of the new stream
+ * @return the new stream
+ */
+func Of(values ...interface{}) Stream {
+	return nil
+}
