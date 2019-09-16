68150f50c976630422698f226c2c5584e3a813c0
陈海欣 <471030698@qq.com>
2019-09-16 21:11:03 +0800

    [ADD] pasta : copy & paste-friendly golang crypto

---
+++ thirdparty/github.com/gtank/cryptopasta/pasta_test.go
@@ -0,0 +1,67 @@
+package cryptopasta_test
+
+import (
+	"bytes"
+	"crypto/rand"
+	"github.com/searKing/golang/thirdparty/github.com/gtank/cryptopasta"
+	"io"
+	"io/ioutil"
+	"testing"
+)
+
+func TestEncryptDecryptGCM(t *testing.T) {
+	randomKey := &[32]byte{}
+	_, err := io.ReadFull(rand.Reader, randomKey[:])
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	gcmTests := []struct {
+		plaintext []byte
+		key       []byte
+	}{
+		{
+			plaintext: []byte("Hello, world!"),
+			key:       randomKey[:],
+		},
+	}
+
+	for _, tt := range gcmTests {
+		ciphertext, err := cryptopasta.Encrypt(tt.plaintext, tt.key)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		plaintext, err := cryptopasta.Decrypt(ciphertext, tt.key)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if !bytes.Equal(plaintext, tt.plaintext) {
+			t.Errorf("plaintexts don't match")
+		}
+		malformedCiphertext := string(ciphertext[0]^0xff) + ciphertext[1:]
+		plaintext, err = cryptopasta.Decrypt(malformedCiphertext)
+		if err == nil {
+			t.Errorf("gcmOpen should not have worked, but did")
+		}
+	}
+}
+
+func BenchmarkAESGCM(b *testing.B) {
+	randomKey := &[32]byte{}
+	_, err := io.ReadFull(rand.Reader, randomKey[:])
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	data, err := ioutil.ReadFile("testdata/big")
+	if err != nil {
+		b.Fatal(err)
+	}
+	b.SetBytes(int64(len(data)))
+
+	for i := 0; i < b.N; i++ {
+		cryptopasta.Encrypt(data, randomKey[:])
+	}
+}
