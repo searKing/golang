8cf982c19d8611de283917c250ded154a05e187f
陈海欣 <471030698@qq.com>
2019-09-21 00:39:23 +0800

    [ADD] interceptors for context.Tags

---
+++ thirdparty/github.com/grpc-ecosystem/go-grpc-middleware/interceptors/tags/kind.go
@@ -0,0 +1,9 @@
+package tags
+
+// interceptors's kind
+type Kind string
+
+const (
+	KindServer Kind = "server"
+	KindClient Kind = "client"
+)
---
+++ thirdparty/github.com/grpc-ecosystem/go-grpc-middleware/interceptors/tags/server_interceptors.go
@@ -0,0 +1,54 @@
+package tags
+
+import (
+	"context"
+	"github.com/grpc-ecosystem/go-grpc-middleware"
+	context_ "github.com/searKing/golang/go/context"
+	"google.golang.org/grpc"
+	"path"
+)
+
+var (
+	// SystemField is used in every statement made through value. Can be overwritten before any initialization code.
+	SystemField = "system"
+
+	// KindField indicates whether this is a server or a client interceptor.
+	KindField = "grpc.kind"
+
+	// ServiceField indicates rpc's service name
+	ServiceField = "grpc.service"
+	// MethodField indicates rpc's service method
+	MethodField = "grpc.method"
+)
+
+// UnaryServerInterceptor returns a new unary server interceptors with tags in context.
+func UnaryServerInterceptor(key interface{}, values map[string]interface{}) grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		newCtx := newContextTagsForCall(ctx, KindServer, info.FullMethod, key, values)
+		return handler(newCtx, req)
+	}
+}
+
+// StreamServerInterceptor returns a new streaming server interceptor with tags in context.
+func StreamServerInterceptor(key interface{}, values map[string]interface{}) grpc.StreamServerInterceptor {
+	return func(srv interface{}, stream grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+		newCtx := newContextTagsForCall(stream.Context(), KindServer, info.FullMethod, key, values)
+		wrapped := grpc_middleware.WrapServerStream(stream)
+		wrapped.WrappedContext = newCtx
+		return handler(srv, wrapped)
+	}
+}
+
+func newContextTagsForCall(ctx context.Context, kind Kind, fullMethodString string, key interface{}, values map[string]interface{}) context.Context {
+	service := path.Dir(fullMethodString)[1:]
+	method := path.Base(fullMethodString)
+	tags := context_.NewTags()
+	tags.Set(SystemField, "grpc")
+	tags.Set(KindField, kind)
+	tags.Set(ServiceField, service)
+	tags.Set(MethodField, method)
+	for key, val := range values {
+		tags.Set(key, val)
+	}
+	return context_.WithTags(ctx, key, tags)
+}
