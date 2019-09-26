bf699c88699666b309ae7b9eda751104480d699c
陈海欣 <471030698@qq.com>
2019-09-26 22:01:20 +0800

    [ADD] strings.MapLeading

--- go/strings/strings.go
+++ go/strings/strings.go
@@ -1,5 +1,7 @@
 package strings
 
+import "unicode"
+
 // SliceContains  reports whether s is within ss.
 func SliceContains(ss []string, s string) bool {
 	for _, v := range ss {
@@ -8,4 +10,31 @@ func SliceContains(ss []string, s string) bool {
 		}
 	}
 	return false
+}
+
+// MapLeading returns a copy of the string s with its first characters modified
+// according to the mapping function. If mapping returns a negative value, the character is
+// dropped from the string with no replacement.
+func MapLeading(mapping func(rune) rune, s string) string {
+	rLeading, sRight := ExtractFirstRune(s)
+	srMapped := mapping(rLeading)
+	if srMapped < 0 {
+		return sRight
+	}
+
+	// Fast path for unchanged input
+	if rLeading == srMapped {
+		return s
+	}
+	return string(srMapped) + sRight
+}
+
+// ToLowerLeading returns s with it's first Unicode letter mapped to their lower case.
+func ToLowerLeading(s string) string {
+	return MapLeading(unicode.ToLower, s)
+}
+
+// ToUpperLeading returns s with it's first Unicode letter mapped to their upper case.
+func ToUpperLeading(s string) string {
+	return MapLeading(unicode.ToUpper, s)
 }
--- go/strings/strings_test.go
+++ go/strings/strings_test.go
@@ -2,7 +2,11 @@ package strings_test
 
 import (
 	"github.com/searKing/golang/go/strings"
+	"reflect"
 	"testing"
+	"unicode"
+	"unicode/utf8"
+	"unsafe"
 )
 
 type SliceContainsTest struct {
@@ -32,5 +36,148 @@ func TestSliceContains(t *testing.T) {
 		if out != test.output {
 			t.Errorf("#%d: got %v; expected %v", n, out, test.output)
 		}
+	}
+}
+
+func tenRunes(ch rune) string {
+	r := make([]rune, 10)
+	for i := range r {
+		r[i] = ch
+	}
+	return string(r)
+}
+
+func leadingTenRunes(lead, ch rune) string {
+	r := make([]rune, 10)
+	for i := range r {
+		if i == 0 {
+			if lead < 0 {
+				continue
+			}
+			r[i ] = lead
+			continue
+		}
+		if ch < 0 {
+			continue
+		}
+		r[i] = ch
+	}
+	return string(r)
+}
+
+// User-defined self-inverse mapping function
+func rot13(r rune) rune {
+	step := rune(13)
+	if r >= 'a' && r <= 'z' {
+		return ((r - 'a' + step) % 26) + 'a'
+	}
+	if r >= 'A' && r <= 'Z' {
+		return ((r - 'A' + step) % 26) + 'A'
+	}
+	return r
+}
+
+func TestMapLeading(t *testing.T) {
+	// Run a couple of awful growth/shrinkage tests
+	a := tenRunes('a')
+	// 1.  Grow. This triggers two reallocations in Map.
+	maxRune := func(rune) rune { return unicode.MaxRune }
+	m := strings.MapLeading(maxRune, a)
+	expect := leadingTenRunes(unicode.MaxRune, 'a')
+	if m != expect {
+		t.Errorf("growing: expected %q got %q", expect, m)
+	}
+
+	// 2. Shrink
+	minRune := func(rune) rune { return 'a' }
+	m = strings.MapLeading(minRune, leadingTenRunes(unicode.MaxRune, 'a'))
+	expect = a
+	if m != expect {
+		t.Errorf("shrinking: expected %q got %q", expect, m)
+	}
+
+	// 3. Rot13
+	m = strings.MapLeading(rot13, "a to zed")
+	expect = "n to zed"
+	if m != expect {
+		t.Errorf("rot13: expected %q got %q", expect, m)
+	}
+
+	// 4. Rot13^2
+	m = strings.MapLeading(rot13, strings.MapLeading(rot13, "a to zed"))
+	expect = "a to zed"
+	if m != expect {
+		t.Errorf("rot13: expected %q got %q", expect, m)
+	}
+
+	// 5. Drop
+	dropNotLatin := func(r rune) rune {
+		if unicode.Is(unicode.Latin, r) {
+			return r
+		}
+		return -1
+	}
+	m = strings.MapLeading(dropNotLatin, "세계, Hello")
+	expect = "계, Hello"
+	if m != expect {
+		t.Errorf("drop: expected %q got %q", expect, m)
+	}
+
+	// 6. Identity
+	identity := func(r rune) rune {
+		return r
+	}
+	orig := "Input string that we expect not to be copied."
+	m = strings.MapLeading(identity, orig)
+	if (*reflect.StringHeader)(unsafe.Pointer(&orig)).Data !=
+		(*reflect.StringHeader)(unsafe.Pointer(&m)).Data {
+		t.Error("unexpected copy during identity map")
+	}
+
+	// 7. Handle invalid UTF-8 sequence
+	replaceNotLatin := func(r rune) rune {
+		if unicode.Is(unicode.Latin, r) {
+			return r
+		}
+		return utf8.RuneError
+	}
+	m = strings.MapLeading(replaceNotLatin, "中 Hello\255World")
+	expect = "\uFFFD Hello\255World"
+	if m != expect {
+		t.Errorf("replace invalid sequence: expected %q got %q", expect, m)
+	}
+
+	// 8. Check utf8.RuneSelf and utf8.MaxRune encoding
+	encode := func(r rune) rune {
+		switch r {
+		case utf8.RuneSelf:
+			return unicode.MaxRune
+		case unicode.MaxRune:
+			return utf8.RuneSelf
+		}
+		return r
+	}
+	s := string(utf8.RuneSelf) + string(utf8.MaxRune)
+	r := string(utf8.MaxRune) + string(utf8.MaxRune) // reverse of s
+	m = strings.MapLeading(encode, s)
+	if m != r {
+		t.Errorf("encoding not handled correctly: expected %q got %q", r, m)
+	}
+	m = strings.MapLeading(encode, r)
+	if m != s {
+		t.Errorf("encoding not handled correctly: expected %q got %q", s, m)
+	}
+
+	// 9. Check mapping occurs in the front, middle and back
+	trimSpaces := func(r rune) rune {
+		if unicode.IsSpace(r) {
+			return -1
+		}
+		return r
+	}
+	m = strings.MapLeading(trimSpaces, "   abc    123   ")
+	expect = "  abc    123   "
+	if m != expect {
+		t.Errorf("trimSpaces: expected %q got %q", expect, m)
 	}
 }
