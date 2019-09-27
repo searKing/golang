b94a77799839ae9804a81992e097f6715d46a06f
陈海欣 <471030698@qq.com>
2019-09-27 10:33:52 +0800

    [CHG] disable yaml type check

--- tools/cmd/go-enum/format.yaml.go
+++ tools/cmd/go-enum/format.yaml.go
@@ -1,21 +1,22 @@
 package main
 
-var yamlImportPackages = []string{`gopkg.in/yaml.v2`}
+var yamlImportPackages []string
+//var yamlImportPackages = []string{`gopkg.in/yaml.v2`}
 
 // Arguments to format are:
 //	[1]: type name
 const yamlTemplate = `
-func _() {
-	var _nil_%[1]s_value = func() (val %[1]s) { return }()
-
-	// An "cannot convert %[1]s literal (type %[1]s) to type yaml.Marshaler" compiler error signifies that the base type have changed.
-	// Re-run the go-enum command to generate them again.
-	var _ yaml.Marshaler = _nil_%[1]s_value
-
-	// An "cannot convert %[1]s literal (type %[1]s) to type yaml.Unmarshaler" compiler error signifies that the base type have changed.
-	// Re-run the go-enum command to generate them again.
-	var _ yaml.Unmarshaler = &_nil_%[1]s_value
-}
+//func _() {
+//	var _nil_%[1]s_value = func() (val %[1]s) { return }()
+//
+//	// An "cannot convert %[1]s literal (type %[1]s) to type yaml.Marshaler" compiler error signifies that the base type have changed.
+//	// Re-run the go-enum command to generate them again.
+//	var _ yaml.Marshaler = _nil_%[1]s_value
+//
+//	// An "cannot convert %[1]s literal (type %[1]s) to type yaml.Unmarshaler" compiler error signifies that the base type have changed.
+//	// Re-run the go-enum command to generate them again.
+//	var _ yaml.Unmarshaler = &_nil_%[1]s_value
+//}
 
 // MarshalYAML implements a YAML Marshaler for %[1]s
 func (i %[1]s) MarshalYAML() (interface{}, error) {
--- tools/cmd/go-enum/go.mod
+++ tools/cmd/go-enum/go.mod
@@ -6,5 +6,4 @@ require (
 	github.com/searKing/golang/go v0.0.0-20190927020759-31255fa46687
 	github.com/searKing/golang/tools/common v0.0.0-20190927020759-31255fa46687
 	golang.org/x/tools v0.0.0-20190926165942-a8d5d34286bd
-	gopkg.in/yaml.v2 v2.2.2
 )
