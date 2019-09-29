3f30c6c6a5b231e55e6ca36fd5099f2ca1e50e71
陈海欣 <471030698@qq.com>
2019-09-29 20:33:06 +0800

    [ADD] add MIME-style key mapping

--- go/context/tags.map.go
+++ go/context/tags.map.go
@@ -1,19 +1,44 @@
 package context
 
+import "net/textproto"
+
+//go:generate go-option -type=mapTags
 type mapTags struct {
+	isMimeKey bool // represents a MIME-style key mapping
+
 	values map[string]interface{}
 }
 
-func (t *mapTags) Set(key string, value interface{}) Tags {
+func (t *mapTags) Set(key string, value interface{}) {
+	if t.isMimeKey {
+		key = textproto.CanonicalMIMEHeaderKey(key)
+	}
 	t.values[key] = value
-	return t
 }
 
 func (t *mapTags) Get(key string) (interface{}, bool) {
+	if t.isMimeKey {
+		key = textproto.CanonicalMIMEHeaderKey(key)
+	}
 	val, ok := t.values[key]
 	return val, ok
 }
 
+// Del deletes the values associated with key.
+func (t *mapTags) Del(key string) {
+	if t.isMimeKey {
+		key = textproto.CanonicalMIMEHeaderKey(key)
+	}
+	delete(t.values, key)
+}
+
 func (t *mapTags) Values() map[string]interface{} {
 	return t.values
+}
+
+// represents a MIME-style key mapping
+func WithTagsMimeKey() MapTagsOption {
+	return MapTagsOptionFunc(func(t *mapTags) {
+		t.isMimeKey = true
+	})
 }
--- go/context/tags.nop.go
+++ go/context/tags.nop.go
@@ -2,14 +2,18 @@ package context
 
 type nopTags struct{}
 
-func (t *nopTags) Set(key string, value interface{}) Tags {
-	return t
+func (t *nopTags) Set(key string, value interface{}) {
+	return
 }
 
 func (t *nopTags) Get(key string) (interface{}, bool) {
 	return nil, false
 }
 
+// Del deletes the values associated with key.
+func (t *nopTags) Del(key string) {
+	return
+}
 func (t *nopTags) Values() map[string]interface{} {
 	return nil
 }
