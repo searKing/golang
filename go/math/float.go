35595d00a041b52702a83b48b56570ff5565cb23
陈海欣 <471030698@qq.com>
2019-10-17 10:52:20 +0800

    [ADD] math.TruncPrecision returns the float value of x, with the maximum n bits precision.

---
+++ go/math/float.go
@@ -0,0 +1,52 @@
+package math
+
+import "math"
+
+// TruncPrecision returns the float value of x, with the maximum n bits precision.
+//
+// Special cases are:
+//	Trunc(±0) = ±0
+//	Trunc(±Inf) = ±Inf
+//	Trunc(NaN) = NaN
+func TruncPrecision(x float64, n int) float64 {
+	n10 := math.Pow10(n)
+	return math.Copysign(math.Trunc((x+0.5/n10)*n10)/n10, x)
+}
+
+func Tolerance(a, b, e float64) bool {
+	// Multiplying by e here can underflow denormal values to zero.
+	// Check a==b so that at least if a and b are small and identical
+	// we say they match.
+	if a == b {
+		return true
+	}
+	d := a - b
+	if d < 0 {
+		d = -d
+	}
+
+	// note: b is correct (expected) value, a is actual value.
+	// make error tolerance a fraction of b, not a.
+	if b != 0 {
+		e = e * b
+		if e < 0 {
+			e = -e
+		}
+	}
+	return d < e
+}
+func Close(a, b float64) bool { return Tolerance(a, b, 1e-14) }
+
+func VeryClose(a, b float64) bool { return Tolerance(a, b, 4e-16) }
+
+func SoClose(a, b, e float64) bool { return Tolerance(a, b, e) }
+
+func Alike(a, b float64) bool {
+	switch {
+	case math.IsNaN(a) && math.IsNaN(b):
+		return true
+	case a == b:
+		return math.Signbit(a) == math.Signbit(b)
+	}
+	return false
+}
---
+++ go/math/float_test.go
@@ -0,0 +1,72 @@
+package math_test
+
+import (
+	math_ "math"
+	"testing"
+
+	"github.com/searKing/golang/go/math"
+)
+
+type TruncPrecisionCaseTest struct {
+	input  float64
+	n      int
+	output float64
+}
+
+var (
+	truncPrecisionCaseTests = []TruncPrecisionCaseTest{
+		{
+			input:  1234.567890,
+			n:      0,
+			output: 1235,
+		},
+		{
+			input:  1234.567890,
+			n:      1,
+			output: 1234.6,
+		},
+		{
+			input:  1234.567890,
+			n:      6,
+			output: 1234.56789,
+		},
+		{
+			input:  1234.567890,
+			n:      10,
+			output: 1234.56789,
+		},
+		{
+			input:  math_.Inf(-1),
+			n:      1,
+			output: math_.Inf(-1),
+		},
+		{
+			input:  math_.Copysign(0, -1),
+			n:      1,
+			output: math_.Copysign(0, -1),
+		},
+		{
+			input:  0,
+			n:      1,
+			output: 0,
+		},
+		{
+			input:  math_.Inf(1),
+			n:      1,
+			output: math_.Inf(1),
+		},
+		{
+			input:  math_.NaN(),
+			n:      1,
+			output: math_.NaN(),
+		},
+	}
+)
+
+func TestTruncPrecision(t *testing.T) {
+	for n, test := range truncPrecisionCaseTests {
+		if got := math.TruncPrecision(test.input, test.n); !math.Alike(got, test.output) {
+			t.Errorf("#%d: TruncPrecision(%g,%d) = %g, want %g", n, test.input, test.n, got, test.output)
+		}
+	}
+}
