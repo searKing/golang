1424701087d19b74194a8b84117c7c5b4a60b6a8
陈海欣 <471030698@qq.com>
2019-11-05 17:14:53 +0800

    [EXAMPLE] flag.StringSliceVar

--- go/flag/example_test.go
+++ go/flag/example_test.go
@@ -2,6 +2,7 @@ package flag_test
 
 import (
 	"flag"
+	"fmt"
 	"os"
 
 	flag_ "github.com/searKing/golang/go/flag"
@@ -15,8 +16,13 @@ func ExampleStringSliceVar() {
 	fs.SetOutput(os.Stdout)
 	flag_.StringSliceVarWithFlagSet(fs, &infos, "i", []string{"hello", "world"}, "info arrays")
 	fs.PrintDefaults()
+	fmt.Printf("infos before parse: %q\n", infos)
+	_ = fs.Parse([]string{"-i", "golang", "-i", "flag", "-i", "string slice"})
+	fmt.Printf("infos after parse: %q\n", infos)
 	// Output:
 	// -i value
 	//     	info arrays (default &["hello" "world"])
+	// infos before parse: ["hello" "world"]
+	// infos after parse: ["golang" "flag" "string slice"]
 
 }
