f8bce430fc0af36e023b7509fc8a195d3f8538c2
陈海欣 <471030698@qq.com>
2019-11-05 17:22:04 +0800

    [README] flag.StringSliceVar

--- go/flag/string_slice.go
+++ go/flag/string_slice.go
@@ -7,13 +7,13 @@ import (
 )
 
 // StringSliceVarWithFlagSet defines a []string flag with specified name, default value, and usage string.
-// The argument p points to a uint64 variable in which to store the value of the flag.
+// The argument p points to a []string variable in which to store the value of the flag.
 func StringSliceVarWithFlagSet(f *flag.FlagSet, p *[]string, name string, value []string, usage string) {
 	f.Var(newStringSliceValue(value, p), name, usage)
 }
 
 // StringSliceWithFlagSet defines an []string flag with specified name, default value, and usage string.
-// The return value is the address of an []string variable that stores the value of the flag.
+// The return value is the address of a []string variable that stores the value of the flag.
 func StringSliceWithFlagSet(f *flag.FlagSet, name string, value []string, usage string) *[]string {
 	p := new([]string)
 	StringSliceVarWithFlagSet(f, p, name, value, usage)
@@ -21,13 +21,13 @@ func StringSliceWithFlagSet(f *flag.FlagSet, name string, value []string, usage 
 }
 
 // StringSliceVar defines a []string flag with specified name, default value, and usage string.
-// The argument p points to a uint64 variable in which to store the value of the flag.
+// The argument p points to a []string variable in which to store the value of the flag.
 func StringSliceVar(p *[]string, name string, value []string, usage string) {
 	flag.CommandLine.Var(newStringSliceValue(value, p), name, usage)
 }
 
 // StringSlice defines an []string flag with specified name, default value, and usage string.
-// The return value is the address of an int variable that stores the value of the flag.
+// The return value is the address of a []string variable that stores the value of the flag.
 func StringSlice(name string, value []string, usage string) *[]string {
 	return StringSliceWithFlagSet(flag.CommandLine, name, value, usage)
 }
