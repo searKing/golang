df2d3f99285163ad428ec15438124c14744098ff
陈海欣 <471030698@qq.com>
2019-11-13 19:30:29 +0800

    [ADD] support backtrace logging for SIGXXX

---
+++ go/reflect/example_test.go
@@ -0,0 +1,31 @@
+package reflect
+
+import (
+	"fmt"
+	"os"
+	osexec "os/exec"
+	"testing"
+)
+
+func TestGetppid(t *testing.T) {
+
+	if os.Getenv("GO_WANT_HELPER_PROCESS") == "1" {
+		fmt.Print(os.Getppid())
+		os.Exit(0)
+	}
+
+	cmd := osexec.Command(os.Args[0], "-test.run=TestGetppid")
+	cmd.Env = append(os.Environ(), "GO_WANT_HELPER_PROCESS=1")
+
+	// verify that Getppid() from the forked process reports our process id
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatalf("Failed to spawn child process: %v %q", err, string(output))
+	}
+
+	childPpid := string(output)
+	ourPid := fmt.Sprintf("%d", os.Getpid())
+	if childPpid != ourPid {
+		t.Fatalf("Child process reports parent process id '%v', expected '%v'", childPpid, ourPid)
+	}
+}
