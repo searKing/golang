e8b5c71fcb0013ac5913942bf55ae4048872a5c1
陈海欣 <471030698@qq.com>
2019-11-16 19:06:16 +0800

    [CHG] auto compile for cpp & go

--- go/os/signal/signal_cgo.go
+++ go/os/signal/signal_cgo.go
@@ -2,32 +2,27 @@
 
 package signal
 
-
-/*
-#cgo pkg-config: ${SRCDIR}/cgo/pkgconfig/libsignal_cgo.pc
-#include <stdlib.h>  // Needed for C.free
-#include <stdio.h>
-#include "signal.cgo.h"
-*/
 import "C"
 import (
 	"os"
+
+	"github.com/searKing/golang/go/os/signal/cgo"
 )
 
 // signalAction act as signal.Notify, which invokes the Go signal handler.
 // https://godoc.org/os/signal#hdr-Go_programs_that_use_cgo_or_SWIG
 func signalAction(sigs ...os.Signal) {
 	for _, sig := range sigs {
-		C.CGOSignalHandlerSignalAction(C.int(Signum(sig)))
+		cgo.SignalAction(Signum(sig))
 	}
 }
 
 // signalDumpTo redirect log to fd, stdout if not set.
 func signalDumpTo(fd int) {
-	C.CGOSignalHandlerSetFd(C.int(fd))
+	cgo.SetFd(fd)
 }
 
 // dumpBacktrace enables|disables log of bt when signal is triggered, disable if not set.
 func dumpBacktrace(enable bool) {
-	C.CGOSignalHandlerSetBacktraceDump(C._Bool(enable))
+	cgo.SetBacktraceDump(enable)
 }
