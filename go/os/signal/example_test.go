898ad82d58962645a6b7aebb02d34161ac8497f3
陈海欣 <471030698@qq.com>
2019-11-25 21:22:42 +0800

    [CHG] testcase

--- go/os/signal/example_test.go
+++ go/os/signal/example_test.go
@@ -11,30 +11,20 @@ import (
 	signal_ "github.com/searKing/golang/go/os/signal"
 )
 
-func ExampleDumpSignalTo() {
+func ExampleNotify() {
 	signal_.DumpSignalTo(syscall.Stderr)
-	signal_.SetSigInvokeChain(syscall.SIGUSR1, syscall.SIGUSR2, 0, syscall.SIGSEGV)
+	signal_.SetSigInvokeChain(syscall.SIGUSR1, syscall.SIGUSR2, 0, syscall.SIGINT)
 
 	// Set up channel on which to send signal notifications.
 	// We must use a buffered channel or risk missing the signal
 	// if we're not ready to receive when the signal is sent.
 	c := make(chan os.Signal, 1)
 	//signal.Notify(c, syscall.SIGINT, syscall.SIGSEGV)
-	signal_.Notify(c, syscall.SIGUSR1, syscall.SIGUSR2, syscall.SIGSEGV)
+	signal_.Notify(c, syscall.SIGUSR1, syscall.SIGUSR2, syscall.SIGINT)
 
 	// simulate to send a SIGINT to this example test
 	go func() {
-		//_ = syscall.Kill(syscall.Getpid(), syscall.SIGQUIT)
-		//_ = syscall.Kill(syscall.Getpid(), syscall.SIGTERM)
-		//_ = syscall.Kill(syscall.Getpid(), syscall.SIGSEGV)
-		//_ = syscall.Kill(syscall.Getpid(), syscall.SIGFPE)
-		//_ = syscall.Kill(syscall.Getpid(), syscall.SIGSEGV)
-		//_ = syscall.Kill(syscall.Getpid(), syscall.SIGSEGV)
-		//internal.Raise(syscall.SIGSEGV)
-
-		time.Sleep(time.Second)
-		var err error
-		fmt.Println(err.Error())
+		_ = syscall.Kill(syscall.Getpid(), syscall.SIGINT)
 	}()
 
 	var wg sync.WaitGroup
@@ -59,18 +49,21 @@ func ExampleDumpSignalTo() {
 				} else {
 					fmt.Printf("Got signal: %s\n", s)
 				}
+				if s == syscall.SIGINT {
+					return
+				}
 
 			case <-time.After(time.Minute):
 				_, _ = fmt.Fprintf(os.Stderr, "time overseed:\n")
 				return
 			}
-			// set os.Stderr tp pass test, for the stacktrace is random.
-			//stderr prints something like:
-			//Signal received(2).
-			//Stacktrace dumped to file: stacktrace.dump.
-			//Previous run crashed:
-			// 0# searking::SignalHandler::operator()(int, __siginfo*, void*) in /private/var/folders/12/870qx8rd0_d96nt6g078wp080000gn/T/___ExampleSignalAction_in_github_com_searKing_golang_go_os_signal
-			// 1# _sigtramp in /usr/lib/system/libsystem_platform.dylib
+			// set os.Stderr to pass test, for the stacktrace is random.
+			// stderr prints something like:
+			// Signal received(2).
+			// Stacktrace dumped to file: stacktrace.dump.
+			// Previous run crashed:
+			//  0# searking::SignalHandler::operator()(int, __siginfo*, void*) in /private/var/folders/12/870qx8rd0_d96nt6g078wp080000gn/T/___ExampleSignalAction_in_github_com_searKing_golang_go_os_signal
+			//  1# _sigtramp in /usr/lib/system/libsystem_platform.dylib
 
 		}
 	}()
