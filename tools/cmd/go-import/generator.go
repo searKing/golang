b2af422e67a72cf9d2e733eec01bae8e85dc6034
陈海欣 <471030698@qq.com>
2019-12-05 18:53:57 +0800

    [CHG] add retry for amr decode, [0,6]

---
+++ tools/cmd/go-import/generator.go
@@ -0,0 +1,13 @@
+package main
+
+import "bytes"
+
+// Generator holds the state of the analysis. Primarily used to buffer
+// the output for format.Source.
+type Generator struct {
+	buf bytes.Buffer // Accumulated output.
+	pkg *Package     // Package we are scanning.
+
+	importPrefix string
+	lineComment  bool
+}
---
+++ tools/cmd/go-import/go.mod
@@ -0,0 +1,3 @@
+module github.com/searKing/golang/tools/cmd/go-import
+
+go 1.13
