193f559c38a8b38c4bccf092ca2348cc2fdd8cb7
searKing <searKingChan@gmail.com>
2020-04-03 01:37:45 +0800

    [CHG]refactor hash ring

--- go/container/hashring/hashring.go
+++ go/container/hashring/hashring.go
@@ -4,7 +4,7 @@
 
 // Package hashring provides a consistent hashing function.
 //
-// KetamaNodeLocator hashing is often used to distribute requests to a changing set of servers.  For example,
+// NodeLocator hashing is often used to distribute requests to a changing set of servers.  For example,
 // say you have some cache servers cacheA, cacheB, and cacheC.  You want to decide which cache server
 // to use to look up information on a user.
 //
@@ -21,10 +21,8 @@
 package hashring
 
 import (
-	"crypto/md5"
 	"fmt"
 	"math"
-	"net"
 	"sort"
 )
 
@@ -34,63 +32,56 @@ import (
 //			               ->     IterateKey    -> HashKey
 type Node interface {
 	// Get the SocketAddress of the server to which this node is connected.
-	GetSocketAddress() net.Addr
+	fmt.Stringer
 }
+
+const defaultNumReps = 160
 
 type StringNode string
 
-// name of the network (for example, "tcp", "udp")
-func (n StringNode) Network() string {
-	return "string"
-}
-
-// string form of address (for example, "192.0.2.1:25", "[2001:db8::1]:80")
 func (n StringNode) String() string {
 	return string(n)
 }
-func (n StringNode) GetSocketAddress() net.Addr {
-	return n
-}
 
-// KetamaNodeLocator holds the information about the allNodes of the consistent hash ketamaNodes.
-//go:generate go-option -type "KetamaNodeLocator"
-type KetamaNodeLocator struct {
+// NodeLocator holds the information about the allNodes of the consistent hash nodes.
+//go:generate go-option -type "NodeLocator"
+type NodeLocator struct {
 	// The List of nodes to use in the Ketama consistent hash continuum
 	// 模拟一致性哈希环的环状结构，存放的都是可用节点
 	// 一致性Hash环
-	sortedKetamaHashRing Uint32Slice       // []HashKey, Index for ketamaNodes binary search
-	ketamaNodes          map[uint32]Node   // <HashKey,Node>
-	allNodes             map[Node]struct{} // <Node>
+	sortedHashes uint32Slice       // []HashKey, Index for nodes binary search
+	nodes        map[uint32]Node   // <HashKey,Node>
+	allNodes     map[Node]struct{} // <Node>
 
 	// The hash algorithm to use when choosing a node in the Ketama consistent hash continuum
 	hashAlg HashAlgorithm
 
 	// node weights for ketama, a map from InetSocketAddress to weight as Integer
-	weights          map[net.Addr]int
-	isWeightedKetama bool
+	weights    map[Node]int
+	isWeighted bool
 
 	// the number of discrete hashes that should be defined for each node in the continuum.
 	numReps int
 	// the format used to name the nodes in Ketama, either SpyMemcached or LibMemcached
-	ketamaNodeKeyFormatter *KetamaNodeKeyFormatter
+	nodeKeyFormatter *KetamaNodeKeyFormatter
 }
 
 // New creates a hash ring of n replicas for each entry.
-func New(opts ...KetamaNodeLocatorOption) *KetamaNodeLocator {
-	r := &KetamaNodeLocator{
-		ketamaNodes:            make(map[uint32]Node),
-		allNodes:               make(map[Node]struct{}),
-		hashAlg:                KetamaHash,
-		weights:                make(map[net.Addr]int),
-		numReps:                160,
-		ketamaNodeKeyFormatter: NewKetamaNodeKeyFormatter(SpyMemcached),
+func New(opts ...NodeLocatorOption) *NodeLocator {
+	r := &NodeLocator{
+		nodes:            make(map[uint32]Node),
+		allNodes:         make(map[Node]struct{}),
+		hashAlg:          KetamaHash,
+		weights:          make(map[Node]int),
+		numReps:          defaultNumReps,
+		nodeKeyFormatter: NewKetamaNodeKeyFormatter(SpyMemcached),
 	}
 	r.ApplyOptions(opts...)
 	return r
 }
 
 // GetAllNodes returns all available nodes
-func (c *KetamaNodeLocator) GetAllNodes() []Node {
+func (c *NodeLocator) GetAllNodes() []Node {
 	var nodes []Node
 	for node, _ := range c.allNodes {
 		nodes = append(nodes, node)
@@ -99,22 +90,26 @@ func (c *KetamaNodeLocator) GetAllNodes() []Node {
 }
 
 // GetPrimaryNode returns the first available node for a name, such as “127.0.0.1:11311-0” for "Alice"
-func (c *KetamaNodeLocator) GetPrimaryNode(name string) (Node, bool) {
+func (c *NodeLocator) GetPrimaryNode(name string) (Node, bool) {
 	return c.getNodeForHashKey(c.getHashKey(name))
 }
 
 // GetMaxHashKey returns the last available node's HashKey
 // that is, Maximum HashKey in the Hash Cycle
-func (c *KetamaNodeLocator) GetMaxHashKey() (uint32, error) {
-	if len(c.sortedKetamaHashRing) == 0 {
+func (c *NodeLocator) GetMaxHashKey() (uint32, error) {
+	if len(c.sortedHashes) == 0 {
 		return 0, fmt.Errorf("NoSuchElementException")
 	}
-	return c.sortedKetamaHashRing[len(c.sortedKetamaHashRing)-1], nil
+	return c.sortedHashes[len(c.sortedHashes)-1], nil
 }
 
 // getNodeForHashKey returns the first available node since iterateHashKey, such as HASH(“127.0.0.1:11311-0”)
-func (c *KetamaNodeLocator) getNodeForHashKey(hash uint32) (Node, bool) {
-	rv, has := c.getKetamaNodes()[hash]
+func (c *NodeLocator) getNodeForHashKey(hash uint32) (Node, bool) {
+	if len(c.sortedHashes) == 0 {
+		return nil, false
+	}
+
+	rv, has := c.getNodes()[hash]
 	if has {
 		return rv, true
 	}
@@ -123,72 +118,70 @@ func (c *KetamaNodeLocator) getNodeForHashKey(hash uint32) (Node, bool) {
 		firstKey = 0
 	}
 
-	hash = c.sortedKetamaHashRing[firstKey]
-	rv, has = c.getKetamaNodes()[hash]
+	hash = c.sortedHashes[firstKey]
+	rv, has = c.getNodes()[hash]
 	return rv, has
 }
 
 // 根据输入物理节点列表，重新构造Hash环，即虚拟节点环
 // updateLocator reconstructs the hash ring with the input nodes
-func (c *KetamaNodeLocator) updateLocator(nodes ...Node) {
-	c.allNodes = make(map[Node]struct{})
+func (c *NodeLocator) updateLocator(nodes ...Node) {
+	allNodes := make(map[Node]struct{})
 	for _, node := range nodes {
-		c.allNodes[node] = struct{}{}
+		allNodes[node] = struct{}{}
 	}
-	c.SetKetamaNodes(nodes...)
+	c.allNodes = allNodes
+	c.SetNodes(nodes...)
 }
 
 // GetNodeRepetitions returns the number of discrete hashes that should be defined for each node
 // in the continuum.
-func (c *KetamaNodeLocator) getNodeRepetitions() int {
+func (c *NodeLocator) getNodeRepetitions() int {
 	return c.numReps
 }
 
-// getKetamaNodes returns the ketamaNodes
-func (c *KetamaNodeLocator) getKetamaNodes() map[uint32]Node {
-	return c.ketamaNodes
+// getNodes returns the nodes
+func (c *NodeLocator) getNodes() map[uint32]Node {
+	return c.nodes
 }
 
-// SetKetamaNodes setups the KetamaNodeLocator with the list of nodes it should use.
+// SetNodes setups the NodeLocator with the list of nodes it should use.
 // If there are existing nodes not present in nodes, they will be removed.
-// @param nodes a List of Nodes for this KetamaNodeLocator to use in
+// @param nodes a List of Nodes for this NodeLocator to use in
 // its continuum
-func (c *KetamaNodeLocator) SetKetamaNodes(nodes ...Node) {
-	if c.isWeightedKetama {
-		c.setWeightKetamaNodes(nodes...)
+func (c *NodeLocator) SetNodes(nodes ...Node) {
+	if c.isWeighted {
+		c.setWeightNodes(nodes...)
 		return
 	}
-	c.setNoWeightKetamaNodes(nodes...)
+	c.setNoWeightNodes(nodes...)
 }
 
-func (c *KetamaNodeLocator) setNoWeightKetamaNodes(nodes ...Node) {
+func (c *NodeLocator) setNoWeightNodes(nodes ...Node) {
 	// Set sets all the elements in the hash.
 	// If there are existing elements not present in nodes, they will be removed.
 	var nodesToBeRemoved []Node
 	// remove missing Nodes
 	for k := range c.allNodes {
-		found := false
 		for _, v := range nodes {
-			if k.GetSocketAddress().String() == v.GetSocketAddress().String() {
-				found = true
+			if k.String() == v.String() {
+				// found
 				break
 			}
 		}
-		if !found {
-			nodesToBeRemoved = append(nodesToBeRemoved, k)
-		}
+		nodesToBeRemoved = append(nodesToBeRemoved, k)
 	}
 	if len(nodesToBeRemoved) == len(nodes) {
-		c.RemoveAllKetamaNodes()
+		c.RemoveAllNodes()
 	} else {
-		c.removeNoWeightKetamaNodes(nodesToBeRemoved...)
+		c.removeNoWeightNodes(nodesToBeRemoved...)
 	}
 	// add all missing elements present in nodes.
 	var nodesToBeAdded []Node
 	for _, k := range nodes {
 		found := false
 		for v := range c.allNodes {
-			if k.GetSocketAddress().String() == v.GetSocketAddress().String() {
+			if k.String() == v.String() {
 				found = true
 				break
 			}
@@ -197,161 +190,167 @@ func (c *KetamaNodeLocator) setNoWeightKetamaNodes(nodes ...Node) {
 			nodesToBeAdded = append(nodesToBeAdded, k)
 		}
 	}
-	c.addNoWeightKetamaNodes(nodesToBeAdded...)
+	c.addNoWeightNodes(nodesToBeAdded...)
 }
 
-func (c *KetamaNodeLocator) setWeightKetamaNodes(nodes ...Node) {
-	c.RemoveAllKetamaNodes()
+func (c *NodeLocator) setWeightNodes(nodes ...Node) {
+	c.RemoveAllNodes()
 	numReps := c.getNodeRepetitions()
 	nodeCount := len(nodes)
 	totalWeight := 0
 
 	for _, node := range nodes {
-		totalWeight += c.weights[node.GetSocketAddress()]
+		totalWeight += c.weights[node]
 	}
 
 	// add all elements present in nodes.
 	for _, node := range nodes {
-		thisWeight := c.weights[node.GetSocketAddress()]
+		thisWeight := c.weights[node]
 		percent := float64(thisWeight) / float64(totalWeight)
-		// floor(percent * numReps / 4 * nodeCount + 1e10) * 4
-		pointerPerServer := (int)((math.Floor(percent*(float64(numReps))/4*float64(nodeCount) + 0.0000000001)) * 4)
-		for i := 0; i < pointerPerServer/4; i++ {
-			// batch size is 4 = SIZE(CRC)/SIZE(uint32)=16B/4B
-			for _, position := range c.ketamaNodePositionsAtIteration(node, i) {
-				c.getKetamaNodes()[position] = node
-				fmt.Printf("adding node %s with weight %d in position %d\n", node, thisWeight, position)
-			}
-		}
+		// floor(percent * numReps * nodeCount + 1e10)
+		pointerPerServer := (int)(math.Floor(percent*(float64(numReps))*float64(nodeCount) + 0.0000000001))
+		c.addNodeWithoutSort(node, pointerPerServer)
 	}
 
-	for _, node := range nodes {
-		c.allNodes[node] = struct{}{}
-	}
-	c.updateSortedKetamaNodes()
+	c.updateSortedNodes()
 }
 
-// RemoveAllKetamaNodes removes all nodes in the continuum....
-func (c *KetamaNodeLocator) RemoveAllKetamaNodes() {
-	c.sortedKetamaHashRing = nil
-	c.ketamaNodes = make(map[uint32]Node)
+// RemoveAllNodes removes all nodes in the continuum....
+func (c *NodeLocator) RemoveAllNodes() {
+	c.sortedHashes = nil
+	c.nodes = make(map[uint32]Node)
 	c.allNodes = make(map[Node]struct{})
 }
 
-// AddKetamaNodes inserts nodes into the consistent hash cycle.
-func (c *KetamaNodeLocator) AddKetamaNodes(nodes ...Node) {
-	if c.isWeightedKetama {
-		c.addWeightKetamaNodes(nodes...)
+// AddNodes inserts nodes into the consistent hash cycle.
+func (c *NodeLocator) AddNodes(nodes ...Node) {
+	if c.isWeighted {
+		c.addWeightNodes(nodes...)
 		return
 	}
-	c.addNoWeightKetamaNodes(nodes...)
+	c.addNoWeightNodes(nodes...)
 }
 
-func (c *KetamaNodeLocator) addWeightKetamaNodes(nodes ...Node) {
-	c.setWeightKetamaNodes(append(c.GetAllNodes(), nodes...)...)
+func (c *NodeLocator) addWeightNodes(nodes ...Node) {
+	c.setWeightNodes(append(c.GetAllNodes(), nodes...)...)
 }
 
-func (c *KetamaNodeLocator) addNoWeightKetamaNodes(nodes ...Node) {
+func (c *NodeLocator) addNoWeightNodes(nodes ...Node) {
 	numReps := c.getNodeRepetitions()
 
 	for _, node := range nodes {
-		// Ketama does some special work with md5 where it reuses chunks.
-		// Check to be backwards compatible, the hash algorithm does not
-		// matter for Ketama, just the placement should always be done using
-		// MD5
+		c.addNodeWithoutSort(node, numReps)
+	}
 
-		// KETAMA_HASH, Special Case, batch mode to speedup
+	c.updateSortedNodes()
+}
 
-		for i := 0; i < numReps; {
-			positions := c.getIterateHashKeyForNode(node, i)
-			if len(positions) == 0 {
-				i++ // ignore no hash node
+func (c *NodeLocator) addNodeWithoutSort(node Node, numReps int) {
+	// Ketama does some special work with md5 where it reuses chunks.
+	// Check to be backwards compatible, the hash algorithm does not
+	// matter for Ketama, just the placement should always be done using
+	// MD5
+
+	// KETAMA_HASH, Special Case, batch mode to speedup
+
+	for i := 0; i < numReps; {
+		positions := c.getIterateHashKeyForNode(node, i)
+		if len(positions) == 0 {
+			numReps++
+			i++ // ignore no hash node
+			break
+		}
+
+		for j, pos := range positions {
+			if i+j > numReps { // out of bound
 				break
 			}
-
-			kBound := len(positions)
-			if kBound+i > numReps { // bound check [0, numReps)
-				kBound = numReps - i
+			if _, has := c.getNodes()[pos]; has {
+				// skip this node, duplicated
+				numReps++
+				continue
 			}
-			// [0, kBound)
-			for k, pos := range positions {
-				if k >= kBound {
-					break // bound check
-				}
-				c.getKetamaNodes()[pos] = node
-			}
-			i += kBound
+			c.getNodes()[pos] = node
 		}
+		i += len(positions)
 	}
 
-	for _, node := range nodes {
-		c.allNodes[node] = struct{}{}
-	}
-	c.updateSortedKetamaNodes()
+	c.allNodes[node] = struct{}{}
 }
 
 // Remove removes nodes from the consistent hash cycle...
-func (c *KetamaNodeLocator) RemoveKetamaNodes(nodes ...Node) {
-	if c.isWeightedKetama {
-		c.removeWeightKetamaNodes(nodes...)
+func (c *NodeLocator) RemoveNodes(nodes ...Node) {
+	if c.isWeighted {
+		c.removeWeightNodes(nodes...)
 		return
 	}
-	c.removeNoWeightKetamaNodes(nodes...)
+	c.removeNoWeightNodes(nodes...)
 }
 
-func (c *KetamaNodeLocator) removeWeightKetamaNodes(nodes ...Node) {
-	c.setWeightKetamaNodes(c.GetAllNodes()...)
+func (c *NodeLocator) removeWeightNodes(nodes ...Node) {
+	for _, node := range nodes {
+		delete(c.allNodes, node)
+	}
+	c.setWeightNodes(c.GetAllNodes()...)
 }
 
-func (c *KetamaNodeLocator) removeNoWeightKetamaNodes(nodes ...Node) {
+func (c *NodeLocator) removeNoWeightNodes(nodes ...Node) {
 	numReps := c.getNodeRepetitions()
 
 	for _, node := range nodes {
-		for i := 0; i < numReps; i++ {
+		for i := 0; i < numReps; {
+			positions := c.getIterateHashKeyForNode(node, i)
+			if len(positions) == 0 {
+				// ignore no hash node
+				numReps++
+				i++
+				continue
+			}
 
-			for i := 0; i < numReps; {
-				positions := c.getIterateHashKeyForNode(node, i)
-				if len(positions) == 0 {
-					i++ // ignore no hash node
-					continue
+			for j, pos := range positions {
+				if i+j > numReps { // out of bound
+					break
 				}
-				i += len(positions)
-				for _, pos := range positions {
-					if n, has := c.ketamaNodes[pos]; has && n == node {
-						delete(c.ketamaNodes, pos)
+				if n, has := c.nodes[pos]; has {
+					if n.String() != node.String() {
+						numReps++ // ignore no hash node
+						continue
 					}
+					delete(c.nodes, pos)
 				}
 			}
+			i += len(positions)
 		}
 		delete(c.allNodes, node)
 	}
-	c.updateSortedKetamaNodes()
+	c.updateSortedNodes()
 }
 
 // tailSearch returns the first available node since iterateHashKey's Index, such as Index(HASH(“127.0.0.1:11311-0”))
-func (c *KetamaNodeLocator) tailSearch(key uint32) (i int, found bool) {
+func (c *NodeLocator) tailSearch(key uint32) (i int, found bool) {
 	found = true
 	f := func(x int) bool {
-		return c.sortedKetamaHashRing[x] >= key
+		return c.sortedHashes[x] >= key
 	}
-	i = sort.Search(len(c.sortedKetamaHashRing), f)
-	if i >= len(c.sortedKetamaHashRing) {
+	// Search uses binary search to find and return the smallest index since iterateHashKey's Index
+	i = sort.Search(len(c.sortedHashes), f)
+	if i >= len(c.sortedHashes) {
 		found = false
 	}
 	return
 }
 
-// Get returns an element close to where name hashes to in the ketamaNodes.
-func (c *KetamaNodeLocator) Get(name string) (Node, bool) {
-	if len(c.ketamaNodes) == 0 {
+// Get returns an element close to where name hashes to in the nodes.
+func (c *NodeLocator) Get(name string) (Node, bool) {
+	if len(c.nodes) == 0 {
 		return nil, false
 	}
 	return c.GetPrimaryNode(name)
 }
 
-// GetTwo returns the two closest distinct elements to the name input in the ketamaNodes.
-func (c *KetamaNodeLocator) GetTwo(name string) (Node, Node, bool) {
-	if len(c.getKetamaNodes()) == 0 {
+// GetTwo returns the two closest distinct elements to the name input in the nodes.
+func (c *NodeLocator) GetTwo(name string) (Node, Node, bool) {
+	if len(c.getNodes()) == 0 {
 		return nil, nil, false
 	}
 	key := c.getHashKey(name)
@@ -359,7 +358,7 @@ func (c *KetamaNodeLocator) GetTwo(name string) (Node, Node, bool) {
 	if !found {
 		firstKey = 0
 	}
-	firstNode, has := c.getKetamaNodes()[c.sortedKetamaHashRing[firstKey]]
+	firstNode, has := c.getNodes()[c.sortedHashes[firstKey]]
 
 	if len(c.allNodes) == 1 {
 		return firstNode, nil, has
@@ -368,25 +367,25 @@ func (c *KetamaNodeLocator) GetTwo(name string) (Node, Node, bool) {
 	start := firstKey
 	var secondNode Node
 	for i := start + 1; i != start; i++ {
-		if i >= len(c.sortedKetamaHashRing) {
+		if i >= len(c.sortedHashes) {
 			i = 0
 		}
-		secondNode = c.getKetamaNodes()[c.sortedKetamaHashRing[i]]
-		if secondNode != firstNode {
+		secondNode = c.getNodes()[c.sortedHashes[i]]
+		if secondNode.String() != firstNode.String() {
 			break
 		}
 	}
 	return firstNode, secondNode, true
 }
 
-// GetN returns the N closest distinct elements to the name input in the ketamaNodes.
-func (c *KetamaNodeLocator) GetN(name string, n int) ([]Node, bool) {
-	if len(c.getKetamaNodes()) == 0 {
+// GetN returns the N closest distinct elements to the name input in the nodes.
+func (c *NodeLocator) GetN(name string, n int) ([]Node, bool) {
+	if len(c.getNodes()) == 0 {
 		return nil, false
 	}
 
-	if len(c.getKetamaNodes()) < n {
-		n = len(c.getKetamaNodes())
+	if len(c.getNodes()) < n {
+		n = len(c.getNodes())
 	}
 
 	key := c.getHashKey(name)
@@ -394,7 +393,7 @@ func (c *KetamaNodeLocator) GetN(name string, n int) ([]Node, bool) {
 	if !found {
 		firstKey = 0
 	}
-	firstNode, has := c.getKetamaNodes()[c.sortedKetamaHashRing[firstKey]]
+	firstNode, has := c.getNodes()[c.sortedHashes[firstKey]]
 
 	nodes := make([]Node, 0, n)
 	nodes = append(nodes, firstNode)
@@ -406,10 +405,10 @@ func (c *KetamaNodeLocator) GetN(name string, n int) ([]Node, bool) {
 	start := firstKey
 	var secondNode Node
 	for i := start + 1; i != start; i++ {
-		if i >= len(c.sortedKetamaHashRing) {
+		if i >= len(c.sortedHashes) {
 			i = 0
 		}
-		secondNode = c.getKetamaNodes()[c.sortedKetamaHashRing[i]]
+		secondNode = c.getNodes()[c.sortedHashes[i]]
 		if !sliceContainsMember(nodes, secondNode) {
 			nodes = append(nodes, secondNode)
 		}
@@ -421,31 +420,23 @@ func (c *KetamaNodeLocator) GetN(name string, n int) ([]Node, bool) {
 	return nodes, true
 }
 
-// 16B of md5
-// 根据key生成16位的MD5摘要， 因此digest数组共16位
-func (c *KetamaNodeLocator) computeMd5(key string) []byte {
-	h := md5.New()
-	h.Write([]byte(key))
-	return h.Sum(nil)
-}
-
-func (c *KetamaNodeLocator) updateSortedKetamaNodes() {
-	hashes := c.sortedKetamaHashRing[:0]
+func (c *NodeLocator) updateSortedNodes() {
+	hashes := c.sortedHashes[:0]
 	// reallocate if we're holding on to too much (1/4th)
-	// len(ketamaNodes) * replicas < cap / 4
-	if cap(c.sortedKetamaHashRing)/(c.numReps*4) > len(c.ketamaNodes) {
+	// len(nodes) * replicas < cap / 4
+	if cap(c.sortedHashes)/(c.numReps*4) > len(c.nodes) {
 		hashes = nil
 	}
-	for k := range c.ketamaNodes {
+	for k := range c.nodes {
 		hashes = append(hashes, k)
 	}
 	sort.Sort(hashes)
-	c.sortedKetamaHashRing = hashes
+	c.sortedHashes = hashes
 }
 
 func sliceContainsMember(set []Node, member Node) bool {
 	for _, m := range set {
-		if m == member {
+		if m.String() == member.String() {
 			return true
 		}
 	}
--- go/container/hashring/sort.go
+++ go/container/hashring/sort.go
@@ -9,11 +9,11 @@ import "sort"
 // Convenience types for common cases
 
 // IntSlice attaches the methods of Interface to []int, sorting in increasing order.
-type Uint32Slice []uint32
+type uint32Slice []uint32
 
-func (p Uint32Slice) Len() int           { return len(p) }
-func (p Uint32Slice) Less(i, j int) bool { return p[i] < p[j] }
-func (p Uint32Slice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p uint32Slice) Len() int           { return len(p) }
+func (p uint32Slice) Less(i, j int) bool { return p[i] < p[j] }
+func (p uint32Slice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
 
 // Sort is a convenience method.
-func (p Uint32Slice) Sort() { sort.Sort(p) }
+func (p uint32Slice) Sort() { sort.Sort(p) }
