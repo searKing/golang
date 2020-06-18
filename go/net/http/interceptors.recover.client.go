509ffe7fbbc733ba0dd83814cdea11906262e2fc
searKing <searKingChan@gmail.com>
2020-06-19 03:09:39 +0800

    [CHG] refactor

--- go/net/http/interceptors.recover.client.go
+++ go/net/http/interceptors.recover.client.go
@@ -2,19 +2,17 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package recovery
+package http
 
 import (
 	"io"
 	"net/http"
-
-	http_ "github.com/searKing/golang/go/net/http"
 )
 
-// ClientInterceptor returns a new client interceptors with recovery from panic.
+// RecoveryClientInterceptor returns a new client interceptors with recovery from panic.
 // affect as recover{f()}; next()
-func ClientInterceptor(next http_.RoundTripHandler, out io.Writer, f func(resp *http.Response, req *http.Request, err interface{})) http_.RoundTripHandler {
-	return http_.RoundTripFunc(func(req *http.Request) (resp *http.Response, err error) {
+func RecoveryClientInterceptor(next RoundTripHandler, out io.Writer, f func(resp *http.Response, req *http.Request, err interface{})) RoundTripHandler {
+	return RoundTripFunc(func(req *http.Request) (resp *http.Response, err error) {
 		defer func() {
 			Recover(out, req, func(err interface{}) interface{} {
 				if f == nil {
