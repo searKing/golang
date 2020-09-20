656ebdae549025f865eb588bc0727bb4588534f8
haixinchen <haixinchen@tencent.com>
2020-09-20 19:49:42 +0800

    [ADD] os.TouchAll and CreateAll

---
+++ go/os/file_test.go
@@ -0,0 +1,36 @@
+package os_test
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	os_ "github.com/searKing/golang/go/os"
+)
+
+// tmpDir creates a temporary directory and returns its name.
+func tmpFile(t *testing.T) string {
+	tmp, err := ioutil.TempFile("", "")
+	if err != nil {
+		t.Fatalf("temp file creation failed: %v", err)
+	}
+	defer os.Remove(tmp.Name())
+	defer tmp.Close()
+	return tmp.Name()
+}
+
+func TestCreateAll(t *testing.T) {
+	tmp := tmpFile(t)
+	if err := os_.CreateAll(tmp); err != nil {
+		t.Fatalf("temp file CreateAll failed: %v", err)
+	}
+	defer os.Remove(tmp)
+}
+
+func TestTouchAll(t *testing.T) {
+	tmp := tmpFile(t)
+	if err := os_.TouchAll(tmp); err != nil {
+		t.Fatalf("temp file TouchAll failed: %v", err)
+	}
+	defer os.Remove(tmp)
+}
