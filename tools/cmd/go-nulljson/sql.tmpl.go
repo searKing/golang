9526c1598c388cc9d7c57557cfa15e84c1bf611b
haixinchen <haixinchen@tencent.com>
2020-09-23 13:00:19 +0800

    [ADD] support nullable params, generate nullable sql field, similar to sql.NullString

---
+++ tools/cmd/go-nulljson/sql.tmpl.go
@@ -0,0 +1,10 @@
+package main
+
+//	SqlJsonType: NullJson type name
+//	ValueType: value type name
+//	NilValue: nil value of map type
+type SqlRender struct {
+	SqlJsonType string // NullJson type name
+	ValueType   string // value type name
+	NilValue    string // nil value of map type
+}
