c673c07f66000105252087d73f3562e5dbd48bb8
haixinchen <haixinchen@tencent.com>
2020-09-30 15:47:50 +0800

    [CHG] ShrinkEmptyColumns

--- third_party/github.com/jmoiron/sqlx/named_parameters_table.go
+++ third_party/github.com/jmoiron/sqlx/named_parameters_table.go
@@ -31,7 +31,7 @@ func NamedTableSelectArguments(table string, cols ...string) (arguments string) 
 // for SQL INSERT statements based on columns.
 //
 //	query := fmt.Sprintf("INSERT INTO table %s", NamedTableInsertArgumentsCombined("table", "foo", "bar"))
-//	// INSERT INTO table (table.foo, table.bar) VALUES (:foo, :bar)
+//	// INSERT INTO table (table.foo, table.bar) VALUES (:table.foo, :table.bar)
 //
 //	query := fmt.Sprintf("INSERT INTO table %s", NamedTableInsertArgumentsCombined("table"))
 //	// INSERT INTO table (foo, bar) DEFAULT VALUES
@@ -45,24 +45,24 @@ func NamedTableInsertArgumentsCombined(table string, cols ...string) (arguments 
 	if len(cols) == 0 {
 		return `VALUES(DEFAULT)`
 	}
-	return fmt.Sprintf(`(%s) VALUES (%s)`, JoinTableColumns(table, cols...), JoinNamedTableValues(cols...))
+	return fmt.Sprintf(`(%s) VALUES (%s)`, JoinTableColumns(table, cols...), JoinNamedTableValues(table, cols...))
 }
 
 // NamedTableInsertArguments returns columns and arguments for SQL INSERT statements based on columns.
 //
 //	columns, arguments := NamedTableInsertArguments("table", "foo", "bar")
 //	query := fmt.Sprintf("INSERT INTO table (%s) VALUES (%s)", columns, arguments)
-//	// INSERT INTO table (table.foo, table.bar) VALUES (:foo, :bar)
+//	// INSERT INTO table (table.foo, table.bar) VALUES (:table.foo, :table.bar)
 // Deprecated: Use NamedTableInsertArgumentsCombined instead.
 func NamedTableInsertArguments(table string, cols ...string) (columns string, arguments string) {
-	return JoinTableColumns(table, cols...), JoinNamedTableValues(cols...)
+	return JoinTableColumns(table, cols...), JoinNamedTableValues(table, cols...)
 }
 
 // NamedTableUpdateArguments returns columns and arguments for SQL UPDATE statements based on columns.
 //
 //	statement := NamedTableUpdateArguments("table", "foo", "bar")
 //	query := fmt.Sprintf("UPDATE table SET %s", statement)
-//	// UPDATE foo SET table.foo=:foo, table.bar=:bar
+//	// UPDATE foo SET table.foo=:table.foo, table.bar=:table.bar
 func NamedTableUpdateArguments(table string, cols ...string) (arguments string) {
 	return JoinNamedTableColumnsValues(table, cols...)
 }
@@ -70,7 +70,7 @@ func NamedTableUpdateArguments(table string, cols ...string) (arguments string) 
 // NamedTableWhereArguments returns conditions for SQL WHERE statements based on columns.
 //
 //	query := fmt.Sprintf("SELECT * FROM table WHERE %s", NamedTableWhereArguments(SqlCompareEqual, SqlOperatorAnd, "table", "foo", "bar"))
-//	// SELECT * FROM table WHERE table.foo=:foo AND table.bar=:bar
+//	// SELECT * FROM table WHERE table.foo=:table.foo AND table.bar=:table.bar
 //
 //	query := fmt.Sprintf("SELECT * FROM table WHERE %s", NamedTableWhereArguments(SqlCompareEqual, SqlOperatorAnd, "table"))
 //	// SELECT * FROM table WHERE TRUE
