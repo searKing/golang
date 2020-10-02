6248213efc15c95fcde7a667e2a6fab5386cd53f
haixinchen <haixinchen@tencent.com>
2020-10-02 20:13:35 +0800

    [CHG] add perm

--- go/os/file.go
+++ go/os/file.go
@@ -39,15 +39,15 @@ func ChtimesNow(name string) error {
 // TouchAll creates the named file or dir. If the file already exists,
 // it is touched to now. If the file does not exist, it is created with mode 0666 (before umask).
 // If the dir does not exist, it is created with mode 0666 (before umask).
-func TouchAll(path string) error {
-	return createAll(path, true, false)
+func TouchAll(path string, perm os.FileMode) error {
+	return createAll(path, perm, true, false)
 }
 
 // CreateAll creates or truncates the named file or dir. If the file already exists,
 // it is truncated. If the file does not exist, it is created with mode 0666 (before umask).
 // If the dir does not exist, it is created with mode 0666 (before umask).
-func CreateAll(path string) error {
-	return createAll(path, false, true)
+func CreateAll(path string, perm os.FileMode) error {
+	return createAll(path, perm, false, true)
 }
 
 // CreateAllIfNotExist creates the named file or dir. If the file does not exist, it is created
@@ -55,13 +55,11 @@ func CreateAll(path string) error {
 // If the dir does not exist, it is created with mode 0666 (before umask).
 // If path is already a directory, CreateAllIfNotExist does nothing
 // and returns nil.
-func CreateAllIfNotExist(path string) error {
-	return createAll(path, false, false)
+func CreateAllIfNotExist(path string, perm os.FileMode) error {
+	return createAll(path, perm, false, false)
 }
 
-func createAll(path string, touch bool, truncate bool) error {
-	var perm os.FileMode = 0666
-
+func createAll(path string, perm os.FileMode, touch bool, truncate bool) error {
 	dir, file := filepath.Split(path)
 	// file or dir exists
 	if fi, err := os.Stat(path); err == nil {
