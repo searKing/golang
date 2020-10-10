44f6e62a214f0b545dad57c9ae1a27803516c840
haixinchen <haixinchen@tencent.com>
2020-10-10 19:49:13 +0800

    [CHG] order

--- third_party/github.com/jmoiron/sqlx/named_statements.go
+++ third_party/github.com/jmoiron/sqlx/named_statements.go
@@ -5,6 +5,25 @@ import (
 	"strings"
 )
 
+type InsertOption int
+
+const (
+	// INSERT inserts new rows into an existing table.
+	InsertOptionInsert InsertOption = iota
+	// REPLACE works exactly like INSERT, except that if an old row in the table has the same value as a
+	// new row for a PRIMARY KEY or a UNIQUE index, the old row is deleted before the new row is inserted.
+	InsertOptionReplace InsertOption = iota
+	// If you specify an ON DUPLICATE KEY UPDATE clause and a row to be inserted would cause a duplicate
+	// value in a UNIQUE index or PRIMARY KEY, an UPDATE of the old row occurs.
+	InsertOptionUpdate InsertOption = iota
+	// If you use the IGNORE modifier, ignorable errors that occur while executing the INSERT statement
+	// are ignored.
+	InsertOptionIgnore InsertOption = iota
+	// deprecated in MySQL 5.6. In MySQL 8.0, DELAYED is not supported
+	// Deprecated: Use InsertOptionInsert instead.
+	InsertOptionDelayed InsertOption = iota
+)
+
 // SimpleStatements is a simple render for simple SQL
 type SimpleStatements struct {
 	TableName      string
@@ -15,6 +34,10 @@ type SimpleStatements struct {
 	Limit          int         // take effect only in SELECT
 	Offset         int         // take effect only in SELECT
 	GroupByColumns []string    // take effect only in WHERE clause, that exists in SELECT, UPDATE, DELETE
+	OrderByColumns []string    // take effect only in WHERE clause, that exists in SELECT, UPDATE, DELETE
+
+	// INSERT
+	InsertOption InsertOption // take effect only in INSERT
 }
 
 // NamedSelectStatement returns a simple sql statement for SQL SELECT statements based on columns.
@@ -44,6 +67,9 @@ func (s SimpleStatements) NamedSelectStatement(appends ...string) string {
 	if len(s.GroupByColumns) > 0 {
 		query += fmt.Sprintf(" GROUP BY %s", JoinTableColumns(s.TableName, s.GroupByColumns...))
 	}
+	if len(s.OrderByColumns) > 0 {
+		query += fmt.Sprintf(" ORDER BY %s", JoinTableColumns(s.TableName, s.OrderByColumns...))
+	}
 	return query + " " + strings.Join(appends, "")
 }
 
@@ -72,15 +98,35 @@ func (s SimpleStatements) NamedSelectStatement(appends ...string) string {
 //
 //	// INSERT INTO foo DEFAULT VALUES
 func (s SimpleStatements) NamedInsertStatement(update bool, appends ...string) string {
+	insertOption := s.InsertOption
 	if len(s.Columns) > 0 && update {
+		insertOption = InsertOptionUpdate
+	}
+	switch insertOption {
+	case InsertOptionReplace:
+		return fmt.Sprintf(`REPLACE INTO %s %s`,
+			s.TableName,
+			NamedInsertArgumentsCombined(s.Columns...)) + " " + strings.Join(appends, "")
+	case InsertOptionUpdate:
 		return fmt.Sprintf(`INSERT INTO %s %s ON DUPLICATE KEY UPDATE %s`,
 			s.TableName,
 			NamedInsertArgumentsCombined(s.Columns...),
 			NamedUpdateArguments(s.Columns...)) + " " + strings.Join(appends, "")
+	case InsertOptionIgnore:
+		return fmt.Sprintf(`INSERT IGNORE INTO %s %s`,
+			s.TableName,
+			NamedInsertArgumentsCombined(s.Columns...)) + " " + strings.Join(appends, "")
+	case InsertOptionDelayed:
+		return fmt.Sprintf(`INSERT DELAYED INTO %s %s`,
+			s.TableName,
+			NamedInsertArgumentsCombined(s.Columns...)) + " " + strings.Join(appends, "")
+	case InsertOptionInsert:
+		fallthrough
+	default:
+		return fmt.Sprintf(`INSERT INTO %s %s`,
+			s.TableName,
+			NamedInsertArgumentsCombined(s.Columns...)) + " " + strings.Join(appends, "")
 	}
-	return fmt.Sprintf(`INSERT INTO %s %s`,
-		s.TableName,
-		NamedInsertArgumentsCombined(s.Columns...)) + " " + strings.Join(appends, "")
 }
 
 // NamedUpdateStatement returns a simple sql statement for SQL UPDATE statements based on columns.
