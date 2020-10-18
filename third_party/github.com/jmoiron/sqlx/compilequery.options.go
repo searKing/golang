35cda97bd80b4b005a8394a5722e8696c19326c7
haixinchen <haixinchen@tencent.com>
2020-10-19 03:46:09 +0800

    [ADD]WithCompileQueryOptionArgument keep column if argument by column name is not zero

--- third_party/github.com/jmoiron/sqlx/compilequery.options.go
+++ third_party/github.com/jmoiron/sqlx/compilequery.options.go
@@ -14,13 +14,18 @@ func WithCompileQueryOptionAliasWithSelect(aliasWithSelect bool) CompileQueryOpt
 	})
 }
 
-// WithCompileQueryOptionTrimByColumn trim node by column name
+// WithCompileQueryOptionArgument keep column if argument by column name is not zero
 // take effect in WHERE|INSERT|UPDATE, ignore if multi rows
+// nil: keep all
+// []string: keep if exist
+// map[string]{{value}} : keep if exist and none zero
+// struct{} tag is `db:"{{col_name}}"`: keep if exist and none zero
+//
 // `SELECT t.a, b FROM t WHERE first_name= :hehe AND middle_name=? OR last_name=?`
 // TO
 // `select t.a as t_a, b as b from t where first_name = :first_name or last_name = :last_name`,
-func WithCompileQueryOptionTrimByColumn(trimByColumn map[string]interface{}) CompileQueryOption {
+func WithCompileQueryOptionArgument(arg interface{}) CompileQueryOption {
 	return CompileQueryOptionFunc(func(opt *compileQuery) {
-		opt.TrimByColumn = trimByColumn
+		opt.Argument = arg
 	})
 }
--- third_party/github.com/jmoiron/sqlx/named.go
+++ third_party/github.com/jmoiron/sqlx/named.go
@@ -6,8 +6,14 @@ package sqlx
 
 import (
 	"errors"
+	"reflect"
 
+	reflect_ "github.com/searKing/golang/go/reflect"
 	"vitess.io/vitess/go/vt/sqlparser"
+)
+
+const (
+	SqlxTag = "db"
 )
 
 //go:generate go-option -type "compileQuery"
@@ -18,9 +24,13 @@ type compileQuery struct {
 	// `select t.a as t_a, b as b`,
 	AliasWithSelect bool
 
-	// trim node by column name
+	// Argument by column name
+	// nil: keep all
+	// []string: keep if exist
+	// map[string]interface{} : keep if exist and none zero
+	// struct{} tag is `db:"{{col_name}}"`: keep if exist and none zero
 	// take effect in WHERE|INSERT|UPDATE, ignore if multi rows
-	TrimByColumn map[string]interface{}
+	Argument interface{}
 }
 
 // CompileQuery compiles a unbound query (using the '?' bind var) into an NamedQuery.
@@ -40,7 +50,7 @@ func CompileQuery(sql string, opts ...CompileQueryOption) (query string, err err
 	if err != nil {
 		return "", err
 	}
-	err = NamedUnbindVars(stmts, opt.AliasWithSelect, opt.TrimByColumn)
+	err = NamedUnbindVars(stmts, opt.AliasWithSelect, opt.Argument)
 	if err != nil {
 		return "", err
 	}
@@ -60,14 +70,14 @@ func CompliantName(in string) string {
 
 // NamedUnbindVars rewrites unbind vars to named vars referenced in the statement.
 // Ideally, this should be done only once.
-func NamedUnbindVars(stmt sqlparser.Statement, withAlias bool, trimByColumn map[string]interface{}) error {
+func NamedUnbindVars(stmt sqlparser.Statement, withAlias bool, arg interface{}) error {
 	return sqlparser.Walk(func(node sqlparser.SQLNode) (kontinue bool, err error) {
 		switch node := node.(type) {
 		case *sqlparser.Where:
-			TrimWhere(node, trimByColumn)
+			TrimWhere(node, arg)
 			break
 		case *sqlparser.Update:
-			if err := TrimUpdate(node, trimByColumn); err != nil {
+			if err := TrimUpdate(node, arg); err != nil {
 				return false, err
 			}
 			break
@@ -136,7 +146,7 @@ func NamedUnbindVars(stmt sqlparser.Statement, withAlias bool, trimByColumn map[
 
 				val.Val = []byte(":" + buf.String())
 			}
-			if err := TrimInsert(node, trimByColumn); err != nil {
+			if err := TrimInsert(node, arg); err != nil {
 				return false, err
 			}
 
@@ -149,15 +159,15 @@ func NamedUnbindVars(stmt sqlparser.Statement, withAlias bool, trimByColumn map[
 	}, stmt)
 }
 
-func TrimWhere(where *sqlparser.Where, trim map[string]interface{}) {
-	if len(trim) == 0 {
+func TrimWhere(where *sqlparser.Where, arg interface{}) {
+	if arg == nil || isNilValue(arg) {
 		return
 	}
-	where.Expr = TrimExpr(where.Expr, trim)
+	where.Expr = TrimExpr(where.Expr, arg)
 }
 
-func TrimInsert(insert *sqlparser.Insert, trim map[string]interface{}) error {
-	if len(trim) == 0 {
+func TrimInsert(insert *sqlparser.Insert, arg interface{}) error {
+	if arg == nil || isNilValue(arg) {
 		return nil
 	}
 	colTuples := insert.Columns
@@ -190,7 +200,7 @@ func TrimInsert(insert *sqlparser.Insert, trim map[string]interface{}) error {
 			filteredVals = append(filteredVals, v)
 			continue
 		}
-		if _, has := trim[col.String()]; !has {
+		if exist(arg, col.String()) {
 			filteredCols = append(filteredCols, col)
 			filteredVals = append(filteredVals, v)
 			continue
@@ -219,7 +229,7 @@ func TrimInsert(insert *sqlparser.Insert, trim map[string]interface{}) error {
 		}
 		buf := sqlparser.NewTrackedBuffer(nil)
 		updateExpr.Name.Format(buf)
-		if _, has := trim[buf.String()]; !has {
+		if exist(arg, buf.String()) {
 			filteredUpdates = append(filteredUpdates, &sqlparser.UpdateExpr{
 				Name: updateExpr.Name,
 				Expr: updateExpr.Expr,
@@ -231,8 +241,8 @@ func TrimInsert(insert *sqlparser.Insert, trim map[string]interface{}) error {
 	return nil
 }
 
-func TrimUpdate(update *sqlparser.Update, trim map[string]interface{}) error {
-	if len(trim) == 0 {
+func TrimUpdate(update *sqlparser.Update, arg interface{}) error {
+	if arg == nil || isNilValue(arg) {
 		return nil
 	}
 
@@ -255,7 +265,8 @@ func TrimUpdate(update *sqlparser.Update, trim map[string]interface{}) error {
 		}
 		buf := sqlparser.NewTrackedBuffer(nil)
 		updateExpr.Name.Format(buf)
-		if _, has := trim[buf.String()]; !has {
+
+		if exist(arg, buf.String()) {
 			filteredUpdates = append(filteredUpdates, &sqlparser.UpdateExpr{
 				Name: updateExpr.Name,
 				Expr: updateExpr.Expr,
@@ -267,15 +278,15 @@ func TrimUpdate(update *sqlparser.Update, trim map[string]interface{}) error {
 	return nil
 }
 
-func TrimExpr(expr sqlparser.Expr, trimByColumn map[string]interface{}) sqlparser.Expr {
-	if len(trimByColumn) == 0 {
-		return expr
+func TrimExpr(expr sqlparser.Expr, arg interface{}) sqlparser.Expr {
+	if arg == nil || isNilValue(arg) {
+		return nil
 	}
 	switch expr := expr.(type) {
 	case *sqlparser.ComparisonExpr:
 		buf := sqlparser.NewTrackedBuffer(nil)
 		expr.Left.Format(buf)
-		if _, has := trimByColumn[buf.String()]; !has {
+		if exist(arg, buf.String()) {
 			return expr
 		}
 		val, ok := expr.Right.(*sqlparser.SQLVal)
@@ -285,11 +296,11 @@ func TrimExpr(expr sqlparser.Expr, trimByColumn map[string]interface{}) sqlparse
 		if val.Type != sqlparser.ValArg {
 			return expr
 		}
-		// trimByColumn this node
+		// arg this node
 		return nil
 	case *sqlparser.AndExpr:
-		rightExpr := TrimExpr(expr.Right, trimByColumn)
-		leftExpr := TrimExpr(expr.Left, trimByColumn)
+		rightExpr := TrimExpr(expr.Right, arg)
+		leftExpr := TrimExpr(expr.Left, arg)
 		if leftExpr == nil && rightExpr == nil {
 			return nil
 		}
@@ -303,8 +314,8 @@ func TrimExpr(expr sqlparser.Expr, trimByColumn map[string]interface{}) sqlparse
 		expr.Right = rightExpr
 		return expr
 	case *sqlparser.OrExpr:
-		rightExpr := TrimExpr(expr.Right, trimByColumn)
-		leftExpr := TrimExpr(expr.Left, trimByColumn)
+		rightExpr := TrimExpr(expr.Right, arg)
+		leftExpr := TrimExpr(expr.Left, arg)
 		if leftExpr == nil && rightExpr == nil {
 			return nil
 		}
@@ -318,8 +329,8 @@ func TrimExpr(expr sqlparser.Expr, trimByColumn map[string]interface{}) sqlparse
 		expr.Right = rightExpr
 		return expr
 	case *sqlparser.XorExpr:
-		rightExpr := TrimExpr(expr.Right, trimByColumn)
-		leftExpr := TrimExpr(expr.Left, trimByColumn)
+		rightExpr := TrimExpr(expr.Right, arg)
+		leftExpr := TrimExpr(expr.Left, arg)
 		if leftExpr == nil && rightExpr == nil {
 			return nil
 		}
@@ -333,7 +344,7 @@ func TrimExpr(expr sqlparser.Expr, trimByColumn map[string]interface{}) sqlparse
 		expr.Right = rightExpr
 		return expr
 	case *sqlparser.NotExpr:
-		expr.Expr = TrimExpr(expr.Expr, trimByColumn)
+		expr.Expr = TrimExpr(expr.Expr, arg)
 		if expr.Expr == nil {
 			return nil
 		}
@@ -341,4 +352,77 @@ func TrimExpr(expr sqlparser.Expr, trimByColumn map[string]interface{}) sqlparse
 	default:
 		return expr
 	}
+}
+
+// exist returns true if col exist and not zero
+func exist(arg interface{}, col string) bool {
+	if arg == nil {
+		return false
+	}
+	switch arg := arg.(type) {
+	case map[string]interface{}:
+		val, has := arg[col]
+		if !has {
+			return false
+		}
+		var zero = reflect_.IsZeroValue(reflect.ValueOf(val))
+		return !zero
+	case []string:
+		for _, t := range arg {
+			if t == col {
+				return true
+			}
+		}
+		return false
+	default:
+	}
+
+	val := reflect.ValueOf(arg)
+	t := val.Type()
+	if val.Kind() == reflect.Ptr {
+		if val.IsNil() { // create the structure if it's nil
+			s := reflect.New(val.Type().Elem())
+			val.Set(s)
+			val = s
+		}
+
+		val = val.Elem()
+		t = t.Elem()
+	}
+
+	if t.Kind() == reflect.Map {
+		for _, e := range val.MapKeys() {
+			if e.Kind() != reflect.String {
+				continue
+			}
+			if e.String() != col {
+				continue
+			}
+			var zero = reflect_.IsZeroValue(val.MapIndex(e))
+			return !zero
+		}
+		return false
+	}
+
+	if t.Kind() != reflect.Struct {
+		return false
+	}
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		// unwrap any payloads
+		if payload := field.Tag.Get(SqlxTag); payload != "" {
+			if payload == col {
+				var zero = reflect_.IsZeroValue(val.FieldByName(field.Name))
+				return !zero
+			}
+		}
+	}
+	return false
+}
+
+func isNilValue(i interface{}) bool {
+	valueOf := reflect.ValueOf(i)
+	kind := valueOf.Kind()
+	isNullable := kind == reflect.Ptr || kind == reflect.Array || kind == reflect.Slice
+	return isNullable && valueOf.IsNil()
 }
