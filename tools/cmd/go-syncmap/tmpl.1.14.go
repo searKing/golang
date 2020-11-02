541f6b629031a2cc4c5427bf929e437a6f5ff46d
haixinchen <haixinchen@tencent.com>
2020-11-02 16:18:53 +0800

    [go-syncmap]refactor Printf with Go Template

--- tools/cmd/go-syncmap/tmpl.1.14.go
+++ tools/cmd/go-syncmap/tmpl.1.14.go
@@ -2,38 +2,4 @@
 
 package main
 
-// Arguments to format are:
-//	[1]: map type name
-//	[2]: key type name
-//	[3]: value type name
-//	[4]: nil value of map type
-const stringOneRun = `func (m *%[1]s) Store(key %[2]s, value %[3]s) {
-    (*sync.Map)(m).Store(key, value)
-}
-
-func (m *%[1]s) LoadOrStore(key %[2]s, value %[3]s) (%[3]s, bool) {
-    actual, loaded := (*sync.Map)(m).LoadOrStore(key, value)
-	if actual == nil {
-        return %[4]s, loaded
-    }
-    return actual.(%[3]s), loaded
-}
-
-func (m *%[1]s) Load(key %[2]s) (%[3]s, bool) {
-    value, ok := (*sync.Map)(m).Load(key)
-    if value == nil {
-    	return %[4]s, ok
-    }
-    return value.(%[3]s), ok
-}
-
-func (m *%[1]s) Delete(key %[2]s) {
-    (*sync.Map)(m).Delete(key)
-}
-
-func (m *%[1]s) Range(f func(key %[2]s, value %[3]s) bool) {
-    (*sync.Map)(m).Range(func(key, value interface{}) bool {
-        return f(key.(%[2]s), value.(%[3]s))
-    })
-}
-`
+const WithMethodLoadAndDelete = false
