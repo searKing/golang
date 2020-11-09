f9f420d3d478e498e7ddc1f9fb6b351d4e72a11c
haixinchen <haixinchen@tencent.com>
2020-11-09 19:20:25 +0800

    [ADD]GetLocalizedMessage

--- third_party/google.golang.org/grpc/status/status.go
+++ third_party/google.golang.org/grpc/status/status.go
@@ -6,8 +6,6 @@ package status
 
 import (
 	"github.com/golang/protobuf/proto"
-	"github.com/searKing/golang/go/error/cause"
-	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -22,11 +20,13 @@ func Newe(c codes.Code, err error, details ...proto.Message) *Status {
 	if err == nil {
 		return status.New(codes.OK, "")
 	}
+
 	if c == codes.OK {
 		// no error details for status with code OK
 		c = codes.Internal
 	}
-	return WithDetails(status.New(c, err.Error()), details...)
+
+	return Convert(c, err, details...)
 }
 
 // Errore returns an error representing c and error.  If err is nil, returns nil.
@@ -37,42 +37,20 @@ func Errore(c codes.Code, err error, details ...proto.Message) error {
 // FromError returns a Status representing err if it was produced from this
 // package or has a method `GRPCStatus() *Status`. Otherwise, ok is false and a
 // Status is returned with code.Code and the original error message.
-func FromError(c codes.Code, err error) (s *Status, ok bool) {
+// code is set only if err has not implemented interface {
+//		GRPCStatus() *Status
+//	}
+func FromError(c codes.Code, err error, details ...proto.Message) (s *Status, ok bool) {
 	stat, ok := status.FromError(err)
 	if ok {
-		return stat, ok
+		return WithDetails(stat, details...), ok
 	}
-	return status.New(c, err.Error()), false
+	return WithDetails(status.New(c, err.Error()), details...), false
 }
 
 // Convert is a convenience function which removes the need to handle the
 // boolean return value from FromError.
-func Convert(c codes.Code, err error) *Status {
-	s, _ := FromError(c, err)
-	return s
-}
-
-// WithBadRequestDetails returns a new status with the provided bad requests messages appended to the status.
-func WithBadRequestDetails(s *Status, fields ...errdetails.BadRequest_FieldViolation) *Status {
-	var badRequest errdetails.BadRequest
-	for _, f := range fields {
-		badRequest.FieldViolations = append(badRequest.FieldViolations, &f)
-	}
-	return WithDetails(s, &badRequest)
-}
-
-// WithDetails returns a new status with the provided details messages appended to the status.
-// If any errors are encountered, it returns original status.
-// WithDetails does not change original code always.
-func WithDetails(s *Status, details ...proto.Message) *Status {
-	stat, err := s.WithDetails(details...)
-	if err == nil {
-		return stat
-	}
-	if s.Err() == nil {
-		return s
-	}
-
-	Newe(s.Code(), cause.WithError(s.Err(), err))
+func Convert(c codes.Code, err error, details ...proto.Message) *Status {
+	s, _ := FromError(c, err, details...)
 	return s
 }
