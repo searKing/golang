1ccf0e728fac8cae1f9e71a8698cee5e95b9d5ef
searKing <searKingChan@gmail.com>
2020-12-04 05:09:11 +0800

    feat(runtime): sp

--- go/runtime/example_test.go
+++ go/runtime/example_test.go
@@ -1,7 +1,12 @@
+// Copyright 2020 The searKing Author. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package runtime_test
 
 import (
 	"fmt"
+	"unsafe"
 
 	"github.com/searKing/golang/go/runtime"
 )
@@ -13,3 +18,42 @@ func ExampleGetCaller() {
 	// Output:
 	// github.com/searKing/golang/go/runtime_test.ExampleGetCaller
 }
+
+//go:nosplit
+func sp() (got, expect uintptr) {
+	var sp uintptr
+	sp = runtime.GetSP(0)
+	return sp, uintptr(unsafe.Pointer(&sp))
+}
+
+//func ExampleGetSP() {
+//	got, expect := sp()
+//	if got != expect {
+//		fmt.Printf("got = %#x\n", got)
+//		fmt.Printf("expect = %#x\n", expect)
+//	}
+//	fmt.Printf("%t", got == expect)
+//
+//	// Output:
+//	// true
+//}
+
+//go:nosplit
+func spv() (got, expect int) {
+	var spv = 1
+	var sp *int
+	sp = (*int)(unsafe.Pointer(runtime.GetSP(0)))
+	return *sp, spv
+}
+
+//func ExampleGetSPV() {
+//	got, expect := spv()
+//	if got != expect {
+//		fmt.Printf("got = %d\n", got)
+//		fmt.Printf("expect = %d\n", expect)
+//	}
+//	fmt.Printf("%t", got == expect)
+//
+//	// Output:
+//	// true
+//}
--- go/runtime/runtime.go
+++ go/runtime/runtime.go
@@ -1,25 +1,60 @@
+// Copyright 2020 The searKing Author. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package runtime
 
-import "unsafe"
+import (
+	_ "runtime"
+	"unsafe"
 
-// Code borrowed from https://github.com/golang/go/blob/master/src/runtime/panic.go#L1068.
+	"github.com/searKing/golang/go/reflect"
+)
+
+// ReadNextSP returns the value, that is SP pointed to where the caller
+// value Next SP pointed to will be reset randomly when ReadNextSP returns
+//go:linkname ReadNextSP github.com/searKing/golang/go/runtime.readNextSP
+func ReadNextSP() int
+
+// readNextSP returns the value, that is SP pointed to where the caller
+// value Next SP pointed to will be reset randomly when ReadNextSP returns
+//go:nosplit
+//go:noinline
+func readNextSP() (x int) {
+	return
+}
+
+// GetSP returns the location, that is SP where the caller
+//go:linkname GetSP github.com/searKing/golang/go/runtime.getSP
+//go:nosplit
+//go:noinline
+func GetSP(uintptr) uintptr
+
+// getSP returns the location, that is SP where the caller
+//go:nosplit
+//go:noinline
+func getSP(x uintptr) uintptr {
+	// x is an argument mainly so that we can return its address.
+	//return uintptr(noescape(unsafe.Pointer(&x))) + unsafe.Sizeof(struct{ _, _ uintptr }{})
+	//return uintptr(noescape(unsafe.Pointer(&x))) + unsafe.Sizeof(uintptr(0))*2
+	return uintptr(noescape(unsafe.Pointer(&x))) + reflect.PtrSize*2
+}
+
+// getNextSP returns the location, that is SP after PUSH 0 where the caller
+//go:nosplit
+//go:noinline
+func getNextSP() (x uintptr) {
+	// x is an argument mainly so that we can return its address.
+	return uintptr(noescape(unsafe.Pointer(&x)))
+}
 
 // noescape hides a pointer from escape analysis.  noescape is
 // the identity function but escape analysis doesn't think the
 // output depends on the input.  noescape is inlined and currently
 // compiles down to zero instructions.
 // USE CAREFULLY!
-// 禁止逃逸,即将指针变成无意义整数
 //go:nosplit
-func NoEscape(p unsafe.Pointer) unsafe.Pointer {
+func noescape(p unsafe.Pointer) unsafe.Pointer {
 	x := uintptr(p)
 	return unsafe.Pointer(x ^ 0)
-}
-
-// getargp returns the location where the caller
-// writes outgoing function call arguments.
-//go:nosplit
-//go:noinline
-func GetArgPtr(x int) uintptr {
-	return uintptr(NoEscape(unsafe.Pointer(&x)))
 }
