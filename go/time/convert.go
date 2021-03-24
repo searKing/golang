3bce71a947e2790a8fabc5841e1c769454e791e0
haixinchen <haixinchen@tencent.com>
2021-03-24 13:21:19 +0800

    feat(time): ConvertTimestamp

---
+++ go/time/convert.go
@@ -0,0 +1,36 @@
+package time
+
+import "time"
+
+// Example:
+// ratio, divide := RatioFrom(fromUnit, toUnit)
+// if divide {
+//   toDuration = fromDuration / ratio
+//   fromDuration = toDuration * ratio
+// } else {
+//   toDuration = fromDuration * ratio
+//   fromDuration = toDuration / ratio
+// }
+func RatioFrom(from time.Duration, to time.Duration) (ratio time.Duration, divide bool) {
+	if from >= to {
+		return from / to, false
+	}
+
+	return to / from, true
+}
+
+// ConvertTimestamp convert timestamp from one unit to another unit
+func ConvertTimestamp(timestamp int64, from, to time.Duration) int64 {
+	ratio, divide := RatioFrom(from, to)
+	if divide {
+		return int64(time.Duration(timestamp) / ratio)
+	}
+	return int64(time.Duration(timestamp) * ratio)
+}
+
+// UnixWithUnit returns the local Time corresponding to the given Unix time by unit
+func UnixWithUnit(timestamp int64, unit time.Duration) time.Time {
+	sec := ConvertTimestamp(timestamp, unit, time.Second)
+	nsec := time.Duration(timestamp)*unit - time.Duration(sec)*time.Second
+	return time.Unix(sec, int64(nsec))
+}
---
+++ go/time/convert_test.go
@@ -0,0 +1,67 @@
+// Copyright 2021 The searKing Author. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package time_test
+
+import (
+	"testing"
+	"time"
+
+	time_ "github.com/searKing/golang/go/time"
+)
+
+func TestConvertTimestamp(t *testing.T) {
+	now := time.Now()
+	tests := []struct {
+		timestamp     int64
+		fromUnit      time.Duration
+		toUnit        time.Duration
+		wantTimestamp int64
+	}{
+		{
+			timestamp:     now.Unix(),
+			fromUnit:      time.Second,
+			toUnit:        time.Nanosecond,
+			wantTimestamp: now.Unix() * int64(time.Second/time.Nanosecond),
+		},
+		{
+			timestamp:     now.UnixNano(),
+			fromUnit:      time.Nanosecond,
+			toUnit:        time.Second,
+			wantTimestamp: now.Unix(),
+		},
+	}
+	for i, tt := range tests {
+		gotTimestamp := time_.ConvertTimestamp(tt.timestamp, tt.fromUnit, tt.toUnit)
+		if tt.wantTimestamp != gotTimestamp {
+			t.Errorf("#%d: ConvertTimestamp expected %q got %q", i, tt.wantTimestamp, gotTimestamp)
+		}
+	}
+}
+
+func TestUnixWithUnit(t *testing.T) {
+	now := time.Now()
+	tests := []struct {
+		timestamp int64
+		unit      time.Duration
+		wantTime  time.Time
+	}{
+		{
+			timestamp: now.Unix(),
+			unit:      time.Second,
+			wantTime:  time.Unix(now.Unix(), 0),
+		},
+		{
+			timestamp: now.UnixNano(),
+			unit:      time.Nanosecond,
+			wantTime:  time.Unix(0, now.UnixNano()),
+		},
+	}
+	for i, tt := range tests {
+		gotTimestamp := time_.UnixWithUnit(tt.timestamp, tt.unit)
+		if tt.wantTime != gotTimestamp {
+			t.Errorf("#%d: ConvertTimestamp expected %q got %q", i, tt.wantTime, gotTimestamp)
+		}
+	}
+}
