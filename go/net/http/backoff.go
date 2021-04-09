ad07e06b5739e83ba67673986201a80970eeb578
haixinchen <haixinchen@tencent.com>
2021-04-09 15:27:13 +0800

    fix(http):  ReplaceHttpRequestBody replace Body and recalculate ContentLength

--- go/net/http/backoff.go
+++ go/net/http/backoff.go
@@ -11,11 +11,14 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"strings"
 	"time"
 
 	time_ "github.com/searKing/golang/go/time"
 )
 
+// ReplaceHttpRequestBody replace Body and recalculate ContentLength
+// If ContentLength should not be recalculated, save and restore it after ReplaceHttpRequestBody
 func ReplaceHttpRequestBody(req *http.Request, body io.Reader) {
 	if req.Body != nil {
 		req.Body.Close()
@@ -25,6 +28,50 @@ func ReplaceHttpRequestBody(req *http.Request, body io.Reader) {
 		rc = ioutil.NopCloser(body)
 	}
 	req.Body = rc
+	req.ContentLength = 0
+	if body != nil {
+		switch v := body.(type) {
+		case *bytes.Buffer:
+			req.ContentLength = int64(v.Len())
+			buf := v.Bytes()
+			req.GetBody = func() (io.ReadCloser, error) {
+				r := bytes.NewReader(buf)
+				return io.NopCloser(r), nil
+			}
+		case *bytes.Reader:
+			req.ContentLength = int64(v.Len())
+			snapshot := *v
+			req.GetBody = func() (io.ReadCloser, error) {
+				r := snapshot
+				return io.NopCloser(&r), nil
+			}
+		case *strings.Reader:
+			req.ContentLength = int64(v.Len())
+			snapshot := *v
+			req.GetBody = func() (io.ReadCloser, error) {
+				r := snapshot
+				return io.NopCloser(&r), nil
+			}
+		default:
+			// This is where we'd set it to -1 (at least
+			// if body != NoBody) to mean unknown, but
+			// that broke people during the Go 1.8 testing
+			// period. People depend on it being 0 I
+			// guess. Maybe retry later. See Issue 18117.
+		}
+		// For client requests, Request.ContentLength of 0
+		// means either actually 0, or unknown. The only way
+		// to explicitly say that the ContentLength is zero is
+		// to set the Body to nil. But turns out too much code
+		// depends on NewRequest returning a non-nil Body,
+		// so we use a well-known ReadCloser variable instead
+		// and have the http package also treat that sentinel
+		// variable to mean explicitly zero.
+		if req.GetBody != nil && req.ContentLength == 0 {
+			req.Body = http.NoBody
+			req.GetBody = func() (io.ReadCloser, error) { return http.NoBody, nil }
+		}
+	}
 }
 
 // Do sends an HTTP request and returns an HTTP response, following
