76dd2547000de9ee0b52ba3d41c834f958033a2d
haixinchen <haixinchen@tencent.com>
2021-04-15 15:21:03 +0800

    feat(strings): ValueOrDefault Return first value nonempty

--- go/strings/default.go
+++ go/strings/default.go
@@ -1,9 +1,13 @@
 package strings
 
-// Return value if nonempty, def otherwise.
-func ValueOrDefault(value, def string) string {
-	if value != "" {
-		return value
+// ValueOrDefault Return first value nonempty
+// Example:
+//	ValueOrDefault(value, def)
+func ValueOrDefault(values ...string) string {
+	for _, v := range values {
+		if v != "" {
+			return v
+		}
 	}
-	return def
+	return ""
 }
