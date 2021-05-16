f1466741b4ded0b2abff5894be726ef67c0289f6
haixinchen <haixinchen@tencent.com>
2021-05-16 16:04:01 +0800

    feat(net): localaddr with routable

--- go/net/addr/local_ip.go
+++ go/net/addr/local_ip.go
@@ -8,6 +8,9 @@ import (
 	"errors"
 	"net"
 	"time"
+
+	strings_ "github.com/searKing/golang/go/strings"
+	"golang.org/x/net/nettest"
 )
 
 // This code is borrowed from https://github.com/uber/tchannel-go/blob/dev/localip.go
@@ -19,7 +22,9 @@ import (
 // Scores are calculated as:
 // -1 for any unknown IP addreseses.
 // +300 for IPv4 addresses
-// +100 for non-local addresses, extra +100 for "up" interaces.
+// +100 for non-local addresses, extra +100 for "up" interfaces.
+// +100 for routable addresses
+// -50 for local mac addr.
 func ScoreAddr(iface net.Interface, addr net.Addr) (int, net.IP) {
 	var ip net.IP
 	if netAddr, ok := addr.(*net.IPNet); ok {
@@ -40,17 +45,28 @@ func ScoreAddr(iface net.Interface, addr net.Addr) (int, net.IP) {
 			score += 100
 		}
 	}
+	_, routable := isRoutableIP("ip", ip)
+	if routable {
+		score -= 25
+	}
 	if isLocalMacAddr(iface.HardwareAddr) {
 		score -= 50
 	}
 	return score, ip
 }
 
-func listenIP(interfaces []net.Interface) (net.IP, error) {
+// filter is a interface filter which returns false if the interface is _not_ to listen on
+func listenIP(interfaces []net.Interface, filter func(iface net.Interface) bool) (net.IP, error) {
+	if filter == nil {
+		filter = func(iface net.Interface) bool { return true }
+	}
 	bestScore := -1
 	var bestIP net.IP
 	// Select the highest scoring IP as the best IP.
 	for _, iface := range interfaces {
+		if !filter(iface) {
+			continue
+		}
 		addrs, err := iface.Addrs()
 		if err != nil {
 			// Skip this interface if there is an error.
@@ -73,14 +89,59 @@ func listenIP(interfaces []net.Interface) (net.IP, error) {
 	return bestIP, nil
 }
 
+// ExpectInterfaceNameFilter
+// If you want to listen specified interfaces (and the loopback) give the name of the interface (eg eth0) here.
+func ExpectInterfaceNameFilter(names ...string) func(iface net.Interface) bool {
+	return func(iface net.Interface) bool {
+		if len(names) == 0 {
+			return true
+		}
+		return strings_.SliceContainsAny(names, iface.Name)
+	}
+}
+
+// ExceptInterfaceNameFilter
+// you can specify which interface _not_ to listen on
+func ExceptInterfaceNameFilter(names ...string) func(iface net.Interface) bool {
+	return func(iface net.Interface) bool {
+		if len(names) == 0 {
+			return true
+		}
+		return !strings_.SliceContainsAny(names, iface.Name)
+	}
+}
+
+// RoutedInterfaceNameFilter returns a network interface that can route IP
+// traffic and satisfies flags.
+//
+// The provided network must be "ip", "ip4" or "ip6".
+func RoutedInterfaceNameFilter() func(iface net.Interface) bool {
+	return func(iface net.Interface) bool {
+		rifs, err := nettest.RoutedInterface("ip", net.FlagUp|net.FlagBroadcast)
+		if err != nil {
+			return true
+		}
+
+		return rifs.Name == iface.Name
+	}
+}
+
 // ListenIP returns the IP to bind to in Listen. It tries to find an IP that can be used
 // by other machines to reach this machine.
-func ListenIP() (net.IP, error) {
+// filters is interface filters any return false if the interface is _not_ to listen on
+func ListenIP(filters ...func(iface net.Interface) bool) (net.IP, error) {
 	interfaces, err := net.Interfaces()
 	if err != nil {
 		return nil, err
 	}
-	return listenIP(interfaces)
+	return listenIP(interfaces, func(iface net.Interface) bool {
+		for _, filter := range filters {
+			if filter != nil && !filter(iface) {
+				return false
+			}
+		}
+		return true
+	})
 }
 
 // DialIP returns the local IP to in Dial.
@@ -121,4 +182,31 @@ func isLocalMacAddr(addr net.HardwareAddr) bool {
 		return false
 	}
 	return addr[0]&2 == 2
+}
+
+func isRoutableIP(network string, ip net.IP) (net.IP, bool) {
+	if !ip.IsLoopback() && !ip.IsLinkLocalUnicast() && !ip.IsGlobalUnicast() {
+		return nil, false
+	}
+	switch network {
+	case "ip4":
+		if ip := ip.To4(); ip != nil {
+			return ip, true
+		}
+	case "ip6":
+		if ip.IsLoopback() { // addressing scope of the loopback address depends on each implementation
+			return nil, false
+		}
+		if ip := ip.To16(); ip != nil && ip.To4() == nil {
+			return ip, true
+		}
+	default:
+		if ip := ip.To4(); ip != nil {
+			return ip, true
+		}
+		if ip := ip.To16(); ip != nil {
+			return ip, true
+		}
+	}
+	return nil, false
 }
