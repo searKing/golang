1e826f316b2706f5f6485adbcd3763399b6431d6
haixinchen <haixinchen@tencent.com>
2021-06-12 21:57:45 +0800

    feat(runtime): BytesPointer

---
+++ go/runtime/cgo.go
@@ -0,0 +1,15 @@
+// Copyright 2021 The searKing Author. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package runtime
+
+import "unsafe"
+
+// BytesPointer returns a pointer which an be used by CGO
+func BytesPointer(data []byte) unsafe.Pointer {
+	if len(data) == 0 {
+		return nil
+	}
+	return unsafe.Pointer(&data[0])
+}
