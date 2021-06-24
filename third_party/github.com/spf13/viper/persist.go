9afb7fea8ef64e2a006cfb9cdefa23c7df7ca020
haixinchen <haixinchen@tencent.com>
2021-06-24 15:25:26 +0800

    feat(viper): PersistConfigTo

--- third_party/github.com/spf13/viper/persist.go
+++ third_party/github.com/spf13/viper/persist.go
@@ -16,21 +16,33 @@ import (
 
 // PersistConfig writes config using into .use.<name>.yaml
 func PersistConfig(v *viper.Viper) error {
+	configFileUsing := DefaultPersisConfigPath(v)
+	return PersistConfigTo(v, configFileUsing)
+}
+
+// DefaultPersisConfigPath returns the given file name to persist as YAML
+func DefaultPersisConfigPath(v *viper.Viper) string {
 	// persist using config
 	f := v.ConfigFileUsed() // ./conf/.sole.yaml
 	if f == "" {
-		return fmt.Errorf("persist skiped, for no config file used")
+		return ".use.yaml"
 	}
 	dir := filepath.Dir(f)
 	base := filepath.Base(f)
 	ext := filepath.Ext(f)
 	name := strings.TrimPrefix(strings.TrimSuffix(base, ext), ".")
 
-	configFileUsing := filepath.Join(dir, ".use."+name+".yaml") // /root/.use.sole.yaml
+	return filepath_.Pathify(filepath.Join(dir, ".use."+name+".yaml")) // /root/.use.sole.yaml
+}
 
-	err := v.WriteConfigAs(configFileUsing)
+// PersistConfigTo writes the completed component config into the given file name as YAML
+func PersistConfigTo(v *viper.Viper, filename string) error {
+	if filename == "" {
+		return fmt.Errorf("persist skiped, for no config file used")
+	}
+	err := v.WriteConfigAs(filename)
 	if err != nil {
-		return fmt.Errorf("write using config file[%s]: %w", filepath_.Pathify(configFileUsing), err)
+		return fmt.Errorf("write using config file[%s]: %w", filename, err)
 	}
 	return nil
 }
