5f19eed5b4c0ddfe7c84b344092b092d393e66d5
haixinchen <haixinchen@tencent.com>
2021-07-26 21:41:27 +0800

    feat(sync): Until

---
+++ go/sync/until.go
@@ -0,0 +1,102 @@
+// Copyright 2021 The searKing Author. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package sync
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"sync"
+)
+
+// ErrUntilClosed is returned by the Until's Do method after a call to Close.
+var ErrUntilClosed = errors.New("sync: Until closed")
+
+// Until represents a class of work and forms a namespace in
+// which units of work can be executed with duplicate suppression.
+// It blocks on certain Do actions and unblock when Retry is called.
+type Until struct {
+	mu         sync.Mutex
+	done       bool
+	blockingCh chan struct{}
+}
+
+// Retry unblocks all blocked pick.
+func (u *Until) Retry() {
+	u.mu.Lock()
+	if u.done {
+		u.mu.Unlock()
+		return
+	}
+	if u.blockingCh != nil {
+		close(u.blockingCh)
+	}
+	u.blockingCh = make(chan struct{})
+	u.mu.Unlock()
+}
+
+// Do executes and returns the results of the given function.
+// It may block in the following cases:
+// - the current fn is nil
+// - the subConn returned by the current picker is not READY
+// When one of these situations happens, Do blocks until the Retry is called.
+func (u *Until) Do(ctx context.Context, fn func() (interface{}, error)) (val interface{}, err error) {
+	u.mu.Lock()
+	if u.blockingCh == nil {
+		u.blockingCh = make(chan struct{})
+	}
+	u.mu.Unlock()
+	var ch chan struct{}
+
+	var lastPickErr error
+	for {
+		u.mu.Lock()
+		if u.done {
+			u.mu.Unlock()
+			return nil, ErrUntilClosed
+		}
+		if fn == nil {
+			ch = u.blockingCh
+		}
+		if ch == u.blockingCh {
+			// This could happen when either:
+			// - forget (the previous if condition), or
+			// - has called Do on the current Do.
+			u.mu.Unlock()
+			select {
+			case <-ctx.Done():
+				var errStr string
+				if lastPickErr != nil {
+					errStr = "latest single_flight_group error: " + lastPickErr.Error()
+				} else {
+					errStr = ctx.Err().Error()
+				}
+				return nil, fmt.Errorf("%s: %w", errStr, ctx.Err())
+			case <-ch:
+			}
+			continue
+		}
+
+		ch = u.blockingCh
+		u.mu.Unlock()
+		val, err = fn()
+		if err != nil {
+			lastPickErr = err
+			// continue back to the beginning of the for loop to redo.
+			continue
+		}
+		return val, err
+	}
+}
+
+func (u *Until) Close() {
+	u.mu.Lock()
+	defer u.mu.Unlock()
+	if u.done {
+		return
+	}
+	u.done = true
+	close(u.blockingCh)
+}
---
+++ go/sync/until_test.go
@@ -0,0 +1,107 @@
+// Copyright 2021 The searKing Author. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package sync_test
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"sync"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	sync_ "github.com/searKing/golang/go/sync"
+)
+
+const goroutineCount = 5
+
+func TestBlockingPickTimeout(t *testing.T) {
+	var until sync_.Until
+	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond)
+	defer cancel()
+	if _, err := until.Do(ctx, func() (interface{}, error) { return "bar", nil }); !errors.Is(err, context.DeadlineExceeded) {
+		t.Errorf("until.Do returned error %v, want DeadlineExceeded", err)
+	}
+}
+
+func TestUntil_DoBlocking(t *testing.T) {
+	var until sync_.Until
+	// All goroutines should block because picker is nil in until.
+	var finishedCount uint64
+	for i := goroutineCount; i > 0; i-- {
+		go func() {
+			if tr, err := until.Do(context.Background(), nil); err != nil || tr != "bar" {
+				t.Errorf("until.Do returned non-nil error: %v", err)
+			}
+			atomic.AddUint64(&finishedCount, 1)
+		}()
+	}
+	time.Sleep(50 * time.Millisecond)
+	if c := atomic.LoadUint64(&finishedCount); c != 0 {
+		t.Errorf("finished goroutines count: %v, want 0", c)
+	}
+	until.Close()
+}
+
+func TestUntil_Do(t *testing.T) {
+	var until sync_.Until
+	// All goroutines should block because picker is nil in until.
+	var finishedCount uint64
+	for i := goroutineCount; i > 0; i-- {
+		go func() {
+			if tr, err := until.Do(context.Background(), func() (interface{}, error) {
+				return "bar", nil
+			}); err != nil || tr != "bar" {
+				t.Errorf("until.Do returned non-nil error: %v", err)
+			}
+			atomic.AddUint64(&finishedCount, 1)
+		}()
+	}
+	time.Sleep(50 * time.Millisecond)
+	if c := atomic.LoadUint64(&finishedCount); c != goroutineCount {
+		t.Errorf("finished goroutines count: %v, want 0", c)
+	}
+	until.Close()
+}
+
+func TestUntil_DoBlockingAndGot(t *testing.T) {
+	var mu sync.Mutex
+	var fn = func() (interface{}, error) {
+		return "bar", fmt.Errorf("must error")
+	}
+
+	var fnHolder = func() (interface{}, error) {
+		mu.Lock()
+		defer mu.Unlock()
+		return fn()
+	}
+
+	var until sync_.Until
+	// All goroutines should block because picker is nil in until.
+	var finishedCount uint64
+	for i := goroutineCount; i > 0; i-- {
+		go func() {
+			if tr, err := until.Do(context.Background(), fnHolder); err != nil || tr != "bar" {
+				t.Errorf("until.Do returned non-nil error: %v", err)
+			}
+			atomic.AddUint64(&finishedCount, 1)
+		}()
+	}
+	time.Sleep(50 * time.Millisecond)
+	if c := atomic.LoadUint64(&finishedCount); c != 0 {
+		t.Errorf("finished goroutines count: %v, want 0", c)
+	}
+	fn = func() (interface{}, error) {
+		return "bar", nil
+	}
+	until.Retry()
+
+	time.Sleep(50 * time.Millisecond)
+	if c := atomic.LoadUint64(&finishedCount); c != goroutineCount {
+		t.Errorf("finished goroutines count: %v, want 0", c)
+	}
+	until.Close()
+}
