1773d751674dc7ec5aa4bb1c980aa46461fbd67a
haixinchen <haixinchen@tencent.com>
2021-07-29 01:10:57 +0800

    feat(http): backoff

--- go/net/http/backoff.go
+++ go/net/http/backoff.go
@@ -6,16 +6,90 @@ package http
 
 import (
 	"bytes"
+	"crypto/x509"
 	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
+	"net/url"
+	"regexp"
+	"strconv"
 	"strings"
 	"time"
 
 	time_ "github.com/searKing/golang/go/time"
 )
+
+var (
+	// A regular expression to match the error returned by net/http when the
+	// configured number of redirects is exhausted. This error isn't typed
+	// specifically so we resort to matching on the error string.
+	redirectsErrorRe = regexp.MustCompile(`stopped after \d+ redirects\z`)
+
+	// A regular expression to match the error returned by net/http when the
+	// scheme specified in the URL is invalid. This error isn't typed
+	// specifically so we resort to matching on the error string.
+	schemeErrorRe = regexp.MustCompile(`unsupported protocol scheme`)
+)
+
+// RetryAfter tries to parse Retry-After response header when a http.StatusTooManyRequests
+// (HTTP Code 429) is found in the resp parameter. Hence it will return the number of
+// seconds the server states it may be ready to process more requests from this client.
+// Don't retry if the error was due to too many redirects.
+// Don't retry if the error was due to an invalid protocol scheme.
+// Don't retry if the error was due to TLS cert verification failure.
+// Don't retry if the http's StatusCode is http.StatusNotImplemented.
+func RetryAfter(resp *http.Response, err error, defaultBackoff time.Duration) (backoff time.Duration, retry bool) {
+	backoff = defaultBackoff
+	if resp != nil {
+		if resp.StatusCode == http.StatusTooManyRequests || resp.StatusCode == http.StatusServiceUnavailable {
+			if sleepInSeconds, err := strconv.ParseInt(resp.Header.Get("Retry-After"), 10, 64); err == nil {
+				backoff = time.Second * time.Duration(sleepInSeconds)
+			}
+		}
+	}
+
+	if err != nil {
+		if v, ok := err.(*url.Error); ok {
+			// Don't retry if the error was due to too many redirects.
+			if redirectsErrorRe.MatchString(v.Error()) {
+				return backoff, false
+			}
+
+			// Don't retry if the error was due to an invalid protocol scheme.
+			if schemeErrorRe.MatchString(v.Error()) {
+				return backoff, false
+			}
+
+			// Don't retry if the error was due to TLS cert verification failure.
+			if _, ok := v.Err.(x509.UnknownAuthorityError); ok {
+				return backoff, false
+			}
+		}
+
+		// The error is likely recoverable so retry.
+		return backoff, true
+	}
+
+	if resp != nil {
+		// 429 Too Many Requests is recoverable. Sometimes the server puts
+		// a Retry-After response header to indicate when the server is
+		// available to start processing request from client.
+		if resp.StatusCode == http.StatusTooManyRequests {
+			return backoff, true
+		}
+
+		// Check the response code. We retry on 500-range responses to allow
+		// the server time to recover, as 500's are typically not permanent
+		// errors and may relate to outages on the server side. This will catch
+		// invalid response codes as well, like 0 and 999.
+		if resp.StatusCode == 0 || (resp.StatusCode >= http.StatusInternalServerError && resp.StatusCode != http.StatusNotImplemented) {
+			return backoff, true
+		}
+	}
+	return backoff, false
+}
 
 // ReplaceHttpRequestBody replace Body and recalculate ContentLength
 // If ContentLength should not be recalculated, save and restore it after ReplaceHttpRequestBody
@@ -74,6 +148,19 @@ func ReplaceHttpRequestBody(req *http.Request, body io.Reader) {
 	}
 }
 
+// ClientInvoker is called by ClientInterceptor to complete RPCs.
+type ClientInvoker func(req *http.Request, retry int) (*http.Response, error)
+
+// ClientInterceptor intercepts the execution of a HTTP on the client.
+// interceptors can be specified as a DoWithBackoffOption, using
+// WithClientInterceptor() or WithChainClientInterceptor(), when DoWithBackoffOption.
+// When a interceptor(s) is set, gRPC delegates all http invocations to the interceptor,
+// and it is the responsibility of the interceptor to call invoker to complete the processing
+// of the HTTP.
+type ClientInterceptor func(req *http.Request, retry int, invoker ClientInvoker, opts ...DoWithBackoffOption) (resp *http.Response, err error)
+
+type RetryAfterHandler func(resp *http.Response, err error, defaultBackoff time.Duration) (backoff time.Duration, retry bool)
+
 // Do sends an HTTP request and returns an HTTP response, following
 // policy (such as redirects, cookies, auth) as configured on the
 // client.
@@ -81,12 +168,63 @@ func Do(req *http.Request) (*http.Response, error) {
 	return http.DefaultClient.Do(req)
 }
 
+//go:generate go-option -type "doWithBackoff"
+type doWithBackoff struct {
+	ClientInterceptor        ClientInterceptor
+	ChainClientInterceptors  []ClientInterceptor
+	RetryAfter               RetryAfterHandler
+	ExponentialBackOffOption []time_.ExponentialBackOffOption
+}
+
+func (o *doWithBackoff) SetDefault() {
+	o.RetryAfter = RetryAfter
+}
+
+// getClientInvoker recursively generate the chained client invoker.
+func getClientInvoker(interceptors []ClientInterceptor, curr int, finalInvoker ClientInvoker, opts ...DoWithBackoffOption) ClientInvoker {
+	if curr == len(interceptors)-1 {
+		return finalInvoker
+	}
+	return func(req *http.Request, retry int) (*http.Response, error) {
+		return interceptors[curr+1](req, retry, getClientInvoker(interceptors, curr+1, finalInvoker), opts...)
+	}
+}
+
+func (o *doWithBackoff) Complete() {
+	interceptors := o.ChainClientInterceptors
+	o.ChainClientInterceptors = nil
+	// Prepend o.ClientInterceptor to the chaining interceptors if it exists, since ClientInterceptor will
+	// be executed before any other chained interceptors.
+	if o.ClientInterceptor != nil {
+		interceptors = append([]ClientInterceptor{o.ClientInterceptor}, interceptors...)
+	}
+	var chainedInt ClientInterceptor
+	if len(interceptors) == 0 {
+		chainedInt = nil
+	} else if len(interceptors) == 1 {
+		chainedInt = interceptors[0]
+	} else {
+		chainedInt = func(req *http.Request, retry int, invoker ClientInvoker, opts ...DoWithBackoffOption) (resp *http.Response, err error) {
+			return interceptors[0](req, retry, getClientInvoker(interceptors, 0, invoker), opts...)
+		}
+	}
+	o.ClientInterceptor = chainedInt
+}
+
 // DoWithBackoff will retry by exponential backoff if failed.
 // If request is not rewindable, retry wil be skipped.
-func DoWithBackoff(httpReq *http.Request, opts ...time_.ExponentialBackOffOption) (*http.Response, error) {
+func DoWithBackoff(httpReq *http.Request, opts ...DoWithBackoffOption) (*http.Response, error) {
+	var opt doWithBackoff
+	opt.SetDefault()
+	opt.ApplyOptions(opts...)
+	if opt.RetryAfter == nil {
+		opt.RetryAfter = RetryAfter
+	}
+	opt.Complete()
+
 	var option []time_.ExponentialBackOffOption
 	option = append(option, time_.WithExponentialBackOffOptionMaxElapsedCount(3))
-	option = append(option, opts...)
+	option = append(option, opt.ExponentialBackOffOption...)
 	backoff := time_.NewDefaultExponentialBackOff(option...)
 	rewindableErr := RequestWithBodyRewindable(httpReq)
 	var retries int
@@ -98,27 +236,55 @@ func DoWithBackoff(httpReq *http.Request, opts ...time_.ExponentialBackOffOption
 			}
 			httpReq.Body = newBody
 		}
-		resp, err := Do(httpReq)
-		if err == nil {
-			return resp, nil
+		var do = func(req *http.Request, retry int) (*http.Response, error) {
+			return Do(req)
 		}
-		retries++
-		if rewindableErr != nil {
-			return nil, fmt.Errorf("http do cannot backoff: %w", retries, rewindableErr)
+		httpDo := do
+		if opt.ClientInterceptor != nil {
+			httpDo = func(req *http.Request, retry int) (*http.Response, error) {
+				return opt.ClientInterceptor(req, retry, do, opts...)
+			}
 		}
+		resp, err := httpDo(httpReq, retries)
 
 		wait, ok := backoff.NextBackOff()
 		if !ok {
-			return nil, fmt.Errorf("http do reach backoff limit after retries %d", retries)
+			if err != nil {
+				return nil, fmt.Errorf("http do reach backoff limit after retries %d: %w", retries, err)
+			} else {
+				return resp, nil
+			}
+		}
+
+		wait, retry := opt.RetryAfter(resp, err, wait)
+		if !retry {
+			if err != nil {
+				return nil, fmt.Errorf("http do reach server limit after retries %d: %w", retries, err)
+			} else {
+				return resp, nil
+			}
+		}
+
+		if rewindableErr != nil {
+			if err != nil {
+				return nil, fmt.Errorf("http do cannot rewindbody after retries %d: %w", retries, err)
+			} else {
+				return resp, nil
+			}
 		}
 
 		timer := time.NewTimer(wait)
 		select {
 		case <-timer.C:
+			retries++
 			continue
 		case <-httpReq.Context().Done():
 			timer.Stop()
-			return nil, fmt.Errorf("http do canceled after retries %d: %w", retries, httpReq.Context().Err())
+			if err != nil {
+				return nil, fmt.Errorf("http do canceled after retries %d: %w", retries, err)
+			} else {
+				return resp, nil
+			}
 		}
 	}
 }
@@ -153,31 +319,29 @@ func DoJson(httpReq *http.Request, req, resp interface{}) error {
 }
 
 // DoJsonWithBackoff the same as DoWithBackoff, but bind with json
-func DoJsonWithBackoff(httpReq *http.Request, req, resp interface{}, opts ...time_.ExponentialBackOffOption) error {
-	var option []time_.ExponentialBackOffOption
-	option = append(option, time_.WithExponentialBackOffOptionMaxElapsedCount(3))
-	option = append(option, opts...)
-	backoff := time_.NewDefaultExponentialBackOff(option...)
-	var retries int
-	for {
-		err := DoJson(httpReq, req, resp)
-		if err == nil {
-			return nil
+func DoJsonWithBackoff(httpReq *http.Request, req, resp interface{}, opts ...DoWithBackoffOption) error {
+	if req != nil {
+		data, err := json.Marshal(req)
+		if err != nil {
+			return err
 		}
-		retries++
-
-		wait, ok := backoff.NextBackOff()
-		if !ok {
-			return fmt.Errorf("http do reach backoff limit after retries %d", retries)
-		}
-
-		timer := time.NewTimer(wait)
-		select {
-		case <-timer.C:
-			continue
-		case <-httpReq.Context().Done():
-			timer.Stop()
-			return fmt.Errorf("http do canceled after retries %d: %w", retries, httpReq.Context().Err())
-		}
+		reqBody := bytes.NewReader(data)
+		httpReq.Header.Set("Content-Type", "application/json")
+		ReplaceHttpRequestBody(httpReq, reqBody)
 	}
+	httpResp, err := DoWithBackoff(httpReq, opts...)
+
+	if err != nil {
+		return err
+	}
+	defer httpResp.Body.Close()
+	if resp == nil {
+		return nil
+	}
+
+	body, err := ioutil.ReadAll(httpResp.Body)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(body, resp)
 }
---
+++ go/net/http/dowithbackoff.options.go
@@ -0,0 +1,52 @@
+// Copyright 2021 The searKing Author. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package http
+
+import (
+	time_ "github.com/searKing/golang/go/time"
+)
+
+// WithDoWithBackoffOptionClientInterceptor returns a DoWithBackoffOption that specifies the interceptor for
+// http clients.
+func WithDoWithBackoffOptionClientInterceptor(f ClientInterceptor) DoWithBackoffOption {
+	return DoWithBackoffOptionFunc(func(o *doWithBackoff) {
+		o.ClientInterceptor = f
+	})
+}
+
+// WithDoWithBackoffOptionChainUnaryInterceptor returns a DoWithBackoffOption that specifies the chained
+// interceptor for http clients. The first interceptor will be the outer most,
+// while the last interceptor will be the inner most wrapper around the real call.
+// All interceptors added by this method will be chained, and the interceptor
+// defined by WithClientInterceptor will always be prepended to the chain.
+func WithDoWithBackoffOptionChainUnaryInterceptor(interceptors ...ClientInterceptor) DoWithBackoffOption {
+	return DoWithBackoffOptionFunc(func(o *doWithBackoff) {
+		o.ChainClientInterceptors = append(o.ChainClientInterceptors, interceptors...)
+	})
+}
+
+func WithDoWithBackoffOptionRetryAfter(f RetryAfterHandler) DoWithBackoffOption {
+	return DoWithBackoffOptionFunc(func(o *doWithBackoff) {
+		o.RetryAfter = f
+	})
+}
+
+func WithDoWithBackoffOptionExponentialBackOffOption(opts ...time_.ExponentialBackOffOption) DoWithBackoffOption {
+	return DoWithBackoffOptionFunc(func(o *doWithBackoff) {
+		o.ExponentialBackOffOption = append(o.ExponentialBackOffOption, opts...)
+	})
+}
+
+func WithDoWithBackoffOptionMaxRetries(retries int) DoWithBackoffOption {
+	return DoWithBackoffOptionFunc(func(o *doWithBackoff) {
+		o.ExponentialBackOffOption = append(o.ExponentialBackOffOption, time_.WithExponentialBackOffOptionMaxElapsedCount(retries))
+	})
+}
+
+func WithDoWithBackoffOptionGrpcBackOff(retries int) DoWithBackoffOption {
+	return DoWithBackoffOptionFunc(func(o *doWithBackoff) {
+		o.ExponentialBackOffOption = append(o.ExponentialBackOffOption, time_.WithExponentialBackOffOptionGRPC())
+	})
+}
