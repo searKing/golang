d1ca43b579c2b160d9f33ccf3718c1b904d5dd22
haixinchen <haixinchen@tencent.com>
2021-08-02 15:57:19 +0800

    feat(io): CloserFunc for io.Closer

--- go/io/io.go
+++ go/io/io.go
@@ -5,11 +5,28 @@
 package io
 
 import (
+	"io"
 	"os"
 )
+
+var _ io.Closer = CloserFunc(nil)
 
 // Stater is the interface that wraps the basic Stat method.
 // Stat returns the FileInfo structure describing file.
 type Stater interface {
 	Stat() (os.FileInfo, error)
+}
+
+// The CloserFunc type is an adapter to allow the use of
+// ordinary functions as io.Closer handlers. If f is a function
+// with the appropriate signature, CloserFunc(f) is a
+// Handler that calls f.
+type CloserFunc func() error
+
+// Close calls f(w, r).
+func (f CloserFunc) Close() error {
+	if f == nil {
+		return nil
+	}
+	return f()
 }
