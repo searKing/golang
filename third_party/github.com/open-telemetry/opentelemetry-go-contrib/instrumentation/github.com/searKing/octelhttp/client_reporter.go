3f641e151d10cdff73fc0f770a2028f410eec172
haixinchen <haixinchen@tencent.com>
2021-08-17 00:13:41 +0800

    feat(octelhttp): metric

---
+++ third_party/github.com/open-telemetry/opentelemetry-go-contrib/instrumentation/github.com/searKing/octelhttp/client_reporter.go
@@ -0,0 +1,77 @@
+// Copyright 2021 The searKing Author. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package otelgrpc
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/semconv"
+	"google.golang.org/grpc/codes"
+)
+
+type clientReporter struct {
+	metrics   *ClientMetrics
+	attrs     []attribute.KeyValue
+	startTime time.Time
+}
+
+func newClientReporter(ctx context.Context, m *ClientMetrics, rpcType grpcType, fullMethod string, target string, source string) *clientReporter {
+	r := &clientReporter{
+		metrics: m,
+	}
+	if r.metrics.clientHandledTimeHistogramEnabled {
+		r.startTime = time.Now()
+	}
+
+	_, attrs := spanInfo(fullMethod, target, source, rpcType)
+	r.attrs = attrs
+	r.metrics.clientStartedCounter.Add(ctx, 1, r.Attrs()...)
+	return r
+}
+
+func (r *clientReporter) Attrs(attrs ...attribute.KeyValue) []attribute.KeyValue {
+	attrs = append(r.attrs, attrs...)
+	filter := AttrsFilter
+	if filter != nil {
+		return filter(attrs...)
+	}
+	return attrs
+}
+
+func (r *clientReporter) ReceivedResponse(ctx context.Context, resp *http.Response) {
+	attrs := r.Attrs(semconv.RPCMessageTypeReceived)
+	r.metrics.clientStreamRequestReceived.Add(ctx, 1, attrs...)
+	if r.metrics.clientStreamReceiveSizeHistogramEnabled {
+		if resp != nil {
+			r.metrics.clientStreamReceiveSizeHistogram.Record(ctx, resp.ContentLength, attrs...)
+		} else {
+			r.metrics.clientStreamReceiveSizeHistogram.Record(ctx, -1, attrs...)
+		}
+	}
+}
+
+func (r *clientReporter) SentRequest(ctx context.Context, req *http.Request) {
+	attrs := r.Attrs(semconv.RPCMessageTypeSent)
+	r.metrics.clientStreamRequestSent.Add(ctx, 1, attrs...)
+	if r.metrics.clientStreamSendSizeHistogramEnabled {
+		if req != nil {
+			r.metrics.clientStreamSendSizeHistogram.Record(ctx, req.ContentLength, attrs...)
+		} else {
+			r.metrics.clientStreamSendSizeHistogram.Record(ctx, -1, attrs...)
+		}
+	}
+}
+
+func (r *clientReporter) Handled(ctx context.Context, code codes.Code) {
+	attrs := r.Attrs(statusCodeAttr(code))
+	r.metrics.clientHandledCounter.Add(ctx, 1, attrs...)
+
+	if r.metrics.clientHandledTimeHistogramEnabled {
+		r.metrics.clientHandledTimeHistogram.Record(ctx, time.Since(r.startTime).Seconds(), attrs...)
+	}
+}
---
+++ third_party/github.com/open-telemetry/opentelemetry-go-contrib/instrumentation/github.com/searKing/octelhttp/interceptor.go
@@ -0,0 +1,103 @@
+// Copyright 2021 The searKing Author. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package otelgrpc
+
+// gRPC tracing middleware
+// https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/trace/semantic_conventions/rpc.md
+import (
+	"fmt"
+	"net"
+	"net/http"
+	"strings"
+
+	"github.com/searKing/golang/third_party/github.com/open-telemetry/opentelemetry-go-contrib/instrumentation/google.golang.org/grpc/otelgrpc"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/semconv"
+	grpccodes "google.golang.org/grpc/codes"
+)
+
+// spanInfo returns a span name and all appropriate attributes from the gRPC
+// method and peer address.
+func spanInfo(fullMethod, peerAddress, localAddress string, grpcType grpcType) (string, []attribute.KeyValue) {
+	attrs := []attribute.KeyValue{semconv.RPCSystemGRPC}
+	name, mAttrs := parseFullMethod(fullMethod)
+	attrs = append(attrs, mAttrs...)
+	attrs = append(attrs, peerAttr(peerAddress)...)
+	attrs = append(attrs, localAttr(localAddress)...)
+	attrs = append(attrs, grpcTypeAttr(grpcType))
+	return name, attrs
+}
+
+func fullMethodFromHttpRequest(req *http.Request) string {
+	if req == nil {
+		return ""
+	}
+	return fmt.Sprintf("%s/%s", req.Method, req.URL.Path)
+}
+
+// peerAttr returns attributes about the peer address.
+func peerAttr(addr string) []attribute.KeyValue {
+	host, port, err := net.SplitHostPort(addr)
+	if err != nil {
+		return []attribute.KeyValue(nil)
+	}
+
+	if host == "" {
+		host = "127.0.0.1"
+	}
+
+	return []attribute.KeyValue{
+		semconv.NetPeerIPKey.String(host),
+		semconv.NetPeerPortKey.String(port),
+	}
+}
+
+// localAttr returns attributes about the local address.
+func localAttr(addr string) []attribute.KeyValue {
+	host, port, err := net.SplitHostPort(addr)
+	if err != nil {
+		return []attribute.KeyValue(nil)
+	}
+
+	if host == "" {
+		host = "127.0.0.1"
+	}
+
+	return []attribute.KeyValue{
+		semconv.NetHostIPKey.String(host),
+		semconv.NetHostPortKey.String(port),
+	}
+}
+
+// parseFullMethod returns a span name following the OpenTelemetry semantic
+// conventions as well as all applicable span attribute.KeyValue attributes based
+// on a gRPC's FullMethod.
+func parseFullMethod(fullMethod string) (string, []attribute.KeyValue) {
+	name := strings.TrimLeft(fullMethod, "/")
+	parts := strings.SplitN(name, "/", 2)
+	if len(parts) != 2 {
+		// Invalid format, does not follow `/package.service/method`.
+		return name, []attribute.KeyValue(nil)
+	}
+
+	var attrs []attribute.KeyValue
+	if service := parts[0]; service != "" {
+		attrs = append(attrs, semconv.RPCServiceKey.String(service))
+	}
+	if method := parts[1]; method != "" {
+		attrs = append(attrs, semconv.RPCMethodKey.String(method))
+	}
+	return name, attrs
+}
+
+// statusCodeAttr returns status code attribute based on given gRPC code
+func statusCodeAttr(c grpccodes.Code) attribute.KeyValue {
+	return otelgrpc.GRPCStatusCodeKey.Int64(int64(c))
+}
+
+// grpcTypeAttr returns gRPC type attribute based on given gRPC type
+func grpcTypeAttr(t grpcType) attribute.KeyValue {
+	return otelgrpc.GRPCTypeKey.String(string(t))
+}
