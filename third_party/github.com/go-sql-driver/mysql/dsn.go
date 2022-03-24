7fba13885ba3b8145d71137e501ac30c97d92443
haixinchen <haixinchen@tencent.com>
2022-03-24 16:39:47 +0800

    feat(github.com/go-sql-driver/mysql): github.com/go-sql-driver/mysql

---
+++ third_party/github.com/go-sql-driver/mysql/dsn.go
@@ -0,0 +1,22 @@
+package mysql
+
+import (
+	"net/url"
+
+	"github.com/go-sql-driver/mysql"
+
+	"github.com/searKing/golang/go/database/dsn"
+)
+
+// ParseDSN schema://[user[:password]@][net[(addr)]]/dbname[?param1=value1&paramN=valueN]
+func ParseDSN(dsn_ string) (schema string, cfg *mysql.Config, err error) {
+	schema, dsnSchemaOmitted, query := dsn.Split(dsn_)
+	// [user[:password]@][net[(addr)]]/dbname[?param1=value1&paramN=valueN]
+	cfg, err = mysql.ParseDSN(dsn.Join("", dsnSchemaOmitted, query))
+	return schema, cfg, err
+}
+
+// GetDSN schema://[user[:password]@][net[(addr)]]/dbname[?param1=value1&paramN=valueN]
+func GetDSN(schema string, cfg *mysql.Config) string {
+	return dsn.Join(schema, cfg.FormatDSN(), url.Values{})
+}
---
+++ third_party/github.com/go-sql-driver/mysql/go.mod
@@ -0,0 +1,8 @@
+module github.com/searKing/golang/third_party/github.com/go-sql-driver/mysql
+
+go 1.16
+
+require (
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/searKing/golang v0.0.151
+)
