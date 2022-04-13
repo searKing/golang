c1e81202993302954bb285d823ecb2117041d79d
searKing <471030698@qq.com>
2022-04-14 00:33:29 +0800

    feat(exp/slices): add Reverse

---
+++ go/exp/slices/reverse.go
@@ -0,0 +1,14 @@
+// Copyright 2022 The searKing Author. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package slices
+
+// Reverse reorder a slice of any ordered type in reverse order.
+func Reverse[S ~[]E, E any](x S) {
+	for i := 0; i < len(x)>>1; i++ {
+		t := x[i]
+		x[i] = x[len(x)-1-i]
+		x[len(x)-1-i] = t
+	}
+}
---
+++ go/exp/slices/reverse_test.go
@@ -0,0 +1,37 @@
+// Copyright 2022 The searKing Author. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package slices_test
+
+import (
+	"fmt"
+	"testing"
+
+	slices_ "github.com/searKing/golang/go/exp/slices"
+	"golang.org/x/exp/slices"
+)
+
+func TestReverse(t *testing.T) {
+	tests := []struct {
+		data []int
+		want []int
+	}{
+		{nil, nil},
+		{[]int{}, []int{}},
+		{[]int{0}, []int{0}},
+		{[]int{1, 0}, []int{0, 1}},
+		{[]int{2, 1, 0}, []int{0, 1, 2}},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%v", tt.data), func(t *testing.T) {
+			{
+				slices_.Reverse(tt.data)
+				if !slices.Equal(tt.data, tt.want) {
+					t.Errorf("reversed %v", tt.want)
+					t.Errorf("   got %v", tt.data)
+				}
+			}
+		})
+	}
+}
