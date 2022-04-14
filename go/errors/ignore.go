f1a6b3c6089da19b94c2d931c0d83c04b5e8377e
searKing <471030698@qq.com>
2022-04-14 19:57:10 +0800

    feat(errors): add ErrIgnore

---
+++ go/errors/ignore.go
@@ -0,0 +1,9 @@
+// Copyright 2022 The searKing Author. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package errors
+
+import "errors"
+
+var ErrIgnore = errors.New("error can be ignored")
