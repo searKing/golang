a053b269250e83441a8aff50b484e5a0b54fb91d
searKing <471030698@qq.com>
2022-04-21 20:08:54 +0800

    feat(exp/slices): add FirstFunc, FirstOrZero, FirstOrZeroFunc

---
+++ go/exp/slices/first.go
@@ -0,0 +1,31 @@
+// Copyright 2022 The searKing Author. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package slices
+
+import (
+	"golang.org/x/exp/slices"
+)
+
+// FirstFunc returns the first item from the slice satisfying f(c), or zero if none do.
+func FirstFunc[S ~[]E, E any](s S, f func(E) bool) (e E, ok bool) {
+	var zeroE E
+	i := slices.IndexFunc(s, f)
+	if i == -1 {
+		return zeroE, false
+	}
+	return s[i], true
+}
+
+// FirstOrZero returns the first non-zero item from the slice, or zero if none do.
+func FirstOrZero[S ~[]E, E comparable](s S) E {
+	var zeroE E
+	return FirstOrZeroFunc(s, func(e E) bool { return e != zeroE })
+}
+
+// FirstOrZeroFunc returns the first item from the slice satisfying f(c), or zero if none do.
+func FirstOrZeroFunc[S ~[]E, E any](s S, f func(E) bool) E {
+	e, _ := FirstFunc(s, f)
+	return e
+}
---
+++ go/exp/slices/first_test.go
@@ -0,0 +1,62 @@
+// Copyright 2022 The searKing Author. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package slices_test
+
+import (
+	"fmt"
+	"testing"
+
+	slices_ "github.com/searKing/golang/go/exp/slices"
+)
+
+func TestFirstOrZero_Int(t *testing.T) {
+	tests := []struct {
+		data []int
+		want int
+	}{
+		{nil, 0},
+		{[]int{}, 0},
+		{[]int{0}, 0},
+		{[]int{1, 0}, 1},
+		{[]int{1, 2}, 1},
+		{[]int{0, 1, 2}, 1},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%v", tt.data), func(t *testing.T) {
+			{
+				got := slices_.FirstOrZero(tt.data)
+				if got != tt.want {
+					t.Errorf("first of %v = %v", tt.data, tt.want)
+					t.Errorf("   got %v", got)
+				}
+			}
+		})
+	}
+}
+
+func TestFirstOrZero_String(t *testing.T) {
+	tests := []struct {
+		data []string
+		want string
+	}{
+		{nil, ""},
+		{[]string{}, ""},
+		{[]string{""}, ""},
+		{[]string{"a", ""}, "a"},
+		{[]string{"a", "b"}, "a"},
+		{[]string{"", "a", "b"}, "a"},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%v", tt.data), func(t *testing.T) {
+			{
+				got := slices_.FirstOrZero(tt.data)
+				if got != tt.want {
+					t.Errorf("first of %v = %v", tt.data, tt.want)
+					t.Errorf("   got %v", got)
+				}
+			}
+		})
+	}
+}
