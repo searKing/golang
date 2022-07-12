f6fd58c4b02fa0c48eaefa9367c461aa3bf76e9e
searKing <471030698@qq.com>
2022-07-12 20:53:24 +0800

    fix(go): fix 'go: downgraded' when go get -u

--- third_party/github.com/grpc-ecosystem/grpc-gateway/doc.go
+++ third_party/github.com/grpc-ecosystem/grpc-gateway/doc.go
@@ -2,6 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package grpc_gateway
+package grpc_gateway // import "github.com/searKing/golang/third_party/github.com/grpc-ecosystem/grpc-gateway"
 
 // PlaceHolder file, so this can be seen as a module.
