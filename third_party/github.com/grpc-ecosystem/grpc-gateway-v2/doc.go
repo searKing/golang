c0d1201d628d0c51d7c30913cddb3bf24689c8ea
searKing <471030698@qq.com>
2022-07-12 21:07:50 +0800

    fix(go): revert comments after package name, by https://pkg.go.dev/cmd/go#hdr-Import_path_checking

--- third_party/github.com/grpc-ecosystem/grpc-gateway-v2/doc.go
+++ third_party/github.com/grpc-ecosystem/grpc-gateway-v2/doc.go
@@ -2,6 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package grpc_gateway_v2 // import "github.com/searKing/golang/third_party/github.com/grpc-ecosystem/grpc-gateway-v2"
+package grpc_gateway_v2
 
 // PlaceHolder file, so this can be seen as a module.
