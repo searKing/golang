aa2495c63f33aeb9552aad21566c96bbeec0bfbc
searKing <471030698@qq.com>
2022-07-18 22:13:04 +0800

    feat(exp/slices): SplitN slices s into subslices and returns a slice of the subslices

--- go/exp/slices/spilt.go
+++ go/exp/slices/spilt.go
@@ -4,16 +4,30 @@
 
 package slices
 
-// SplitN slices s into subslices and returns a slice of the subslices .
+// Split slices s into all substrings separated by sep and returns a slice of
+// the substrings between those separators.
+//
+// If sep is <= zero, Split splits after each element, as chunk size is 1. If both s
+// and sep are empty or zero, Split returns an empty slice.
+//
+// It is equivalent to SplitN with a count of -1.
+func Split[S ~[]E, E any](s S, sep int) []S {
+	return SplitN(s, sep, -1)
+}
+
+// SplitN slices s into subslices and returns a slice of the subslices.
 //
 // The count determines the number of subslices to return:
 //   n > 0: at most n subslices; the last subslices will be the unsplit remainder.
+// 		The count determines the number of subslices to return:
+//   		sep > 0: Split splits every sep as chunk size; the last subslices will be the unsplit remainder.
+//   		sep <= 0: take len(S)/n as chunk size
 //   n == 0: the result is nil (zero subslices)
 //   n < 0: all subslices as n == len(s)
 //
-// Edge cases for s and sep (for example, empty strings) are handled
+// Edge cases for s and sep (for example, zero) are handled
 // as described in the documentation for Split.
-func SplitN[S ~[]E, E any](s S, n int) []S {
+func SplitN[S ~[]E, E any](s S, sep int, n int) []S {
 	if s == nil || n == 0 {
 		return nil
 	}
@@ -28,12 +42,15 @@ func SplitN[S ~[]E, E any](s S, n int) []S {
 	if chunkSize == 0 {
 		chunkSize = 1
 	}
+	if sep > 0 {
+		chunkSize = sep
+	}
 	var chunks []S
 	for len(s) > 0 {
 		if len(chunks) == n-1 || chunkSize > len(s) {
 			chunkSize = len(s)
 		}
-		chunk := append(S([]E{}), s[:chunkSize]...)
+		chunk := append([]E{}, s[:chunkSize]...)
 		s = s[chunkSize:]
 		chunks = append(chunks, S(chunk))
 	}
--- go/exp/slices/split_test.go
+++ go/exp/slices/split_test.go
@@ -13,76 +13,93 @@ import (
 
 var equalIntTests = []struct {
 	s    []int
+	sep  int
 	n    int
 	want [][]int
 }{
 	{
 		nil,
 		0,
+		0,
 		nil,
 	},
 	{
 		[]int{},
 		0,
-		nil,
-	},
-	{
-		[]int{1},
 		0,
 		nil,
 	},
 	{
 		[]int{1},
+		0,
+		0,
+		nil,
+	},
+	{
+		[]int{1},
+		0,
 		1,
 		[][]int{{1}},
 	},
 	{
 		[]int{1},
+		0,
 		2,
 		[][]int{{1}},
 	},
 	{
 		[]int{1, 2, 3},
+		0,
 		-1,
 		[][]int{{1}, {2}, {3}},
 	},
 	{
 		[]int{1, 2, 3},
 		0,
+		0,
 		nil,
 	},
 	{
 		[]int{1, 2, 3},
+		0,
 		1,
 		[][]int{{1, 2, 3}},
 	},
 	{
 		[]int{1, 2, 3},
+		0,
 		2,
 		[][]int{{1}, {2, 3}},
 	},
 	{
 		[]int{1, 2, 3},
+		0,
 		3,
 		[][]int{{1}, {2}, {3}},
 	},
 	{
 		[]int{1, 2, 3},
+		0,
 		4,
 		[][]int{{1}, {2}, {3}},
 	},
+	{[]int{1, 2, 3}, 1, 0, nil},
+	{[]int{1, 2, 3}, 1, -1, [][]int{{1}, {2}, {3}}},
+	{[]int{1, 2, 3}, 3, -1, [][]int{{1, 2, 3}}},
+	{[]int{1, 2, 3}, 1, 3, [][]int{{1}, {2}, {3}}},
+	{[]int{1, 2, 3, 4}, 1, 3, [][]int{{1}, {2}, {3, 4}}},
 }
 
 func TestSplitN(t *testing.T) {
 	for _, test := range equalIntTests {
-		got := slices_.SplitN(test.s, test.n)
+		got := slices_.SplitN(test.s, test.sep, test.n)
 		if len(test.want) != len(got) {
-			t.Errorf("SplitN(%v, %v) = %v, want %v", test.s, test.n, got, test.want)
+			t.Errorf("SplitN(%v, %v, %v) = %v, want %v", test.s, test.sep, test.n, got, test.want)
 			continue
 		}
 		for i := range got {
 			if !slices.Equal(got[i], test.want[i]) {
-				t.Errorf("SplitN(%v, %v) = %v, want %v", test.s, test.n, got, test.want)
+				t.Errorf("SplitN(%v, %v, %v) = %v, want %v", test.s, test.sep, test.n, got, test.want)
 				break
 			}
 		}
