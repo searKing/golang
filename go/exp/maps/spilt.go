6529882dc5c3ef44c87a8545aad68a183a03d98b
searKing <471030698@qq.com>
2022-10-25 19:25:43 +0800

    feat(exp/maps): Split|SplitN of maps behaves like strings.Split|SplitN

--- go/exp/maps/spilt.go
+++ go/exp/maps/spilt.go
@@ -4,32 +4,84 @@
 
 package maps
 
-// SplitN slices s into submaps and returns a map of the submaps .
+// Split slices s into all submaps separated by sep and returns a slice of
+// the submaps between those separators.
+//
+// If s is less than sep and sep is more than zero, Split returns a
+// slice of length 1 whose only element is s.
+//
+// If s is nil, Split returns nil (zero submaps).
+//
+// If both s and sep are empty or zero, Split returns an empty slice.
+//
+// If sep is <= zero, Split splits after each element, as chunk size is 1.
+//
+// It is equivalent to SplitN with a count of -1.
+func Split[M ~map[K]V, K comparable, V any](m M, sep int) []M {
+	return SplitN(m, sep, -1)
+}
+
+// SplitN slices s into submaps and returns a slice of the submaps.
 //
 // The count determines the number of submaps to return:
-//   n > 0: at most n submaps; the last submaps will be the unsplit remainder.
-//   n == 0: the result is nil (zero submaps)
-//   n < 0: all submaps as n == len(s)
 //
-// Edge cases for s and sep (for example, empty strings) are handled
+//	  n > 0: at most n submaps; the last submaps will be the unsplit remainder.
+//			The count determines the number of submaps to return:
+//	  		sep > 0: Split splits every sep as chunk size; the last submaps will be the unsplit remainder.
+//	  		sep <= 0: take len(S)/n as chunk size
+//	  n == 0: the result is nil (zero submaps)
+//	  n < 0: all submaps as n == len(s)
+//
+// Edge cases for s and sep (for example, zero) are handled
 // as described in the documentation for Split.
-func SplitN[M ~map[K]V, K comparable, V any](m M, n int) []M {
-	if m == nil || n == 0 {
-		return nil
-	}
+func SplitN[M ~map[K]V, K comparable, V any](m M, sep int, n int) []M {
+	// n < 0: all submaps as n == len(s)
 	if n < 0 {
 		n = len(m)
 	}
-	if n <= 1 || len(m) <= 1 {
+	// Below: n >= 0
+
+	// n == 0: the result is nil (zero submaps)
+	// If s is nil, Split returns nil (zero submaps).
+	if n == 0 || m == nil {
+		return nil
+	}
+
+	// Below: s != nil && n > 0
+
+	// If both s and sep are empty or zero, Split returns an empty slice.
+	if len(m) == 0 && sep == 0 {
+		return []M{}
+	}
+
+	// If s is less or equal than sep and sep is more than zero, Split returns a
+	// slice of length 1 whose only element is s.
+	if len(m) <= sep && sep > 0 {
 		return []M{m}
 	}
 
+	// Below: len(s) > 0 && len(s) > sep && n > 0
+
+	// n > 0: at most n submaps; the last submaps will be the unsplit remainder.
+	//      The count determines the number of submaps to return:
+	//      sep > 0: Split splits every sep as chunk size; the last submaps will be the unsplit remainder.
+	//	  	sep <= 0: take len(S)/n as chunk size
+
+	if n == 1 || len(m) == 1 {
+		return []M{m}
+	}
+
+	// Below: len(s) > 1 && len(s) > sep && n > 1
+
+	// If sep is <= zero, Split splits after each element, as chunk size is 1.
 	chunkSize := len(m) / n
 	if chunkSize == 0 {
 		chunkSize = 1
 	}
+	if sep > 0 {
+		chunkSize = sep
+	}
 	var chunks []M
-
 	var chunk = make(M)
 	for k, v := range m {
 		if len(chunks) == n-1 || chunkSize > len(m) {
--- go/exp/maps/split_test.go
+++ go/exp/maps/split_test.go
@@ -10,113 +10,189 @@ import (
 	maps_ "github.com/searKing/golang/go/exp/maps"
 )
 
-var equalIntTests = []struct {
-	s    map[int]string
-	n    int
+var splitTests = []struct {
+	m    map[int]string
+	sep  int
 	want []map[int]string
 }{
-	{
-		nil,
-		0,
-		nil,
-	},
-	{
-		map[int]string{},
-		0,
-		nil,
-	},
-	{
-		map[int]string{1: "One"},
-		0,
-		nil,
-	},
-	{
-		map[int]string{1: "One"},
-		1,
-		[]map[int]string{{1: "One"}},
-	},
-	{
-		map[int]string{1: "One"},
-		2,
-		[]map[int]string{{1: "One"}},
-	},
-	{
-		map[int]string{1: "One", 2: "Two", 3: "Three"},
-		-1,
-		[]map[int]string{{1: "One"}, {2: "Two"}, {3: "Three"}},
-	},
-	{
-		map[int]string{1: "One", 2: "Two", 3: "Three"},
-		0,
-		nil,
-	},
-	{
-		map[int]string{1: "One", 2: "Two", 3: "Three"},
-		1,
-		[]map[int]string{{1: "One", 2: "Two", 3: "Three"}},
-	},
-	{
-		map[int]string{1: "One", 2: "Two", 3: "Three"},
-		2,
-		[]map[int]string{{1: "One"}, {2: "Two", 3: "Three"}},
-	},
-	{
-		map[int]string{1: "One", 2: "Two", 3: "Three"},
-		3,
-		[]map[int]string{{1: "One"}, {2: "Two"}, {3: "Three"}},
-	},
-	{
-		map[int]string{1: "One", 2: "Two", 3: "Three"},
-		4,
-		[]map[int]string{{1: "One"}, {2: "Two"}, {3: "Three"}},
-	},
-	{
-		map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"},
-		-1,
-		[]map[int]string{{1: "One"}, {2: "Two"}, {3: "Three"}, {4: "Four"}},
-	},
-	{
-		map[int]string{1: "One", 2: "Two", 3: "Three"},
-		0,
-		nil,
-	},
-	{
-		map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"},
-		1,
-		[]map[int]string{{1: "One", 2: "Two", 3: "Three", 4: "Four"}},
-	},
-	{
-		map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"},
-		2,
-		[]map[int]string{{1: "One", 4: "Four"}, {2: "Two", 3: "Three"}},
-	},
-	{
-		map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"},
-		3,
-		[]map[int]string{{1: "One"}, {2: "Two"}, {3: "Three", 4: "Four"}},
-	},
-	{
-		map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"},
-		4,
-		[]map[int]string{{1: "One"}, {2: "Two"}, {3: "Three"}, {4: "Four"}},
-	},
-	{
-		map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"},
-		5,
-		[]map[int]string{{1: "One"}, {2: "Two"}, {3: "Three"}, {4: "Four"}},
-	},
+	{nil, -1, nil},
+	{nil, 0, nil},
+	{nil, 1, nil},
+	{map[int]string{}, -1, nil},
+	{map[int]string{}, 0, nil},
+	{map[int]string{}, 1, nil},
+	{map[int]string{1: "One"}, -1, []map[int]string{{1: "One"}}},
+	{map[int]string{1: "One"}, 0, []map[int]string{{1: "One"}}},
+	{map[int]string{1: "One"}, 1, []map[int]string{{1: "One"}}},
+	{map[int]string{1: "One"}, 2, []map[int]string{{1: "One"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three"}, -1, []map[int]string{{1: "One"}, {2: "Two"}, {3: "Three"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three"}, 0, []map[int]string{{1: "One"}, {2: "Two"}, {3: "Three"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three"}, 1, []map[int]string{{1: "One"}, {2: "Two"}, {3: "Three"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three"}, 2, []map[int]string{{1: "One", 2: "Two"}, {3: "Three"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three"}, 3, []map[int]string{{1: "One", 2: "Two", 3: "Three"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three"}, 4, []map[int]string{{1: "One", 2: "Two", 3: "Three"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, -1, []map[int]string{{1: "One"}, {2: "Two"}, {3: "Three"}, {4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 0, []map[int]string{{1: "One"}, {2: "Two"}, {3: "Three"}, {4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 1, []map[int]string{{1: "One"}, {2: "Two"}, {3: "Three"}, {4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 2, []map[int]string{{1: "One", 2: "Two"}, {3: "Three", 4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 3, []map[int]string{{1: "One", 2: "Two", 3: "Three"}, {4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 4, []map[int]string{{1: "One", 2: "Two", 3: "Three", 4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 5, []map[int]string{{1: "One", 2: "Two", 3: "Three", 4: "Four"}}},
 }
 
-func TestSplitN(t *testing.T) {
-	for _, test := range equalIntTests {
-		got := maps_.SplitN(test.s, test.n)
+func TestSplit(t *testing.T) {
+	for i, test := range splitTests {
+		got := maps_.Split(test.m, test.sep)
 		if len(test.want) != len(got) {
-			t.Errorf("SplitN(%v, %v) = %v, want %v", test.s, test.n, got, test.want)
+			t.Errorf("#%d: Split(%v, %v) = %v, want %v", i, test.m, test.sep, got, test.want)
 			continue
 		}
 		for i := range got {
 			if len(got[i]) != len(test.want[i]) {
-				t.Errorf("SplitN(%v, %v) = %v, want %v", test.s, test.n, got, test.want)
+				t.Errorf("#%d: Split(%v, %v) = %v, want %v", i, test.m, test.sep, got, test.want)
+				break
+			}
+		}
+	}
+}
+
+var splitNTests = []struct {
+	m    map[int]string
+	sep  int
+	n    int
+	want []map[int]string
+}{
+	{nil, -1, -1, nil},
+	{nil, -1, 0, nil},
+	{nil, -1, 1, nil},
+	{nil, 0, -1, nil},
+	{nil, 0, 0, nil},
+	{nil, 0, 1, nil},
+	{nil, 1, -1, nil},
+	{nil, 1, 0, nil},
+	{nil, 1, 1, nil},
+	{map[int]string{}, -1, -1, nil},
+	{map[int]string{}, -1, 0, nil},
+	{map[int]string{}, -1, 1, []map[int]string{{}}},
+	{map[int]string{}, 0, -1, nil},
+	{map[int]string{}, 0, 0, nil},
+	{map[int]string{}, 0, 1, nil},
+	{map[int]string{}, 1, -1, nil},
+	{map[int]string{}, 1, 0, nil},
+	{map[int]string{}, 1, 1, []map[int]string{{}}},
+	{map[int]string{1: "One"}, -1, -1, []map[int]string{{1: "One"}}},
+	{map[int]string{1: "One"}, -1, 0, nil},
+	{map[int]string{1: "One"}, -1, 1, []map[int]string{{1: "One"}}},
+	{map[int]string{1: "One"}, -1, 2, []map[int]string{{1: "One"}}},
+	{map[int]string{1: "One"}, 0, -1, []map[int]string{{1: "One"}}},
+	{map[int]string{1: "One"}, 0, 0, nil},
+	{map[int]string{1: "One"}, 0, 1, []map[int]string{{1: "One"}}},
+	{map[int]string{1: "One"}, 0, 2, []map[int]string{{1: "One"}}},
+	{map[int]string{1: "One"}, 1, -1, []map[int]string{{1: "One"}}},
+	{map[int]string{1: "One"}, 1, 0, nil},
+	{map[int]string{1: "One"}, 1, 1, []map[int]string{{1: "One"}}},
+	{map[int]string{1: "One"}, 1, 2, []map[int]string{{1: "One"}}},
+	{map[int]string{1: "One"}, 2, -1, []map[int]string{{1: "One"}}},
+	{map[int]string{1: "One"}, 2, 0, nil},
+	{map[int]string{1: "One"}, 2, 1, []map[int]string{{1: "One"}}},
+	{map[int]string{1: "One"}, 2, 2, []map[int]string{{1: "One"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three"}, -1, -1, []map[int]string{{1: "One"}, {2: "Two"}, {3: "Three"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three"}, -1, 0, nil},
+	{map[int]string{1: "One", 2: "Two", 3: "Three"}, -1, 1, []map[int]string{{1: "One", 2: "Two", 3: "Three"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three"}, -1, 2, []map[int]string{{1: "One"}, {2: "Two", 3: "Three"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three"}, -1, 3, []map[int]string{{1: "One"}, {2: "Two"}, {3: "Three"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three"}, -1, 4, []map[int]string{{1: "One"}, {2: "Two"}, {3: "Three"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three"}, 0, -1, []map[int]string{{1: "One"}, {2: "Two"}, {3: "Three"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three"}, 0, 0, nil},
+	{map[int]string{1: "One", 2: "Two", 3: "Three"}, 0, 1, []map[int]string{{1: "One", 2: "Two", 3: "Three"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three"}, 0, 2, []map[int]string{{1: "One"}, {2: "Two", 3: "Three"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three"}, 0, 3, []map[int]string{{1: "One"}, {2: "Two"}, {3: "Three"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three"}, 0, 4, []map[int]string{{1: "One"}, {2: "Two"}, {3: "Three"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three"}, 1, -1, []map[int]string{{1: "One"}, {2: "Two"}, {3: "Three"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three"}, 1, 0, nil},
+	{map[int]string{1: "One", 2: "Two", 3: "Three"}, 1, 1, []map[int]string{{1: "One", 2: "Two", 3: "Three"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three"}, 1, 2, []map[int]string{{1: "One"}, {2: "Two", 3: "Three"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three"}, 1, 3, []map[int]string{{1: "One"}, {2: "Two"}, {3: "Three"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three"}, 1, 4, []map[int]string{{1: "One"}, {2: "Two"}, {3: "Three"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three"}, 2, -1, []map[int]string{{1: "One", 2: "Two"}, {3: "Three"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three"}, 2, 0, nil},
+	{map[int]string{1: "One", 2: "Two", 3: "Three"}, 2, 1, []map[int]string{{1: "One", 2: "Two", 3: "Three"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three"}, 2, 2, []map[int]string{{1: "One", 2: "Two"}, {3: "Three"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three"}, 2, 3, []map[int]string{{1: "One", 2: "Two"}, {3: "Three"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three"}, 2, 4, []map[int]string{{1: "One", 2: "Two"}, {3: "Three"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three"}, 3, -1, []map[int]string{{1: "One", 2: "Two", 3: "Three"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three"}, 3, 0, nil},
+	{map[int]string{1: "One", 2: "Two", 3: "Three"}, 3, 1, []map[int]string{{1: "One", 2: "Two", 3: "Three"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three"}, 3, 2, []map[int]string{{1: "One", 2: "Two", 3: "Three"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three"}, 3, 3, []map[int]string{{1: "One", 2: "Two", 3: "Three"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three"}, 3, 4, []map[int]string{{1: "One", 2: "Two", 3: "Three"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three"}, 4, -1, []map[int]string{{1: "One", 2: "Two", 3: "Three"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three"}, 4, 0, nil},
+	{map[int]string{1: "One", 2: "Two", 3: "Three"}, 4, 1, []map[int]string{{1: "One", 2: "Two", 3: "Three"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three"}, 4, 2, []map[int]string{{1: "One", 2: "Two", 3: "Three"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three"}, 4, 3, []map[int]string{{1: "One", 2: "Two", 3: "Three"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three"}, 4, 4, []map[int]string{{1: "One", 2: "Two", 3: "Three"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, -1, -1, []map[int]string{{1: "One"}, {2: "Two"}, {3: "Three"}, {4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, -1, 0, nil},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, -1, 1, []map[int]string{{1: "One", 2: "Two", 3: "Three", 4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, -1, 2, []map[int]string{{1: "One", 2: "Two"}, {3: "Three", 4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, -1, 3, []map[int]string{{1: "One"}, {2: "Two"}, {3: "Three", 4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, -1, 4, []map[int]string{{1: "One"}, {2: "Two"}, {3: "Three"}, {4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, -1, 5, []map[int]string{{1: "One"}, {2: "Two"}, {3: "Three"}, {4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 0, -1, []map[int]string{{1: "One"}, {2: "Two"}, {3: "Three"}, {4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 0, 0, nil},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 0, 1, []map[int]string{{1: "One", 2: "Two", 3: "Three", 4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 0, 2, []map[int]string{{1: "One", 2: "Two"}, {3: "Three", 4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 0, 3, []map[int]string{{1: "One"}, {2: "Two"}, {3: "Three", 4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 0, 4, []map[int]string{{1: "One"}, {2: "Two"}, {3: "Three"}, {4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 0, 5, []map[int]string{{1: "One"}, {2: "Two"}, {3: "Three"}, {4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 1, -1, []map[int]string{{1: "One"}, {2: "Two"}, {3: "Three"}, {4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 1, 0, nil},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 1, 1, []map[int]string{{1: "One", 2: "Two", 3: "Three", 4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 1, 2, []map[int]string{{1: "One"}, {2: "Two", 3: "Three", 4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 1, 3, []map[int]string{{1: "One"}, {2: "Two"}, {3: "Three", 4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 1, 4, []map[int]string{{1: "One"}, {2: "Two"}, {3: "Three"}, {4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 1, 5, []map[int]string{{1: "One"}, {2: "Two"}, {3: "Three"}, {4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 2, -1, []map[int]string{{1: "One", 2: "Two"}, {3: "Three", 4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 2, 0, nil},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 2, 1, []map[int]string{{1: "One", 2: "Two", 3: "Three", 4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 2, 2, []map[int]string{{1: "One", 2: "Two"}, {3: "Three", 4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 2, 3, []map[int]string{{1: "One", 2: "Two"}, {3: "Three", 4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 2, 4, []map[int]string{{1: "One", 2: "Two"}, {3: "Three", 4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 2, 5, []map[int]string{{1: "One", 2: "Two"}, {3: "Three", 4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 3, -1, []map[int]string{{1: "One", 2: "Two", 3: "Three"}, {4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 3, 0, nil},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 3, 1, []map[int]string{{1: "One", 2: "Two", 3: "Three", 4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 3, 2, []map[int]string{{1: "One", 2: "Two", 3: "Three"}, {4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 3, 3, []map[int]string{{1: "One", 2: "Two", 3: "Three"}, {4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 3, 4, []map[int]string{{1: "One", 2: "Two", 3: "Three"}, {4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 3, 5, []map[int]string{{1: "One", 2: "Two", 3: "Three"}, {4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 4, -1, []map[int]string{{1: "One", 2: "Two", 3: "Three", 4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 4, 0, nil},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 4, 1, []map[int]string{{1: "One", 2: "Two", 3: "Three", 4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 4, 2, []map[int]string{{1: "One", 2: "Two", 3: "Three", 4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 4, 3, []map[int]string{{1: "One", 2: "Two", 3: "Three", 4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 4, 4, []map[int]string{{1: "One", 2: "Two", 3: "Three", 4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 4, 5, []map[int]string{{1: "One", 2: "Two", 3: "Three", 4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 5, -1, []map[int]string{{1: "One", 2: "Two", 3: "Three", 4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 5, 0, nil},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 5, 1, []map[int]string{{1: "One", 2: "Two", 3: "Three", 4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 5, 2, []map[int]string{{1: "One", 2: "Two", 3: "Three", 4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 5, 3, []map[int]string{{1: "One", 2: "Two", 3: "Three", 4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 5, 4, []map[int]string{{1: "One", 2: "Two", 3: "Three", 4: "Four"}}},
+	{map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}, 5, 5, []map[int]string{{1: "One", 2: "Two", 3: "Three", 4: "Four"}}},
+}
+
+func TestSplitN(t *testing.T) {
+	for i, test := range splitNTests {
+		got := maps_.SplitN(test.m, test.sep, test.n)
+		if len(test.want) != len(got) {
+			t.Errorf("#%d: SplitN(%v, %v, %v) = %v, want %v", i, test.m, test.sep, test.n, got, test.want)
+			continue
+		}
+		for i := range got {
+			if len(got[i]) != len(test.want[i]) {
+				t.Errorf("#%d: SplitN(%v, %v, %v) = %v, want %v", i, test.m, test.sep, test.n, got, test.want)
 				break
 			}
 		}
