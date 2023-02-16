db9caa2e0bedd817b924e2c8858afe4acacd1668
searKing <471030698@qq.com>
2023-02-16 20:56:19 +0800

    refact(exp/slices)：add TypeAssertFilter|TypeAssertFilterFunc for type assertion of slice

--- go/exp/slices/filter.go
+++ go/exp/slices/filter.go
@@ -4,6 +4,8 @@
 
 package slices
 
+import "reflect"
+
 // Filter returns a slice satisfying c != zero within all c in the slice.
 // Filter modifies the contents of the slice s; it does not create a new slice.
 func Filter[S ~[]E, E comparable](s S) S {
@@ -35,4 +37,72 @@ func FilterFunc[S ~[]E, E any](s S, f func(E) bool) S {
 		}
 	}
 	return s[:i]
+}
+
+// TypeAssertFilter returns a slice satisfying r, ok := any(c).(R); ok == true within all r in the slice.
+// TypeAssertFilter does not modify the contents of the slice s; it creates a new slice.
+func TypeAssertFilter[S ~[]E, M ~[]R, E any, R any](s S) M {
+	if len(s) == 0 {
+		if s == nil {
+			return nil
+		}
+		var emptyM = M{}
+		return emptyM
+	}
+	var m = M{}
+
+	var zeroE E
+	var zeroR R
+	var nilable = any(zeroE) == nil
+
+	var rt = reflect.TypeOf(zeroR)
+	var convertible bool
+	if !nilable || any(zeroE) != nil {
+		var et = reflect.TypeOf(zeroE)
+		convertible = et.ConvertibleTo(rt)
+	}
+
+	for _, v := range s {
+		if !convertible && !nilable {
+			continue
+		}
+
+		if any(v) == nil && any(zeroR) == nil {
+			var zeroR R
+			m = append(m, zeroR)
+			continue
+		}
+
+		if r, ok := any(v).(R); ok {
+			m = append(m, r)
+			continue
+		}
+		if convertible {
+			if r, ok := reflect.ValueOf(v).Convert(rt).Interface().(R); ok {
+				m = append(m, r)
+				continue
+			}
+		}
+	}
+	return m
+}
+
+// TypeAssertFilterFunc returns a slice satisfying f(c) within all c in the slice.
+// TypeAssertFilterFunc does not modify the contents of the slice s; it creates a new slice.
+func TypeAssertFilterFunc[S ~[]E, M ~[]R, E any, R any](s S, f func(E) (R, bool)) M {
+	if len(s) == 0 {
+		if s == nil {
+			return nil
+		}
+		var emptyM = M{}
+		return emptyM
+	}
+
+	var m = M{}
+	for _, v := range s {
+		if r, ok := f(v); ok {
+			m = append(m, r)
+		}
+	}
+	return m
 }
--- go/exp/slices/filter_test.go
+++ go/exp/slices/filter_test.go
@@ -6,6 +6,8 @@ package slices_test
 
 import (
 	"fmt"
+	"reflect"
+	"strconv"
 	"testing"
 
 	slices_ "github.com/searKing/golang/go/exp/slices"
@@ -29,6 +31,10 @@ func TestFilter(t *testing.T) {
 		t.Run(fmt.Sprintf("%v", tt.data), func(t *testing.T) {
 			{
 				got := slices_.Filter(tt.data)
+				if (got == nil && tt.data != nil) || (got != nil && tt.data == nil) {
+					t.Errorf("slices_.Filter(%v) = %v, want %v", tt.data, got, tt.want)
+					return
+				}
 
 				if slices.Compare(got, tt.want) != 0 {
 					t.Errorf("slices_.Filter(%v) = %v, want %v", tt.data, got, tt.want)
@@ -58,6 +64,10 @@ func TestFilterFunc(t *testing.T) {
 				got := slices_.FilterFunc(copy, func(e int) bool {
 					return e != 0
 				})
+				if (got == nil && tt.data != nil) || (got != nil && tt.data == nil) {
+					t.Errorf("slices_.FilterFunc(%v, func(e int) bool {return e != 0}) = %v, want %v", tt.data, got, tt.want)
+					return
+				}
 
 				if slices.Compare(got, tt.want) != 0 {
 					t.Errorf("slices_.FilterFunc(%v, func(e int) bool {return e != 0}) = %v, want %v", tt.data, got, tt.want)
@@ -65,4 +75,215 @@ func TestFilterFunc(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestTypeAssertFilter(t *testing.T) {
+	tests := []struct {
+		data []int
+		want []int8
+	}{
+		{nil, nil},
+		{[]int{}, []int8{}},
+		{[]int{0}, []int8{0}},
+		{[]int{1, 0}, []int8{1, 0}},
+		{[]int{1, 2}, []int8{1, 2}},
+		{[]int{0, 1, 2}, []int8{0, 1, 2}},
+		{[]int{0, 1, 0, 2}, []int8{0, 1, 0, 2}},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%v", tt.data), func(t *testing.T) {
+			{
+				copy := slices.Clone(tt.data)
+				got := slices_.TypeAssertFilter[[]int, []int8](copy)
+				if (got == nil && tt.data != nil) || (got != nil && tt.data == nil) {
+					t.Errorf("slices_.TypeAssertFilter(%v) = %v, want %v", tt.data, got, tt.want)
+					return
+				}
+
+				if slices.Compare(got, tt.want) != 0 {
+					t.Errorf("slices_.TypeAssertFilter(%v) = %v, want %v", tt.data, got, tt.want)
+				}
+			}
+		})
+	}
+}
+
+func TestTypeAssertFilterConvert(t *testing.T) {
+	var zeroDog Dog
+	var zeroMale Male
+	_ = zeroDog
+	_ = zeroMale
+	tests := []struct {
+		data []any
+		want []Human
+	}{
+		{nil, nil},
+		{[]any{}, []Human{}},
+		{[]any{nil}, []Human{nil}},
+		{[]any{0, nil}, []Human{nil}},
+		{[]any{0, nil, 0, nil}, []Human{nil, nil}},
+		{[]any{1, 0, &Dog{}, &Male{}, Dog{}, Male{}}, []Human{&Male{}, Male{}}},
+		{[]any{1, 2, zeroDog, zeroMale}, []Human{Male{}}},
+		{[]any{zeroMale, zeroDog, 2, &zeroDog, 0, &zeroMale}, []Human{Male{}, &Male{}}},
+		{[]any{&zeroMale, &zeroDog, 2, zeroDog, 0, zeroMale}, []Human{&Male{}, Male{}}},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%v", tt.data), func(t *testing.T) {
+			{
+				copy := slices.Clone(tt.data)
+				got := slices_.TypeAssertFilter[[]any, []Human](copy)
+				if (got == nil && tt.data != nil) || (got != nil && tt.data == nil) {
+					t.Errorf("slices_.TypeAssertFilterFunc(%v, func(e any) (Human, bool) {h, ok := e.(Human); return h, ok}) = %v, want %v", tt.data, got, tt.want)
+					return
+				}
+
+				for i, v := range got {
+					if i >= len(tt.want) {
+						t.Errorf("slices_.TypeAssertFilterFunc(%v, func(e any) (Human, bool) {h, ok := e.(Human); return h, ok}) = %v, want %v", tt.data, got, tt.want)
+						return
+					}
+
+					if reflect.TypeOf(v) != reflect.TypeOf(tt.want[i]) {
+						t.Errorf("slices_.TypeAssertFilterFunc(%v, func(e any) (Human, bool) {h, ok := e.(Human); return h, ok}) =[%d]: %v, want %v", tt.data, i, reflect.TypeOf(v).String(), reflect.TypeOf(tt.want[i]).String())
+					}
+				}
+			}
+		})
+	}
+}
+
+func TestTypeAssertFilterFunc(t *testing.T) {
+	tests := []struct {
+		data []int
+		want []int8
+	}{
+		{nil, nil},
+		{[]int{}, []int8{}},
+		{[]int{0}, []int8{0}},
+		{[]int{1, 0}, []int8{1, 0}},
+		{[]int{1, 2}, []int8{1, 2}},
+		{[]int{0, 1, 2}, []int8{0, 1, 2}},
+		{[]int{0, 1, 0, 2}, []int8{0, 1, 0, 2}},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%v", tt.data), func(t *testing.T) {
+			{
+				copy := slices.Clone(tt.data)
+				got := slices_.TypeAssertFilterFunc[[]int, []int8](copy, func(e int) (int8, bool) {
+					return int8(e), true
+				})
+				if (got == nil && tt.data != nil) || (got != nil && tt.data == nil) {
+					t.Errorf("slices_.TypeAssertFilterFunc(%v, func(e int) (int8, bool) {return int8(e), true}) = %v, want %v", tt.data, got, tt.want)
+					return
+				}
+
+				if slices.Compare(got, tt.want) != 0 {
+					t.Errorf("slices_.TypeAssertFilterFunc(%v, func(e int) (int8, bool) {return int8(e), true}) = %v, want %v", tt.data, got, tt.want)
+					return
+				}
+			}
+		})
+	}
+}
+
+func TestTypeAssertFilterFuncItoa(t *testing.T) {
+	tests := []struct {
+		data []int
+		want []string
+	}{
+		{nil, nil},
+		{[]int{}, []string{}},
+		{[]int{0}, []string{"0"}},
+		{[]int{1, 0}, []string{"1", "0"}},
+		{[]int{1, 2}, []string{"1", "2"}},
+		{[]int{0, 1, 2}, []string{"0", "1", "2"}},
+		{[]int{0, 1, 0, 2}, []string{"0", "1", "0", "2"}},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%v", tt.data), func(t *testing.T) {
+			{
+				copy := slices.Clone(tt.data)
+				got := slices_.TypeAssertFilterFunc[[]int, []string](copy, func(e int) (string, bool) {
+					return strconv.Itoa(e), true
+				})
+				if (got == nil && tt.data != nil) || (got != nil && tt.data == nil) {
+					t.Errorf("slices_.TypeAssertFilterFunc(%v, func(e int) (string, bool) {return strconv.Itoa(e), true}) = %v, want %v", tt.data, got, tt.want)
+					return
+				}
+
+				if slices.Compare(got, tt.want) != 0 {
+					t.Errorf("slices_.TypeAssertFilterFunc(%v, func(e int) (string, bool) {return strconv.Itoa(e), true}) = %v, want %v", tt.data, got, tt.want)
+					return
+				}
+			}
+		})
+	}
+}
+
+func TestTypeAssertFilterFuncConvert(t *testing.T) {
+	var zeroDog Dog
+	var zeroMale Male
+	_ = zeroDog
+	_ = zeroMale
+	tests := []struct {
+		data []any
+		want []Human
+	}{
+		{nil, nil},
+		{[]any{}, []Human{}},
+		{[]any{0}, []Human{}},
+		{[]any{1, 0, &Dog{}, &Male{}}, []Human{&Male{}}},
+		{[]any{1, 2, zeroDog, zeroMale}, []Human{Male{}}},
+		{[]any{1, 2, zeroDog, 0, zeroMale, &zeroDog, 0, &zeroMale}, []Human{Male{}, &Male{}}},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%v", tt.data), func(t *testing.T) {
+			{
+				copy := slices.Clone(tt.data)
+				got := slices_.TypeAssertFilterFunc[[]any, []Human](copy, func(e any) (Human, bool) {
+					h, ok := e.(Human)
+					return h, ok
+				})
+				if (got == nil && tt.data != nil) || (got != nil && tt.data == nil) {
+					t.Errorf("slices_.TypeAssertFilterFunc(%v, func(e int) (int8, bool) {return int8(e), true}) = %v, want %v", tt.data, got, tt.want)
+					return
+				}
+
+				for _, v := range got {
+					if v.Kind() != "male" {
+						t.Errorf("slices_.TypeAssertFilterFunc(%v, func(e any) (Human, bool) {h, ok := e.(Human); return h, ok}) = %v, want %v", tt.data, got, tt.want)
+						return
+					}
+				}
+			}
+		})
+	}
+}
+
+type Animal interface {
+	Kind() string
+}
+type Human interface {
+	Name() string
+	Animal
+}
+
+var _ Human = (*Male)(nil)
+
+type Male struct{}
+
+func (m Male) Kind() string {
+	return "male"
+}
+
+func (Male) Name() string {
+	return "bob"
+}
+
+var _ Animal = (*Dog)(nil)
+
+type Dog struct{}
+
+func (d Dog) Kind() string {
+	return "dog"
 }
