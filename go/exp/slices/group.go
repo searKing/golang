0598efae572a41b1bee549168c00cde933d6a78c
searKing <471030698@qq.com>
2023-04-06 20:18:20 +0800

    feat(exp/slices)：add Group and GroupFunc returns a map group by all elements in s that have the same values.

---
+++ go/exp/slices/group.go
@@ -0,0 +1,69 @@
+// Copyright 2023 The searKing Author. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package slices
+
+import "github.com/searKing/golang/go/exp/constraints"
+
+// Group returns a map group by all elements in s that have the same values.
+//
+// If s is nil, Group returns nil (zero map).
+//
+// If s is empty, Group returns an empty map.
+//
+// Else, Group returns a map group by all elements in s that have the same values.
+func Group[S ~[]E, M ~map[E]N, E comparable, N constraints.Number](s S) M {
+	// If s is nil, Split returns nil (zero map).
+	if s == nil {
+		return nil
+	}
+
+	// Below: s != nil
+
+	// If s is empty, Split returns an empty map.
+	if len(s) == 0 {
+		var emptyM = M{}
+		return emptyM
+	}
+
+	// Below: len(s) > 0 && f != nil
+	var m = M{}
+	for _, e := range s {
+		m[e] = m[e] + 1
+	}
+	return m
+
+}
+
+// GroupFunc returns a map satisfying f(c) within
+// all c in the map group by all elements in s that have the same values.
+//
+// If s is nil, GroupFunc returns nil (zero map).
+//
+// If s and f are both empty or nil, GroupFunc returns an empty map.
+//
+// Else, GroupFunc returns a map satisfying f(c)
+// within all c in the map group by all elements in s that have the same values.
+func GroupFunc[S ~[]E, M ~map[K]S, E any, K comparable](s S, f func(E) K) M {
+	// If s is nil, Split returns nil (zero submaps).
+	if s == nil {
+		return nil
+	}
+
+	// Below: s != nil
+
+	// If both s and f are empty or nil, Split returns an empty map.
+	if len(s) == 0 && f == nil {
+		var emptyM = M{}
+		return emptyM
+	}
+
+	// Below: len(s) > 0 && f != nil
+	var m = M{}
+	for _, e := range s {
+		k := f(e)
+		m[k] = append(m[k], e)
+	}
+	return m
+}
---
+++ go/exp/slices/group_test.go
@@ -0,0 +1,72 @@
+// Copyright 2023 The searKing Author. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package slices_test
+
+import (
+	"testing"
+
+	slices_ "github.com/searKing/golang/go/exp/slices"
+	"golang.org/x/exp/slices"
+)
+
+var groupTests = []struct {
+	s    []int
+	want map[int]int
+}{
+	{nil, nil},
+	{[]int{}, map[int]int{}},
+	{[]int{1}, map[int]int{1: 1}},
+	{[]int{1, 1}, map[int]int{1: 2}},
+	{[]int{1, 2, 3}, map[int]int{1: 1, 2: 1, 3: 1}},
+	{[]int{1, 1, 2, 3, 3}, map[int]int{1: 2, 2: 1, 3: 2}},
+	{[]int{1, 2, 3, 4}, map[int]int{1: 1, 2: 1, 3: 1, 4: 1}},
+	{[]int{1, 2, 3, 4, 2, 4, 2}, map[int]int{1: 1, 2: 3, 3: 1, 4: 2}},
+}
+
+func TestGroup(t *testing.T) {
+	for i, test := range groupTests {
+		got := slices_.Group[[]int, map[int]int, int, int](test.s)
+		if len(test.want) != len(got) {
+			t.Errorf("#%d: Group(%v) = %v, want %v", i, test.s, got, test.want)
+			continue
+		}
+		for k := range got {
+			if got[k] != test.want[k] {
+				t.Errorf("#%d: Group(%v) = %v, want %v", i, test.s, got, test.want)
+				break
+			}
+		}
+	}
+}
+
+var groupFuncTests = []struct {
+	s    []int
+	want map[int][]int
+}{
+	{nil, nil},
+	{[]int{}, map[int][]int{}},
+	{[]int{1}, map[int][]int{1: {1}}},
+	{[]int{1, 1}, map[int][]int{1: {1, 1}}},
+	{[]int{1, 2, 3}, map[int][]int{1: {1}, 2: {2}, 3: {3}}},
+	{[]int{1, 1, 2, 3, 3}, map[int][]int{1: {1, 1}, 2: {2}, 3: {3, 3}}},
+	{[]int{1, 2, 3, 4}, map[int][]int{1: {1}, 2: {2}, 3: {3}, 4: {4}}},
+	{[]int{1, 2, 3, 4, 2, 4, 2}, map[int][]int{1: {1}, 2: {2, 2, 2}, 3: {3}, 4: {4, 4}}},
+}
+
+func TestGroupFunc(t *testing.T) {
+	for i, test := range groupFuncTests {
+		got := slices_.GroupFunc[[]int, map[int][]int, int, int](test.s, func(i int) int { return i })
+		if len(test.want) != len(got) {
+			t.Errorf("#%d: Group(%v) = %v, want %v", i, test.s, got, test.want)
+			continue
+		}
+		for k := range got {
+			if !slices.Equal(got[k], test.want[k]) {
+				t.Errorf("#%d: Group(%v) = %v, want %v", i, test.s, got, test.want)
+				break
+			}
+		}
+	}
+}
