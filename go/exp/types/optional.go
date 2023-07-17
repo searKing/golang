066a97060b4086986844fab3432d223a12616d4d
searKing <471030698@qq.com>
2023-07-17 19:03:07 +0800

    feat(exp/types)：add Optional represents a Value that may be null.

---
+++ go/exp/types/optional.go
@@ -0,0 +1,89 @@
+// Copyright 2023 The searKing Author. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package types
+
+import (
+	"fmt"
+	"io"
+	"reflect"
+
+	"golang.org/x/exp/constraints"
+)
+
+// Optional represents a Value that may be null.
+type Optional[E any] struct {
+	Value E
+	Valid bool // Valid is true if Value is not NULL
+}
+
+func (o Optional[E]) Format(s fmt.State, verb rune) {
+	if o.Valid {
+		_, _ = fmt.Fprintf(s, "%"+string(verb), o.Value)
+		return
+	}
+	switch verb {
+	case 'v':
+		if s.Flag('+') {
+			_, _ = fmt.Fprintf(s, "null %s: %+v", reflect.TypeOf(o.Value).String(), o.Value)
+			return
+		}
+		fallthrough
+	case 's', 'q':
+		_, _ = io.WriteString(s, "null")
+	}
+}
+
+func (o Optional[E]) String() string {
+	if o.Valid {
+		return fmt.Sprintf("%v", o.Value)
+	}
+	return "null"
+}
+
+// ValueOr returns the contained value if available, another value otherwise
+func (o Optional[E]) ValueOr(e E) E {
+	if o.Valid {
+		return o.Value
+	}
+	return e
+}
+
+func CompareOptional[E constraints.Ordered](a, b Optional[E]) int {
+	if a.Valid && !b.Valid {
+		return 1
+	}
+	if !a.Valid && b.Valid {
+		return -1
+	}
+	if !a.Valid && !b.Valid {
+		return 0
+	}
+
+	if a.Value == b.Value {
+		return 0
+	}
+	if a.Value < b.Value {
+		return -1
+	}
+	return 1
+}
+
+func Opt[E any](e E) Optional[E] {
+	return Optional[E]{
+		Value: e,
+		Valid: true,
+	}
+}
+
+// NullOpt is the null Optional.
+//
+// Deprecated: Use a literal types.Optional[E]{} instead.
+func NullOpt[E any]() Optional[E] {
+	var zeroE E
+	return Optional[E]{
+		Value: zeroE,
+		Valid: false,
+	}
+}
---
+++ go/exp/types/optional_test.go
@@ -0,0 +1,148 @@
+// Copyright 2023 The searKing Author. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package types_test
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+
+	"github.com/searKing/golang/go/exp/types"
+)
+
+func ExampleOptional_String() {
+	fmt.Printf("%s\n", types.Optional[string]{}.String())
+	fmt.Printf("%s\n", types.Optional[int]{}.String())
+	fmt.Printf("%s\n", types.Optional[*int]{}.String())
+
+	fmt.Printf("%s\n", types.Opt("").String())
+	fmt.Printf("%s\n", types.Opt(0).String())
+	fmt.Printf("%s\n", types.Opt[*int](nil).String())
+
+	fmt.Printf("%s\n", types.Opt("hello world").String())
+	fmt.Printf("%s\n", types.Opt(0xFF).String())
+	var p int
+	fmt.Printf("%s\n", types.Opt(&p).String()[:2])
+
+	// Output:
+	// null
+	// null
+	// null
+	//
+	// 0
+	// <nil>
+	// hello world
+	// 255
+	// 0x
+}
+
+func ExampleOptional_Format() {
+	fmt.Printf("'%+v'\n", types.Optional[string]{})
+	fmt.Printf("'%v'\n", types.Optional[string]{})
+	fmt.Printf("'%s'\n", types.Optional[string]{})
+	fmt.Printf("'%+v'\n", types.Optional[int]{})
+	fmt.Printf("'%v'\n", types.Optional[int]{})
+	fmt.Printf("'%s'\n", types.Optional[int]{})
+	fmt.Printf("'%+v'\n", types.Optional[*int]{})
+	fmt.Printf("'%v'\n", types.Optional[*int]{})
+	fmt.Printf("'%s'\n", types.Optional[*int]{})
+
+	fmt.Printf("'%+v'\n", types.Opt(""))
+	fmt.Printf("'%v'\n", types.Opt(""))
+	fmt.Printf("'%s'\n", types.Opt(""))
+	fmt.Printf("'%+v'\n", types.Opt(0))
+	fmt.Printf("'%v'\n", types.Opt(0))
+	fmt.Printf("'%s'\n", types.Opt(0))
+	fmt.Printf("'%+v'\n", types.Opt[*int](nil))
+	fmt.Printf("'%v'\n", types.Opt[*int](nil))
+	fmt.Printf("'%s'\n", types.Opt[*int](nil))
+
+	fmt.Printf("'%+v'\n", types.Opt("hello world"))
+	fmt.Printf("'%v'\n", types.Opt("hello world"))
+	fmt.Printf("'%s'\n", types.Opt("hello world"))
+	fmt.Printf("'%+v'\n", types.Opt(0xFF))
+	fmt.Printf("'%v'\n", types.Opt(0xFF))
+	fmt.Printf("'%s'\n", types.Opt(0xFF))
+	var p int
+	fmt.Printf("'%s'\n", fmt.Sprintf("%+v", types.Opt(&p))[:2])
+	fmt.Printf("'%s'\n", fmt.Sprintf("%v", types.Opt(&p))[:2])
+	fmt.Printf("'%s'\n", fmt.Sprintf("%s", types.Opt(&p))[:11])
+
+	// Output:
+	// 'null string: '
+	// 'null'
+	// 'null'
+	// 'null int: 0'
+	// 'null'
+	// 'null'
+	// 'null *int: <nil>'
+	// 'null'
+	// 'null'
+	// ''
+	// ''
+	// ''
+	// '0'
+	// '0'
+	// '%!s(int=0)'
+	// '<nil>'
+	// '<nil>'
+	// '%!s(*int=<nil>)'
+	// 'hello world'
+	// 'hello world'
+	// 'hello world'
+	// '255'
+	// '255'
+	// '%!s(int=255)'
+	// '0x'
+	// '0x'
+	// '%!s(*int=0x'
+}
+
+func TestOptional_String(t *testing.T) {
+	tests := []struct {
+		a    types.Optional[int]
+		want string
+	}{
+		{types.Optional[int]{}, "null"},
+		{types.Optional[int]{}, "null"},
+		{types.Opt[int](1), "1"},
+		{types.Opt[int](0), "0"},
+	}
+	for i, tt := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			{
+				got := tt.a.String()
+				if got != tt.want {
+					t.Errorf("%v.String() got (%v), want ( %v)", tt.a, got, tt.want)
+				}
+			}
+		})
+	}
+
+}
+
+func TestCompareOptional(t *testing.T) {
+	tests := []struct {
+		a, b types.Optional[int]
+		want int
+	}{
+		{types.Optional[int]{}, types.Optional[int]{}, 0},
+		{types.Optional[int]{}, types.Opt[int](1), -1},
+		{types.Opt[int](1), types.Optional[int]{}, 1},
+		{types.Opt[int](1), types.Opt[int](1), 0},
+		{types.Opt[int](0), types.Opt[int](1), -1},
+		{types.Opt[int](1), types.Opt[int](0), 1},
+	}
+	for i, tt := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			{
+				got := types.CompareOptional(tt.a, tt.b)
+				if got != tt.want {
+					t.Errorf("CompareOptional(%v, %v) got (%v), want ( %v)", tt.a, tt.b, got, tt.want)
+				}
+			}
+		})
+	}
+}
