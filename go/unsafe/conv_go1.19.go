2c64a7a724082fc0432753ebd8ddb6569d4fdd8e
searKing <471030698@qq.com>
2023-08-24 17:06:46 +0800

    fix(unsafe): StringToBytes does not correctly set the capacity on the returned slice.

--- go/unsafe/conv_go1.19.go
+++ go/unsafe/conv_go1.19.go
@@ -12,7 +12,12 @@ import (
 
 // StringToBytes converts string to byte slice without a memory allocation.
 func StringToBytes(s string) []byte {
-	return *(*[]byte)(unsafe.Pointer(&s))
+	return *(*[]byte)(unsafe.Pointer(
+		&struct {
+			string
+			Cap int
+		}{s, len(s)},
+	))
 }
 
 // BytesToString converts byte slice to string without a memory allocation.
