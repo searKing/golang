7830b02706927c29b119936fa28e8fa86ab8988d
searKing <471030698@qq.com>
2023-08-30 16:52:11 +0800

    test(tools/go-import): run endtoend test in target dir

--- tools/cmd/go-import/go.mod
+++ tools/cmd/go-import/go.mod
@@ -3,6 +3,6 @@ module github.com/searKing/golang/tools/cmd/go-import
 go 1.16
 
 require (
-	github.com/searKing/golang/go v1.2.52
-	golang.org/x/tools v0.4.0
+	github.com/searKing/golang/go v1.2.98
+	github.com/searKing/golang/tools/go-import v1.2.98
 )
--- tools/cmd/go-import/import-gen.go
+++
@@ -1,7 +0,0 @@
-// Copyright 2020 The searKing Author. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-package main
-
-//go:generate go-bindata -o import_files.go -pkg main ./tmpl/...
