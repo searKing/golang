c9bf9dccc3e2ee1750126ce9640d2eed27b6868b
searKing <471030698@qq.com>
2023-12-25 11:37:44 +0800

    perf(go/exp): use go 1.21's approach for slices.Reverse.

--- go/exp/slices/reverse.go
+++ go/exp/slices/reverse.go
@@ -4,12 +4,11 @@
 
 package slices
 
+import "slices"
+
 // Reverse reorder a slice of any ordered type in reverse order.
 // Reverse modifies the contents of the slice s; it does not create a new slice.
-func Reverse[S ~[]E, E any](x S) {
-	for i := 0; i < len(x)>>1; i++ {
-		t := x[i]
-		x[i] = x[len(x)-1-i]
-		x[len(x)-1-i] = t
-	}
+// Deprecated: Use slices.Reverse instead since go1.21.
+func Reverse[S ~[]E, E any](s S) {
+	slices.Reverse(s)
 }
