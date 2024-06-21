d637caf9478ea3a0fb21d90fb7c317b2a60883cf
searKing <471030698@qq.com>
2024-06-21 11:37:07 +0800

    feat(grpc-gateway-v2): add WithForwardResponseMessageHandler.

--- third_party/github.com/grpc-ecosystem/grpc-gateway-v2/grpc/error.go
+++ third_party/github.com/grpc-ecosystem/grpc-gateway-v2/grpc/error.go
@@ -30,3 +30,13 @@ type ForwardResponseMessageHandlerFunc func(ctx context.Context, mux *runtime.Se
 func (f ForwardResponseMessageHandlerFunc) ForwardResponseMessage(ctx context.Context, mux *runtime.ServeMux, marshaler runtime.Marshaler, w http.ResponseWriter, req *http.Request, resp proto.Message, opts ...func(context.Context, http.ResponseWriter, proto.Message) error) {
 	f(ctx, mux, marshaler, w, req, resp, opts...)
 }
+
+type ForwardResponseOptionHandler interface {
+	ForwardResponseOption(context.Context, http.ResponseWriter, proto.Message) error
+}
+
+type ForwardResponseHandlerFunc func(context.Context, http.ResponseWriter, proto.Message) error
+
+func (f ForwardResponseHandlerFunc) ForwardResponseOption(ctx context.Context, w http.ResponseWriter, m proto.Message) error {
+	return f(ctx, w, m)
+}
