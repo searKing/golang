f06e3916ebd2007474331da16ddc55c1fa6a2a31
searKing <471030698@qq.com>
2024-06-26 16:22:56 +0800

    feat(grpc-gateway-v2): log http request received if level debug is enabled.

--- third_party/github.com/grpc-ecosystem/grpc-gateway-v2/grpc/logging.http.go
+++ third_party/github.com/grpc-ecosystem/grpc-gateway-v2/grpc/logging.http.go
@@ -8,11 +8,19 @@ import (
 	"fmt"
 	"log/slog"
 	"net/http"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/logging"
+	slices_ "github.com/searKing/golang/go/exp/slices"
 	http_ "github.com/searKing/golang/go/net/http"
 	time_ "github.com/searKing/golang/go/time"
+)
+
+var (
+	// SystemTag is tag representing an event inside gRPC call.
+	SystemTag = []string{"protocol", "http"}
 )
 
 // HttpInterceptor returns a new unary http interceptors that optionally logs endpoint handling.
@@ -23,33 +31,52 @@ func HttpInterceptor(l logging.Logger) func(handler http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			var cost time_.Cost
 			cost.Start()
+			var attrs []any
+			attrs = append(attrs, slog.String(SystemTag[0], SystemTag[1]))
+			if slog.Default().Enabled(r.Context(), slog.LevelDebug) {
+				attrs = append(attrs, slog.Time("http.start_time", time.Now()))
+			}
+			attrs = append(attrs, extractLoggingFieldsFromHttpRequest(r)...)
+			if slog.Default().Enabled(r.Context(), slog.LevelDebug) {
+				l.Log(r.Context(), logging.LevelDebug, fmt.Sprintf("http request received"), attrs...)
+			}
 
 			rw := http_.NewRecordResponseWriter(w)
 			handler.ServeHTTP(rw, r)
-			attrs := logging.ExtractFields(r.Context())
-			attrs = append(attrs, slog.String("remote_addr", r.RemoteAddr))
-			ip := http_.ClientIP(r)
-			if ip != "" && !strings.HasPrefix(r.RemoteAddr, ip) {
-				attrs = append(attrs, slog.String("client_ip", http_.ClientIP(r)))
-			}
 
-			attrs = append(attrs, slog.String("method", r.Method), slog.String("request_uri", r.RequestURI))
-
-			absRequestURI := strings.HasPrefix(r.RequestURI, "http://") || strings.HasPrefix(r.RequestURI, "https://")
-			if !absRequestURI {
-				host := r.Host
-				if host == "" && r.URL != nil {
-					host = r.URL.Host
-				}
-				if host != "" {
-					attrs = append(attrs, slog.String("host", host))
-				}
-			}
-			attrs = append(attrs, slog.String("status_code", http.StatusText(rw.Status())),
+			attrs = append(attrs, slog.String("http.status_code", slices_.FirstOrZero(http.StatusText(rw.Status()), "CODE("+strconv.FormatInt(int64(rw.Status()), 10)+")")),
 				slog.Duration("cost", cost.Elapse()),
-				slog.Int64("request_body_size", r.ContentLength),
-				slog.Int("response_body_size", rw.Size()))
+				slog.Int64("http.request_body_size", r.ContentLength),
+				slog.Int("http.response_body_size", rw.Size()))
 			l.Log(r.Context(), logging.LevelInfo, fmt.Sprintf("finished http call with code %d", rw.Status()), attrs...)
 		})
 	}
+}
+
+func extractLoggingFieldsFromHttpRequest(r *http.Request) []any {
+	attrs := logging.ExtractFields(r.Context())
+	if slog.Default().Enabled(r.Context(), slog.LevelDebug) {
+		if d, ok := r.Context().Deadline(); ok {
+			attrs = append(attrs, slog.Time("http.request.deadline", d))
+		}
+	}
+	attrs = append(attrs, slog.String("http.remote_addr", r.RemoteAddr))
+	ip := http_.ClientIP(r)
+	if ip != "" && !strings.HasPrefix(r.RemoteAddr, ip) {
+		attrs = append(attrs, slog.String("http.client_ip", http_.ClientIP(r)))
+	}
+
+	attrs = append(attrs, slog.String("http.method", r.Method), slog.String("http.request.uri", r.RequestURI))
+
+	absRequestURI := strings.HasPrefix(r.RequestURI, "http://") || strings.HasPrefix(r.RequestURI, "https://")
+	if !absRequestURI {
+		host := r.Host
+		if host == "" && r.URL != nil {
+			host = r.URL.Host
+		}
+		if host != "" {
+			attrs = append(attrs, slog.String("http.host", host))
+		}
+	}
+	return attrs
 }
