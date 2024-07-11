fab0c929e20ba2895991e5f10ead304abdfa6c34
searKing <471030698@qq.com>
2024-07-11 16:08:52 +0800

    refactor(grpc-gateway-v2): deprecated: Use official runtime.JSONPb instead of JSONPb and runtime.HTTPBodyMarshaler instead of HTTPBodyPb.

--- third_party/github.com/grpc-ecosystem/grpc-gateway-v2/runtime/marshal_httpbodyproto.go
+++ third_party/github.com/grpc-ecosystem/grpc-gateway-v2/runtime/marshal_httpbodyproto.go
@@ -13,5 +13,7 @@ import (
 // the actual message used as the response. If not, then this will
 // simply fallback to the Marshaler specified as its default Marshaler.
 //
+// Deprecated: Use runtime.HTTPBodyMarshaler instead.
+//
 //go:generate go-option -type=HTTPBodyPb
 type HTTPBodyPb runtime.HTTPBodyMarshaler
