eb057bee6682632e077f543eb04e85139a181426
searKing <471030698@qq.com>
2025-08-06 22:42:49 +0800

    doc: format comment

--- go/crypto/rand/string.go
+++ go/crypto/rand/string.go
@@ -21,26 +21,26 @@ const (
 	CharsetAlphaNum          = CharsetDecimalDigits + CharsetAlpha
 	CharsetBase64            = CharsetAlphaNum + "+/"
 
-	// StdEncoding is the standard base64 encoding, as defined in
+	// CharsetBase64StdEncoding is the standard base64 encoding, as defined in
 	// RFC 4648.
 	CharsetBase64StdEncoding = CharsetBase64RawStdEncoding + string(base64.StdPadding)
 
-	// URLEncoding is the alternate base64 encoding defined in RFC 4648.
+	// CharsetBase64URLEncoding is the alternate base64 encoding defined in RFC 4648.
 	// It is typically used in URLs and file names.
 	CharsetBase64URLEncoding = CharsetBase64RawURLEncoding + string(base64.StdPadding)
 
-	// RawStdEncoding is the standard raw, unpadded base64 encoding,
+	// CharsetBase64RawStdEncoding is the standard raw, unpadded base64 encoding,
 	// as defined in RFC 4648 section 3.2.
 	// This is the same as StdEncoding but omits padding characters.
 	CharsetBase64RawStdEncoding = CharsetAlphaNum + "+/"
 
-	// RawURLEncoding is the unpadded alternate base64 encoding defined in RFC 4648.
+	// CharsetBase64RawURLEncoding is the unpadded alternate base64 encoding defined in RFC 4648.
 	// It is typically used in URLs and file names.
 	// This is the same as URLEncoding but omits padding characters.
 	CharsetBase64RawURLEncoding = CharsetAlphaNum + "-_"
 )
 
-// take in a character set and a length and will generate a random string using that character set.
+// StringWithCharset takes in a character set and a length and will generate a random string using that character set.
 func StringWithCharset(len int, charset string) string {
 	s, err := StringCryptoWithCharset(int64(len), charset)
 	if err == nil {
@@ -49,7 +49,7 @@ func StringWithCharset(len int, charset string) string {
 	return StringMathWithCharset(len, charset)
 }
 
-// only take in a length, and will use a default characters set to generate a random string
+// String only takes in a length, and will use a default characters set to generate a random string
 func String(len int) string {
 	return StringWithCharset(len, CharsetAlphaNum)
 }
