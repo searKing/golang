525529612c40f9b1f2964ee057a23ba5548fe30d
searKing <471030698@qq.com>
2025-10-28 21:20:34 +0800

    feat(webserver): reorder input options to last for improved parameter prioritization.

--- pkg/webserver/webserver.interceptors.grpc.go
+++ pkg/webserver/webserver.interceptors.grpc.go
@@ -28,84 +28,88 @@ var (
 // gRPC-Gateway does not support gRPC interceptors when call gRPC's service handler in process.
 // See: https://github.com/grpc-ecosystem/grpc-gateway/issues/1043
 func (f *Factory) UnaryHandlers(handlers ...grpc_.UnaryHandlerDecorator) []grpc_.UnaryHandlerDecorator {
+	var s []grpc_.UnaryHandlerDecorator
 	// recover
-	handlers = append(handlers, grpc_.UnaryHandlerDecoratorFunc(recovery.UnaryHandler))
+	s = append(s, grpc_.UnaryHandlerDecoratorFunc(recovery.UnaryHandler))
 	// validate
 	if v := f.fc.Validator; v != nil {
-		handlers = append(handlers, validator_.UnaryHandlerDecorator(v))
+		s = append(s, validator_.UnaryHandlerDecorator(v))
 	}
 	// request id
 	if f.fc.FillRequestId {
-		handlers = append(handlers, grpc_.UnaryHandlerDecoratorFunc(requestid.UnaryHandler))
+		s = append(s, grpc_.UnaryHandlerDecoratorFunc(requestid.UnaryHandler))
 	}
-	return handlers
+	return append(s, handlers...)
 }
 
 func (f *Factory) UnaryServerInterceptors(interceptors ...grpc.UnaryServerInterceptor) []grpc.UnaryServerInterceptor {
+	var s []grpc.UnaryServerInterceptor
 	// recover
-	interceptors = append(interceptors, recovery.UnaryServerInterceptor())
+	s = append(s, recovery.UnaryServerInterceptor())
 	// handle request timeout
-	interceptors = append(interceptors, timeoutlimit.UnaryServerInterceptor(f.fc.HandledTimeoutUnary))
+	s = append(s, timeoutlimit.UnaryServerInterceptor(f.fc.HandledTimeoutUnary))
 	// burst limit
-	interceptors = append(interceptors, burstlimit.UnaryServerInterceptor(f.fc.MaxConcurrencyUnary, f.fc.BurstLimitTimeoutUnary))
+	s = append(s, burstlimit.UnaryServerInterceptor(f.fc.MaxConcurrencyUnary, f.fc.BurstLimitTimeoutUnary))
 	// map tags in context, for request local storage
-	interceptors = append(interceptors, tags.UnaryServerInterceptor(tagsCtxMarkerKey, map[string]any{}))
+	s = append(s, tags.UnaryServerInterceptor(tagsCtxMarkerKey, map[string]any{}))
 	// validate
 	if v := f.fc.Validator; v != nil {
-		interceptors = append(interceptors, validator_.UnaryServerInterceptor(v))
+		s = append(s, validator_.UnaryServerInterceptor(v))
 	}
 	// request id
 	if f.fc.FillRequestId {
-		interceptors = append(interceptors, requestid.UnaryServerInterceptor())
+		s = append(s, requestid.UnaryServerInterceptor())
 	}
-	return interceptors
+	return append(s, interceptors...)
 }
 
 func (f *Factory) StreamServerInterceptors(interceptors ...grpc.StreamServerInterceptor) []grpc.StreamServerInterceptor {
+	var s []grpc.StreamServerInterceptor
 	// recover
-	interceptors = append(interceptors, recovery.StreamServerInterceptor())
+	s = append(s, recovery.StreamServerInterceptor())
 	// handle request timeout
-	interceptors = append(interceptors, timeoutlimit.StreamServerInterceptor(f.fc.HandledTimeoutUnary))
+	s = append(s, timeoutlimit.StreamServerInterceptor(f.fc.HandledTimeoutUnary))
 	// burst limit
-	interceptors = append(interceptors, burstlimit.StreamServerInterceptor(f.fc.MaxConcurrencyUnary, f.fc.BurstLimitTimeoutUnary))
+	s = append(s, burstlimit.StreamServerInterceptor(f.fc.MaxConcurrencyUnary, f.fc.BurstLimitTimeoutUnary))
 	// map tags in context, for request local storage
-	interceptors = append(interceptors, tags.StreamServerInterceptor(tagsCtxMarkerKey, map[string]any{}))
+	s = append(s, tags.StreamServerInterceptor(tagsCtxMarkerKey, map[string]any{}))
 	// validate
 	if v := f.fc.Validator; v != nil {
-		interceptors = append(interceptors, validator_.StreamServerInterceptor(v))
+		s = append(s, validator_.StreamServerInterceptor(v))
 	}
 	// request id
 	if f.fc.FillRequestId {
-		interceptors = append(interceptors, requestid.StreamServerInterceptor())
+		s = append(s, requestid.StreamServerInterceptor())
 	}
-	return interceptors
+	return append(s, interceptors...)
 }
 
 func (f *Factory) UnaryClientInterceptors(interceptors ...grpc.UnaryClientInterceptor) []grpc.UnaryClientInterceptor {
+	var s []grpc.UnaryClientInterceptor
 	// handle request timeout
-	interceptors = append(interceptors, timeoutlimit.UnaryClientInterceptor(f.fc.HandledTimeoutUnary))
+	s = append(s, timeoutlimit.UnaryClientInterceptor(f.fc.HandledTimeoutUnary))
 	// burst limit
-	interceptors = append(interceptors, burstlimit.UnaryClientInterceptor(f.fc.MaxConcurrencyUnary, f.fc.BurstLimitTimeoutUnary))
+	s = append(s, burstlimit.UnaryClientInterceptor(f.fc.MaxConcurrencyUnary, f.fc.BurstLimitTimeoutUnary))
 	// map tags in context, for request local storage
-	interceptors = append(interceptors, tags.UnaryClientInterceptor(tagsCtxMarkerKey, map[string]any{}))
+	s = append(s, tags.UnaryClientInterceptor(tagsCtxMarkerKey, map[string]any{}))
 	// request id
 	if f.fc.FillRequestId {
-		interceptors = append(interceptors, requestid.UnaryClientInterceptor())
+		s = append(s, requestid.UnaryClientInterceptor())
 	}
-
-	return interceptors
+	return append(s, interceptors...)
 }
 
 func (f *Factory) StreamClientInterceptors(interceptors ...grpc.StreamClientInterceptor) []grpc.StreamClientInterceptor {
+	var s []grpc.StreamClientInterceptor
 	// handle request timeout
-	interceptors = append(interceptors, timeoutlimit.StreamClientInterceptor(f.fc.HandledTimeoutUnary))
+	s = append(s, timeoutlimit.StreamClientInterceptor(f.fc.HandledTimeoutUnary))
 	// burst limit
-	interceptors = append(interceptors, burstlimit.StreamClientInterceptor(f.fc.MaxConcurrencyUnary, f.fc.BurstLimitTimeoutUnary))
+	s = append(s, burstlimit.StreamClientInterceptor(f.fc.MaxConcurrencyUnary, f.fc.BurstLimitTimeoutUnary))
 	// map tags in context, for request local storage
-	interceptors = append(interceptors, tags.StreamClientInterceptor(tagsCtxMarkerKey, map[string]any{}))
+	s = append(s, tags.StreamClientInterceptor(tagsCtxMarkerKey, map[string]any{}))
 	// request id
 	if f.fc.FillRequestId {
-		interceptors = append(interceptors, requestid.StreamClientInterceptor())
+		s = append(s, requestid.StreamClientInterceptor())
 	}
-	return interceptors
+	return append(s, interceptors...)
 }
